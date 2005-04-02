817670855f80a618f21d726c5eaa09f28b04eca6
wmayer <wmayer@users.sourceforge.net>
2005-04-02 14:25:59 +0000

    iso8859

--- src/Mod/Part/Gui/Part_de.ts
+++ src/Mod/Part/Gui/Part_de.ts
@@ -1,153 +1,155 @@
-<!DOCTYPE TS><TS>
-<context>
-    <name>PartGui::DlgPartBox</name>
-    <message>
-        <source>Box definition</source>
-        <translation>Quaderdefinition</translation>
-    </message>
-    <message>
-        <source>Position:</source>
-        <translation>Position:</translation>
-    </message>
-    <message>
-        <source>X:</source>
-        <translation>X:</translation>
-    </message>
-    <message>
-        <source>0</source>
-        <translation>0</translation>
-    </message>
-    <message>
-        <source>Z:</source>
-        <translation>Z:</translation>
-    </message>
-    <message>
-        <source>Y:</source>
-        <translation>Y:</translation>
-    </message>
-    <message>
-        <source>Size:</source>
-        <translation>Gr√∂√üe:</translation>
-    </message>
-    <message>
-        <source>100</source>
-        <translation>100</translation>
-    </message>
-    <message>
-        <source>U:</source>
-        <translation>U:</translation>
-    </message>
-    <message>
-        <source>V:</source>
-        <translation>V:</translation>
-    </message>
-    <message>
-        <source>W:</source>
-        <translation>W:</translation>
-    </message>
-    <message>
-        <source>Apply</source>
-        <translation>√úbernehmen</translation>
-    </message>
-    <message>
-        <source>Cancel</source>
-        <translation>Abbrechen</translation>
-    </message>
-    <message>
-        <source>OK</source>
-        <translation>Ok</translation>
-    </message>
-</context>
-<context>
-    <name>PartGui::DlgPartImportIges</name>
-    <message>
-        <source>IGES input file</source>
-        <translation>IGES-Datei</translation>
-    </message>
-    <message>
-        <source>File Name</source>
-        <translation>Dateiname</translation>
-    </message>
-    <message>
-        <source>...</source>
-        <translation>...</translation>
-    </message>
-    <message>
-        <source>Cancel</source>
-        <translation>Abbrechen</translation>
-    </message>
-    <message>
-        <source>OK</source>
-        <translation>Ok</translation>
-    </message>
-</context>
-<context>
-    <name>PartGui::DlgPartImportStep</name>
-    <message>
-        <source>Step input file</source>
-        <translation>STEP-Datei</translation>
-    </message>
-    <message>
-        <source>File Name</source>
-        <translation>Dateiname</translation>
-    </message>
-    <message>
-        <source>...</source>
-        <translation>...</translation>
-    </message>
-    <message>
-        <source>Cancel</source>
-        <translation>Abbrechen</translation>
-    </message>
-    <message>
-        <source>OK</source>
-        <translation>Ok</translation>
-    </message>
-</context>
-<context>
-    <name>PartGui::DlgSettings3DViewPart</name>
-    <message>
-        <source>Part Viewing Settings</source>
-        <translation>Part-Viewer</translation>
-    </message>
-    <message>
-        <source>3DViewPart settings</source>
-        <translation>3D-Viewer</translation>
-    </message>
-    <message>
-        <source>View accuracy / Performance</source>
-        <translation>Genauigkeit/Performanz</translation>
-    </message>
-    <message>
-        <source>Max deviation of view mesh (smaller means nicer but slower):</source>
-        <translation>Max. Abweichung der Tessellierung:</translation>
-    </message>
-    <message>
-        <source>View smooting</source>
-        <translation>Gl√§ttung</translation>
-    </message>
-    <message>
-        <source>Not using per vertex normals (using flat shading)</source>
-        <translation>Nicht Normale per Vertex (flache Schattierung)</translation>
-    </message>
-    <message>
-        <source>Using high qualiti normals (slower but nicer)</source>
-        <translation>Hohe Qualit√§t der Normalen</translation>
-    </message>
-    <message>
-        <source>Show view mesh vertexes</source>
-        <translation>Zeige Vertexe der Tessellierung</translation>
-    </message>
-    <message>
-        <source>Hilight color</source>
-        <translation>Farbe</translation>
-    </message>
-</context>
-<context>
-    <name>QObject</name>
-    <message>
-        <source>Part design</source>
-        <translation>Part design</translation>
-    </message>
-</context>
+<?xml version="1.0" encoding="ISO-8859-1"?>
+<!DOCTYPE TS>
+<TS>
+    <context>
+        <name>PartGui::DlgPartBox</name>
+        <message>
+            <source>Box definition</source>
+            <translation>Quaderdefinition</translation>
+        </message>
+        <message>
+            <source>Position:</source>
+            <translation>Position:</translation>
+        </message>
+        <message>
+            <source>X:</source>
+            <translation>X:</translation>
+        </message>
+        <message>
+            <source>0</source>
+            <translation>0</translation>
+        </message>
+        <message>
+            <source>Z:</source>
+            <translation>Z:</translation>
+        </message>
+        <message>
+            <source>Y:</source>
+            <translation>Y:</translation>
+        </message>
+        <message>
+            <source>Size:</source>
+            <translation>Grˆﬂe:</translation>
+        </message>
+        <message>
+            <source>100</source>
+            <translation>100</translation>
+        </message>
+        <message>
+            <source>U:</source>
+            <translation>U:</translation>
+        </message>
+        <message>
+            <source>V:</source>
+            <translation>V:</translation>
+        </message>
+        <message>
+            <source>W:</source>
+            <translation>W:</translation>
+        </message>
+        <message>
+            <source>Apply</source>
+            <translation>‹bernehmen</translation>
+        </message>
+        <message>
+            <source>Cancel</source>
+            <translation>Abbrechen</translation>
+        </message>
+        <message>
+            <source>OK</source>
+            <translation>Ok</translation>
+        </message>
+    </context>
+    <context>
+        <name>PartGui::DlgPartImportIges</name>
+        <message>
+            <source>IGES input file</source>
+            <translation>IGES-Datei</translation>
+        </message>
+        <message>
+            <source>File Name</source>
+            <translation>Dateiname</translation>
+        </message>
+        <message>
+            <source>...</source>
+            <translation>...</translation>
+        </message>
+        <message>
+            <source>Cancel</source>
+            <translation>Abbrechen</translation>
+        </message>
+        <message>
+            <source>OK</source>
+            <translation>Ok</translation>
+        </message>
+    </context>
+    <context>
+        <name>PartGui::DlgPartImportStep</name>
+        <message>
+            <source>Step input file</source>
+            <translation>STEP-Datei</translation>
+        </message>
+        <message>
+            <source>File Name</source>
+            <translation>Dateiname</translation>
+        </message>
+        <message>
+            <source>...</source>
+            <translation>...</translation>
+        </message>
+        <message>
+            <source>Cancel</source>
+            <translation>Abbrechen</translation>
+        </message>
+        <message>
+            <source>OK</source>
+            <translation>Ok</translation>
+        </message>
+    </context>
+    <context>
+        <name>PartGui::DlgSettings3DViewPart</name>
+        <message>
+            <source>Part Viewing Settings</source>
+            <translation>Part-Viewer</translation>
+        </message>
+        <message>
+            <source>3DViewPart settings</source>
+            <translation>3D-Viewer</translation>
+        </message>
+        <message>
+            <source>View accuracy / Performance</source>
+            <translation>Genauigkeit/Performanz</translation>
+        </message>
+        <message>
+            <source>Max deviation of view mesh (smaller means nicer but slower):</source>
+            <translation>Max. Abweichung der Tessellierung:</translation>
+        </message>
+        <message>
+            <source>View smooting</source>
+            <translation>Gl‰ttung</translation>
+        </message>
+        <message>
+            <source>Not using per vertex normals (using flat shading)</source>
+            <translation>Nicht Normale per Vertex (flache Schattierung)</translation>
+        </message>
+        <message>
+            <source>Using high qualiti normals (slower but nicer)</source>
+            <translation>Hohe Qualit‰t der Normalen</translation>
+        </message>
+        <message>
+            <source>Show view mesh vertexes</source>
+            <translation>Zeige Vertexe der Tessellierung</translation>
+        </message>
+        <message>
+            <source>Hilight color</source>
+            <translation>Farbe</translation>
+        </message>
+    </context>
+    <context>
+        <name>QObject</name>
+        <message>
+            <source>Part design</source>
+            <translation>Part design</translation>
+        </message>
+    </context>
 </TS>
