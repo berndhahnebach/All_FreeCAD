7c53cae7f201e8a0a04b066abfd11077852e6b20
wmayer <wmayer@users.sourceforge.net>
2005-04-26 22:05:16 +0000

    set current workbench as default if none is set in parameters

--- src/Gui/DlgGeneralImp.cpp
+++ src/Gui/DlgGeneralImp.cpp
@@ -50,15 +50,15 @@ using namespace Gui::Dialog;
 DlgGeneralImp::DlgGeneralImp( QWidget* parent,  const char* name, WFlags fl )
     : DlgGeneralBase( parent, name, fl )
 {
-
   // fills the combo box with all available workbenches
   QStringList work = ApplicationWindow::Instance->workbenches();
   work.sort();
-
   // do not save the content but the current item only
   AutoloadModuleCombo->setKeepPreference( true );
   AutoloadModuleCombo->insertStringList( work );
-
+  // set the current workbench as default, AutoloadModuleCombo->onRestore() will change
+  // it, if it is set in the preferences
+  AutoloadModuleCombo->setCurrentText( ApplicationWindow::Instance->activeWorkbench() );
 }
 
 /** 
@@ -150,5 +150,3 @@ void DlgGeneralImp::loadSettings()
 #include "DlgGeneral.cpp"
 #include "moc_DlgGeneral.cpp"
 #include "moc_DlgGeneralImp.cpp"
-
-
