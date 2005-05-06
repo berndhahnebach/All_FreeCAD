b0af7508ccba84cb304f456987c4d850efdd8e41
wmayer <wmayer@users.sourceforge.net>
2005-05-06 12:22:56 +0000

    Linux stuff

--- src/Mod/Mesh/App/Delaunay.cpp
+++ src/Mod/Mesh/App/Delaunay.cpp
@@ -40,7 +40,9 @@ const Vector3 Vector3::UNIT_X(1,0,0);
 const Vector3 Vector3::UNIT_Y(0,1,0);
 const Vector3 Vector3::UNIT_Z(0,0,1);
 
+#ifdef FC_OS_WIN32
 const Real INFINITY = DBL_MAX;
+#endif
 
 
 //----------------------------------------------------------------------------
