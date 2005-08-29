549115ac111847fdf2038567ecc24bfe62495f30
wmayer <wmayer@users.sourceforge.net>
2005-08-29 21:16:01 +0000

    transform impl.

--- src/Mod/Mesh/App/Mesh.cpp
+++ src/Mod/Mesh/App/Mesh.cpp
@@ -48,12 +48,31 @@ void MeshPropertyNormal::resizePoints(void)
 }
 
 
-void MeshPropertyNormal::transform(const Matrix4D &rclMat)
+void MeshPropertyNormal::transform(const Matrix4D& mat)
 {
-  // trivial implementation ;-)
-  setInvalid();
-  Normales.clear();
+  // A normal vector is only a direction with unit length, so we only need to rotate it
+  // (no translations or scaling)
 
+  // Extract scale factors (assumes an orthogonal rotation matrix)
+  // Use the fact that the length of the row vectors of R are all equal to 1
+  // And that scaling is applied after rotating
+  double s[3];
+  s[0] = sqrt(mat[0][0] * mat[0][0] + mat[0][1] * mat[0][1] + mat[0][2] * mat[0][2]);
+  s[1] = sqrt(mat[1][0] * mat[1][0] + mat[1][1] * mat[1][1] + mat[1][2] * mat[1][2]);
+  s[2] = sqrt(mat[2][0] * mat[2][0] + mat[2][1] * mat[2][1] + mat[2][2] * mat[2][2]);
+
+  // Set up the rotation matrix: zero the translations and make the scale factors = 1
+  Matrix4D rot;
+  rot.Unit();
+  for (int i = 0; i < 3; i++)
+    for (int j = 0; j < 3; j++)
+      rot[i][j] = mat[i][j] / s[i];
+
+  // Rotate the normal vectors
+  for (std::vector<Vector3D>::iterator it = Normales.begin(); it != Normales.end(); ++it)
+  {
+    *it = rot * (*it);
+  }
 }
 
 // ----------------------------------------------------------------------------
