4aa881551670be6ab84948cc389278aa8cd03900
wmayer <wmayer@users.sourceforge.net>
2005-11-06 00:44:37 +0000

    new build

--- src/Version.h
+++ src/Version.h
@@ -1,4 +1,5 @@
 #define FCVersionMajor 0
 #define FCVersionMinor 3
-#define FCVersionBuild 3
-#define FCVersionDisDa "Tue Oct  4 10:04:27 2005" 
+#define FCVersionBuild 4
+#define FCVersionDisDa "Sun Nov  6 01:21:46 2005" 
+
