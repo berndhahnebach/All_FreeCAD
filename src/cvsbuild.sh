8abeac2bde2525b6e102e3ad8ec4373d55395144
wmayer <wmayer@users.sourceforge.net>
2006-01-08 00:33:14 +0000

    Linux stuff

--- src/cvsbuild.sh
+++ src/cvsbuild.sh
@@ -1,7 +1,25 @@
 #!/bin/sh
+# cvsbuild.sh
+# Run this to generate all initial makefiles.
 
+#aclocal
+#autoheader
+#automake --add-missing
+#autoconf configure.ac > configure && chmod ug+x configure
+#./configure
+
+echo "calling libtoolize"
+libtoolize --force --copy
+
+
+echo "calling aclocal"
 aclocal
-autoheader
-automake --add-missing
-autoconf configure.ac > configure && chmod ug+x configure
-./configure
+
+echo "calling autoconf"
+autoconf
+
+echo "calling automake"
+automake
+
+echo "Done"
+echo "Please run configure."
