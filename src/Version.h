16e246c8b1b5a6a1e98599aad166e8c6bc64a899
jriegel <FreeCAD@juergen-riegel.net>
2006-01-15 19:03:44 +0000

    New Version

--- src/Version.h
+++ src/Version.h
@@ -1,5 +1,5 @@
 #define FCVersionMajor 0
-#define FCVersionMinor 3
-#define FCVersionBuild 5
+#define FCVersionMinor 5
+#define FCVersionBuild 1
 #define FCVersionDisDa "Wed Nov 30 15:57:41 2005" 
 
