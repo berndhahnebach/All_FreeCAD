fcc42077d8692862add9cbf967a812994361abde
jriegel <FreeCAD@juergen-riegel.net>
2006-07-21 13:05:56 +0000

    Fix -t option to run tests

--- src/App/FreeCADTest.py
+++ src/App/FreeCADTest.py
@@ -32,64 +32,7 @@
 
 Log ("FreeCAD test running...\n\n")
 
-TempPath = os.getenv('TEMP')
-if not TempPath: TempPath='.'
-Log ('Using temp path: ' + TempPath + '\n')
-
-# Basics +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-FreeCAD.PrintLog ("Testing console...\n")
-
-FreeCAD.PrintMessage("   Printing message\n")
-FreeCAD.PrintError("   Printing error\n")
-FreeCAD.PrintWarning("   Printing warning\n")
-FreeCAD.PrintLog("   Printing Log\n")
-
-# Application and Document +++++++++++++++++++++++++++++++++++++++++++++++++
-Log ("Testing Application and Document...\n")
-
-Log("   Creating new Part document and filing up\n")
-Doc = App.New("Part")
-i = Doc.UndoLimit
-i = Doc.AvailableUndos
-i = Doc.AvailableRedos
-
-Main = Doc.Main #getting the main label of the document 
-L1 = Main.GetLabel(1)
-L1 = Main.GetLabel(1)
-L1 = Main.GetLabel(1)
-L1 = Main.GetLabel(1)
-L1 = Main.GetLabel(1)
-L1 = Main.GetLabel(1)
-L2 = Main.GetLabel(2)
-L3 = Main.GetLabel(3)
-L4 = Main.GetLabel(4)
-L5 = Main.GetLabel(5)
-L6 = Main.GetLabel(6)
-L7 = Main.GetLabel(7)
-L1.Int = 1
-L1.Real = 1.0
-L1.Name = "Hallo"
-
-# saving and restoring
-
-SavePath = TempPath + os.sep + "Test1.FCStd"
-Log("   Save and Open the document to: " + SavePath + "\n")
-Doc.SaveAs(SavePath)
-Log("provocate exceptio by try loading a already open document\n")
-try:
-    Doc2 = App.DocOpen(SavePath)
-except:
-    Log("exception thrown, OK\n")
-else:
-    Log("no exception thrown, ERROR\n")
-    raise IOError
-# closing doc
-Doc = 0
-
-#Doc2 = App.DocOpen(SavePath)
-
-#Log(Doc2.Main.GetLabel(1).Name)
-
+import TestApp;TestApp.TestText("TestApp.All")
 
 
 FreeCAD.PrintLog ("FreeCAD test done\n")
