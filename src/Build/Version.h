d7357af91ad370675b864c38fd305c743d51cc05
jriegel <FreeCAD@juergen-riegel.net>
2007-02-03 21:38:29 +0000

    Version 0.7

--- src/Build/Version.h
+++ src/Build/Version.h
@@ -1,13 +1,13 @@
 
 /// Version Number
 #define FCVersionMajor "0"
-#define FCVersionMinor "5"
+#define FCVersionMinor "7"
 
-#define FCRevision      "169"      //Highest committed revision number
-#define FCRevisionDate  "2006/08/18 16:01:18"     //Date of highest committed revision
-#define FCRevisionRange "169"    //Update revision range
+#define FCRevision      "434"      //Highest committed revision number
+#define FCRevisionDate  "2007/02/03 22:21:18"     //Date of highest committed revision
+#define FCRevisionRange "434"    //Update revision range
 #define FCRepositoryURL "https://svn.sourceforge.net/svnroot/free-cad/trunk/src"      //Repository URL of the working copy
-#define FCCurrentDateT  "2006/08/21 21:06:09"      //Current system date & time
+#define FCCurrentDateT  "2007/02/03 22:21:18"      //Current system date & time
 
 //Placeholders of the form "$WCxxx?TrueText:FalseText$" are replaced with
 //TrueText if the tested condition is true, and FalseText if false.
