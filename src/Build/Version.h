75f099e3fb41ba4231b0a86f0e232a0876b81cfb
wmayer <wmayer@users.sourceforge.net>
2007-02-28 15:14:20 +0000

    + Update WXI files

--- src/Build/Version.h
+++ src/Build/Version.h
@@ -3,14 +3,14 @@
 #define FCVersionMajor "0"
 #define FCVersionMinor "7"
 
-#define FCRevision      "434"      //Highest committed revision number
-#define FCRevisionDate  "2007/02/03 22:21:18"     //Date of highest committed revision
-#define FCRevisionRange "434"    //Update revision range
+#define FCRevision      "479"      //Highest committed revision number
+#define FCRevisionDate  "2007/02/28 14:24:45"     //Date of highest committed revision
+#define FCRevisionRange "478:479"    //Update revision range
 #define FCRepositoryURL "https://svn.sourceforge.net/svnroot/free-cad/trunk/src"      //Repository URL of the working copy
-#define FCCurrentDateT  "2007/02/03 22:21:18"      //Current system date & time
+#define FCCurrentDateT  "2007/02/28 16:11:32"      //Current system date & time
 
 //Placeholders of the form "$WCxxx?TrueText:FalseText$" are replaced with
 //TrueText if the tested condition is true, and FalseText if false.
 
 #define FCScrClean      "Src modified"     //True if local modifications found
-#define FCScrMixed      "Src not mixed"          //True if mixed update revisions found
+#define FCScrMixed      "Src mixed"          //True if mixed update revisions found
