820ba5710100ebb4df4d92aa7169e971edf14de5
jriegel <FreeCAD@juergen-riegel.net>
2007-03-14 18:00:42 +0000

    + starting new boost::command_line_parser

--- src/App/Application.cpp
+++ src/App/Application.cpp
@@ -35,6 +35,9 @@
 # endif
 #endif
 
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
+
 
 #include "Application.h"
 #include "Document.h"
@@ -998,6 +1001,94 @@ void Application::LoadParameters(void)
 }
 
 
+#if 0
+// A helper function to simplify the main part.
+template<class T>
+ostream& operator<<(ostream& os, const vector<T>& v)
+{
+    copy(v.begin(), v.end(), ostream_iterator<T>(cout, " ")); 
+    return os;
+}
+
+
+void Application::ParseOptions(int ac, char ** av)
+{
+        int opt;
+    
+        // Declare a group of options that will be 
+        // allowed only on command line
+        po::options_description generic("Generic options");
+        generic.add_options()
+            ("version,v", "print version string")
+            ("help", "produce help message")    
+            ;
+    
+        // Declare a group of options that will be 
+        // allowed both on command line and in
+        // config file
+        po::options_description config("Configuration");
+        config.add_options()
+            ("optimization", po::value<int>(&opt)->default_value(10), 
+                  "optimization level")
+            ("include-path,I", 
+                 po::value< vector<string> >()->composing(), 
+                 "include path")
+            ;
+
+        // Hidden options, will be allowed both on command line and
+        // in config file, but will not be shown to the user.
+        po::options_description hidden("Hidden options");
+        hidden.add_options()
+            ("input-file", po::value< vector<string> >(), "input file")
+            ;
+
+        
+        po::options_description cmdline_options;
+        cmdline_options.add(generic).add(config).add(hidden);
+
+        po::options_description config_file_options;
+        config_file_options.add(config).add(hidden);
+
+        po::options_description visible("Allowed options");
+        visible.add(generic).add(config);
+        
+        po::positional_options_description p;
+        p.add("input-file", -1);
+        
+        po::variables_map vm;
+        store(po::command_line_parser(ac, av).
+              options(cmdline_options).positional(p).run(), vm);
+
+        ifstream ifs("multiple_sources.cfg");
+        store(parse_config_file(ifs, config_file_options), vm);
+        notify(vm);
+    
+        if (vm.count("help")) {
+            cout << visible << "\n";
+            exit( 0);
+        }
+
+        if (vm.count("version")) {
+            cout << "Multiple sources example, version 1.0\n";
+            exit( 0);
+        }
+
+        if (vm.count("include-path"))
+        {
+            cout << "Include paths are: " 
+                 << vm["include-path"].as< vector<string> >() << "\n";
+        }
+
+        if (vm.count("input-file"))
+        {
+            cout << "Input files are: " 
+                 << vm["input-file"].as< vector<string> >() << "\n";
+        }
+
+        cout << "Optimization level is " << opt << "\n";    
+}
+
+#else
 void Application::ParseOptions(int argc, char ** argv)
 {
 	static const char Usage[] = \
@@ -1183,6 +1274,7 @@ void Application::ParseOptions(int argc, char ** argv)
 
 }  
 
+#endif
 
 void Application::ExtractUser()
 {
