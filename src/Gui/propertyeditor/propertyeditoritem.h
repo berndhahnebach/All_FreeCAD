0471e873225f5b66a00d0c4d3cb54964ff8d669d
wmayer <wmayer>
2007-03-22 12:46:41 +0000

    + Linux port

--- src/Gui/propertyeditor/propertyeditoritem.h
+++ src/Gui/propertyeditor/propertyeditoritem.h
@@ -25,11 +25,11 @@
 #define PROPERTYEDITORITEM_H
 
 #ifndef __Qt4All__
-# include "Qt4All.h"
+# include <Gui/Qt4All.h>
 #endif
 
 #ifndef __Qt3All__
-# include "Qt3All.h"
+# include <Gui/Qt3All.h>
 #endif
 
 #include <vector>
