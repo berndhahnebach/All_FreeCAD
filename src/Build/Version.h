f5744d3bf1af89a82908b63f235c3491b35520b4
wmayer <wmayer>
2007-03-23 13:20:06 +0000

    + Linux port is running

--- src/Build/Version.h
+++ src/Build/Version.h
@@ -3,11 +3,11 @@
 #define FCVersionMajor "0"
 #define FCVersionMinor "7"
 
-#define FCRevision      "501"      //Highest committed revision number
-#define FCRevisionDate  "2007/03/21 14:25:01"     //Date of highest committed revision
-#define FCRevisionRange "501"    //Update revision range
+#define FCRevision      "506"      //Highest committed revision number
+#define FCRevisionDate  "2007/03/23 01:17:23"     //Date of highest committed revision
+#define FCRevisionRange "506"    //Update revision range
 #define FCRepositoryURL "https://svn.sourceforge.net/svnroot/free-cad/trunk"      //Repository URL of the working copy
-#define FCCurrentDateT  "2007/03/21 16:19:08"      //Current system date & time
+#define FCCurrentDateT  "2007/03/23 08:35:09"      //Current system date & time
 
 //Placeholders of the form "$WCxxx?TrueText:FalseText$" are replaced with
 //TrueText if the tested condition is true, and FalseText if false.
