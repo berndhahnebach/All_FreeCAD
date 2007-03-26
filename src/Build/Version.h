e3f154cc1a79c1b70208247a1e23a0a6df606232
wmayer <wmayer>
2007-03-26 09:35:39 +0000

    + Python configure test for correct version

--- src/Build/Version.h
+++ src/Build/Version.h
@@ -3,14 +3,14 @@
 #define FCVersionMajor "0"
 #define FCVersionMinor "7"
 
-#define FCRevision      "510"      //Highest committed revision number
-#define FCRevisionDate  "2007/03/23 23:36:56"     //Date of highest committed revision
-#define FCRevisionRange "510:511"    //Update revision range
+#define FCRevision      "513"      //Highest committed revision number
+#define FCRevisionDate  "2007/03/25 17:53:47"     //Date of highest committed revision
+#define FCRevisionRange "513"    //Update revision range
 #define FCRepositoryURL "https://svn.sourceforge.net/svnroot/free-cad/trunk"      //Repository URL of the working copy
-#define FCCurrentDateT  "2007/03/24 08:34:11"      //Current system date & time
+#define FCCurrentDateT  "2007/03/26 04:44:33"      //Current system date & time
 
 //Placeholders of the form "$WCxxx?TrueText:FalseText$" are replaced with
 //TrueText if the tested condition is true, and FalseText if false.
 
 #define FCScrClean      "Src modified"     //True if local modifications found
-#define FCScrMixed      "Src mixed"          //True if mixed update revisions found
+#define FCScrMixed      "Src not mixed"          //True if mixed update revisions found
