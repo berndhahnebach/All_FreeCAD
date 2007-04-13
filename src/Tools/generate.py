9fa5c52d3af799ebfe87afc32a36d35db8b84c96
jriegel <FreeCAD@juergen-riegel.net>
2007-04-13 15:03:11 +0000

    BUGFIX [ 1698640 ] Python generators too static

--- src/Tools/generate.py
+++ src/Tools/generate.py
@@ -1,6 +1,6 @@
 #! python
 # -*- coding: utf-8 -*-
-# (c) 2006 Jürgen Riegel 
+# (c) 2006 Jürgen Riegel  GPL
 
 Usage = """generate - generates a FreeCAD Module out of an XML model
 
@@ -8,16 +8,18 @@ Usage:
    generate [Optionen] Model.xml Model2.xml Model3.xml ...
    
 Options:
- -h, --help               print this help
+ -h, --help          print this help
+ -o, --outputPath    specify the output path if differs from source path
  
 Generate source code out of an model definition.
    
 Autor:
   (c) 2006 Juergen Riegel
   juergen.riegel@web.de
+	Licence: GPL
 
 Version:
-  0.1
+  0.2
 """
 
 import os,sys,string,re,getopt,codecs,binascii
@@ -28,23 +30,19 @@ import generateTemplates.templateClassPyExport
 
 # Globals
 
-def generate(filename):
+def generate(filename,path):
   # load model
   GenerateModelInst = generateBase.generateModel_Module.parse(filename)
-  # Convert filename to an absolute path
-  filename = os.path.abspath(filename)
-  head,tail = os.path.split(filename)
-  print head,tail
 
   if(len(GenerateModelInst.Module)!=0): 
     Module= generateTemplates.templateModule.TemplateModule()
-    Module.path = head
+    Module.path = path
     Module.module = GenerateModelInst.Module[0]
     Module.Generate()
     print "Done generating: " + GenerateModelInst.Module[0].Name
   else:
     Export = generateTemplates.templateClassPyExport.TemplateClassPyExport()
-    Export.path = head+"/"
+    Export.path = path+"/"
     Export.export = GenerateModelInst.PythonExport[0]
     Export.Generate()
     print "Done generating: " + GenerateModelInst.PythonExport[0].Name
@@ -56,9 +54,10 @@ def generate(filename):
 
 
 def main():
+	defaultPath = ""
 	
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], "h", ["help"])
+		opts, args = getopt.getopt(sys.argv[1:], "ho:", ["help","outputPath="])
 	except getopt.GetoptError:
 		# print help information and exit:
 		sys.stderr.write(Usage)
@@ -69,6 +68,9 @@ def main():
 		if o in ("-h", "--help"):
 			sys.stderr.write(Usage)
 			sys.exit()
+		if o in ("-o", "--outputPath"):
+			defaultPath = a
+
 
 	# runing through the files
 	if (len(args) ==0):
@@ -76,7 +78,13 @@ def main():
 		generate("../Mod/PartDesign/PartDesign_Model.xml")                  
 	else:
 		for i in args:
-			generate(i)
+			filename = os.path.abspath(i)
+			if(defaultPath == ""):
+				head,tail = os.path.split(filename)
+				print head,tail
+				generate(filename,head)
+			else:
+				generate(filename,defaultPath)
 
 	
 if __name__ == "__main__":
