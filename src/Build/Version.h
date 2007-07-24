3752bf2f2587abba19f53d2c4b719ddc5d340ff6
wmayer <wmayer>
2007-07-24 23:49:21 +0000

    + fix bug #1749694 + empty man page added

--- src/Build/Version.h
+++ src/Build/Version.h
@@ -3,14 +3,14 @@
 #define FCVersionMajor "0"
 #define FCVersionMinor "7"
 
-#define FCRevision      "639"      //Highest committed revision number
-#define FCRevisionDate  "2007/07/09 15:12:12"     //Date of highest committed revision
-#define FCRevisionRange "639"    //Update revision range
+#define FCRevision      "644"      //Highest committed revision number
+#define FCRevisionDate  "2007/07/24 00:04:14"     //Date of highest committed revision
+#define FCRevisionRange "644:646"    //Update revision range
 #define FCRepositoryURL "https://free-cad.svn.sourceforge.net/svnroot/free-cad/trunk"      //Repository URL of the working copy
-#define FCCurrentDateT  "2007/07/09 21:51:59"      //Current system date & time
+#define FCCurrentDateT  "2007/07/24 15:58:24"      //Current system date & time
 
 //Placeholders of the form "$WCxxx?TrueText:FalseText$" are replaced with
 //TrueText if the tested condition is true, and FalseText if false.
 
 #define FCScrClean      "Src modified"     //True if local modifications found
-#define FCScrMixed      "Src not mixed"          //True if mixed update revisions found
+#define FCScrMixed      "Src mixed"          //True if mixed update revisions found
