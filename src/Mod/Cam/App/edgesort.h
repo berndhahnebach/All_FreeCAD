3c0228eb4e9fb5c74ed363acdb3c1fdff97c4cc3
dvdjimmy <dvdjimmy>
2007-11-13 16:08:37 +0000

    +added edge_sorter

--- src/Mod/Cam/App/edgesort.h
+++ src/Mod/Cam/App/edgesort.h
@@ -1,37 +1,40 @@
-#ifndef WIREEXPLORER_H
-#define WIREEXPLORER_H
+#ifndef EDGESORT_H
+#define EDGESORT_H
 
 #include <TopoDS_Wire.hxx>
 #include <TopoDS_Edge.hxx>
+#include <TopoDS_Shape.hxx>
 #include <gp_Pnt.hxx>
 #include <TopTools_MapOfShape.hxx>
 #include <map>
 #include <list>
 
-struct WireExplorer_gp_PntLess
+struct Edgesort_gp_Pnt_Less
 {
-       bool operator()(const gp_Pnt& _Left, const gp_Pnt& _Right) const
+       bool operator()(const gp_Pnt & _Left, const gp_Pnt & _Right) const
        {
                Standard_Real x1,y1,z1,x2,y2,z2;
-               _Left.Coord(x1,y1,z1);
-               _Right.Coord(x2,y2,z2);
-               if ( x1 != x2 )
+			   _Left.Coord(x1,y1,z1);
+			   _Right.Coord(x2,y2,z2);
+               if ( fabs(x1- x2) > 0.01 )
                        return x1 < x2;
-               else if ( y1 != y2 )
+               else if ( fabs(y1 -y2) > 0.01 )
                        return y1 < y2;
-               return z1 < z2;
+			   else if ( fabs(z1 -z2) > 0.01 )
+                       return z1 < z2;
+               return false;
        }
 };
 
 typedef std::list<TopoDS_Edge> tEdgeList;
-typedef std::map<gp_Pnt,tEdgeList,WireExplorer_gp_PntLess>  tMapPntShapes;
-typedef std::pair<gp_Pnt,tEdgeList> tMapPntShapesPair;
+typedef std::map<gp_Pnt,tEdgeList,Edgesort_gp_Pnt_Less>  tMapPntEdge;
+typedef std::pair<gp_Pnt,tEdgeList> tMapPntEdgePair;
 
-class WireExplorer
+class Edgesort
 {
 public:
-       Standard_EXPORT WireExplorer(const TopoDS_Wire& wire);
-       Standard_EXPORT virtual ~WireExplorer(void);
+       Standard_EXPORT Edgesort(const TopoDS_Shape& aShape);
+       Standard_EXPORT virtual ~Edgesort(void);
 
        Standard_EXPORT void Init();
        Standard_EXPORT bool More();
@@ -42,9 +45,9 @@ private:
        void Perform(const TopoDS_Edge& edge);
        bool PerformEdges(gp_Pnt& point);
        bool IsValidEdge(const TopoDS_Edge& edge);
-       TopoDS_Wire m_wire;
+       TopoDS_Shape m_shape;
 
-       tMapPntShapes m_vertices;
+       tMapPntEdge m_vertices;
 
        bool m_done;
        tEdgeList m_edges;
