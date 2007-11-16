d42029336e504674d59d75be78f34834b6986542
jriegel <FreeCAD@juergen-riegel.net>
2007-11-16 16:08:48 +0000

    + implementing image capturing

--- src/Mod/Image/App/CaptureClass.cpp
+++ src/Mod/Image/App/CaptureClass.cpp
@@ -1,16 +1,9 @@
 //---------------------------------------------------------------------------
-
-#ifdef _MSC_VER
-#pragma hdrstop
-#endif
+#include "PreCompiled.h"
 
 #include "CaptureClass.h"
 
 //---------------------------------------------------------------------------
-
-#ifdef _MSC_VER
-#pragma package(smart_init)
-#endif
 
 
 	/// Constructor with an Capture number, 0 will ask for one
@@ -21,6 +14,14 @@ Capturerer::Capturerer(int num)
 
   if( !capture )
 	throw "Cant create capture device";
+
+    hScale=0.5;
+    vScale=0.5;
+    lineWidth=1;
+    useLabel=true;
+
+    // Init font
+    cvInitFont(&font,CV_FONT_HERSHEY_SIMPLEX/*|CV_FONT_ITALIC*/, hScale,vScale,0,lineWidth);
 
 
 }
@@ -35,36 +36,63 @@ Capturerer::Capturerer(const char* fileName)
 
 }
 
+Capturerer::~Capturerer()
+{
+	if(_bIsWinOn)
+		cvDestroyWindow("Capture");
+    if(capture)
+        cvReleaseCapture(&capture);
+}
+
+
 void Capturerer::setCaptureWindows(bool On)
 {
 	if(!_bIsWinOn && On)
 	{
 		cvNamedWindow( "Capture", 0 );
+        _bIsWinOn = true;
 	}
 	if(_bIsWinOn && !On)
 	{
 		cvDestroyWindow("Capture");
+        _bIsWinOn = false;
 	}
 }
 
 
-void Capturerer::getOneCapture(void)
+char Capturerer::getOneCapture(void)
 {
+    static int i = 0;
     // Get frame
     IplImage* frame = NULL;
 	frame = cvQueryFrame( capture );
+
+    double time = cvGetCaptureProperty(capture, CV_CAP_PROP_FPS );
   
     if( !frame )
 		throw "Cannot get frame";
 
+    if(! captureImage)
+        size = cvGetSize(frame);
+        captureImage = cvCreateImage( size, 8, 3 );
+    
      // copy memory frame to image
 	cvCopy( frame, captureImage, 0 );
 
     // Flip
 	cvFlip(captureImage, captureImage);
 
+    // label
+    if (useLabel){
+        sprintf(buff,"T:%d ",i++);
+        cvPutText (captureImage,buff, cvPoint(0,size.height - 5) , &font, cvScalar(0,255,0));
+    }
+
+
 	if(_bIsWinOn)
 	  cvShowImage( "Capture", captureImage );
+
+    return cvWaitKey(10);
 
 }
 
--- src/Mod/Image/App/CaptureClass.h
+++ src/Mod/Image/App/CaptureClass.h
@@ -9,21 +9,35 @@
 
 //---------------------------------------------------------------------------
 
-class Capturerer
+class ImageAppExport Capturerer
 {
+public:
 	/// Constructor with an Capture number, 0 will ask for one
 	Capturerer(int num = 0);
 	/// Constructor with an File name. 	Object will capture from that Video file
 	Capturerer(const char* fileName);
 
+    ~Capturerer();
+
 	void setCaptureWindows(bool On);
 
-	void getOneCapture(void);
+	char getOneCapture(void);
 
 private:
   CvCapture* capture;
   IplImage *captureImage;
   bool _bIsWinOn;
+
+  CvSize size;
+
+    // font stuff
+    CvFont font;
+    double hScale;
+    double vScale;
+    int    lineWidth;
+    bool   useLabel;
+    char   buff[100];
+
 
 };
 
