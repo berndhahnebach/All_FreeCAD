3ffe767fec43f78c18947000136770d8141ca166
jriegel <FreeCAD@juergen-riegel.net>
2008-01-19 15:20:51 +0000

    + add forgotten files

---
+++ src/Mod/Part/App/TopoShapeFacePyImp.cpp
@@ -0,0 +1,44 @@
+
+#include "PreCompiled.h"
+
+#include "Mod/Part/App/TopoShape.h"
+#include <ShapeAnalysis.hxx>
+
+// inclusion of the generated files (generated out of TopoShapeFacePy.xml)
+#include "TopoShapeFacePy.h"
+#include "TopoShapeFacePy.cpp"
+
+using namespace Part;
+
+// returns a string which represent the object e.g. when printed in python
+const char *TopoShapeFacePy::representation(void) const
+{
+    return "<TopoShape object>";
+}
+
+
+Py::Object TopoShapeFacePy::getWire(void) const
+{
+    TopoDS_Shape clSh = getTopoShapePtr()->_Shape;
+    if (clSh.ShapeType() == TopAbs_FACE)
+    {
+		TopoDS_Face clFace = (TopoDS_Face&)clSh;
+		TopoDS_Wire clWire = ShapeAnalysis::OuterWire(clFace);
+		return Py::Object(new TopoShapePy(new TopoShape(clWire)),true);
+	}else
+		throw "Internal error, TopoDS_SHape is not a face!";
+
+    return Py::Object();
+}
+
+PyObject *TopoShapeFacePy::getCustomAttributes(const char* attr) const
+{
+    return 0;
+}
+
+int TopoShapeFacePy::setCustomAttributes(const char* attr, PyObject *obj)
+{
+    return 0; 
+}
+
+
