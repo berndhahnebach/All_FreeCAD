a531628c5a5ca870181840d1cc2ec57768ec45ed
wmayer <wmayer@users.sourceforge.net>
2008-01-20 13:11:01 +0000

    + update Makefile.am

--- package.sh
+++ package.sh
@@ -2,6 +2,6 @@
 
 # In the Debian rules file we expect to get the OpenCascade installation path
 # by the environment variable CASROOT.
-export CASROOT=${CASROOT:-$HOME/Projects/OpenCASCADE6.2.0/ros}
+export CASROOT=${CASROOT:-/opt/OpenCASCADE6.2.0/ros}
 make debian-package
 
