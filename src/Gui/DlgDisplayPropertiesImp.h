a53347cb8775195fd8f7e8b96a3fa449c36dd9d0
wmayer <wmayer>
2008-04-25 14:50:52 +0000

    + make display dialog non-modal and dockable

--- src/Gui/DlgDisplayPropertiesImp.h
+++ src/Gui/DlgDisplayPropertiesImp.h
@@ -36,6 +36,7 @@
 
 
 #include "ui_DlgDisplayProperties.h"
+#include "Selection.h"
 #include <App/Material.h>
 
 namespace App
@@ -55,31 +56,38 @@ namespace Dialog {
  * templates to create a new document.
  * \author Jürgen Riegel
  */
-class DlgDisplayPropertiesImp : public QDialog, public Ui_DlgDisplayProperties
+class DlgDisplayPropertiesImp : public QDialog, public Ui_DlgDisplayProperties, public Gui::SelectionSingleton::ObserverType
 {
     Q_OBJECT
 
 public:
-  DlgDisplayPropertiesImp( QWidget* parent = 0, Qt::WFlags fl = 0 );
-  ~DlgDisplayPropertiesImp();
+    DlgDisplayPropertiesImp( QWidget* parent = 0, Qt::WFlags fl = 0 );
+    ~DlgDisplayPropertiesImp();
+    /// Observer message from the Selection
+    void OnChange(Gui::SelectionSingleton::SubjectType &rCaller,
+                  Gui::SelectionSingleton::MessageType Reason);
 
-public Q_SLOTS:
-  void on_changeMaterial_activated(const QString&);
-  void on_changeMode_activated(const QString&);
-  void on_changePlot_activated(const QString&);
-  void on_buttonColor_changed();
-  void on_spinTransparency_valueChanged(int);
-  void on_spinPointSize_valueChanged(double);
-  void on_spinLineWidth_valueChanged(double);
-  void on_buttonUserDefinedMaterial_clicked();
+private Q_SLOTS:
+    void on_changeMaterial_activated(const QString&);
+    void on_changeMode_activated(const QString&);
+    void on_changePlot_activated(const QString&);
+    void on_buttonColor_changed();
+    void on_spinTransparency_valueChanged(int);
+    void on_spinPointSize_valueChanged(int);
+    void on_spinLineWidth_valueChanged(int);
+    void on_buttonUserDefinedMaterial_clicked();
 
-protected:
-  void accept();
-  void reject();
-
-protected:
-  std::vector<ViewProvider*> Provider;
-  QMap<QString, App::Material::MaterialType> Materials;
+private:
+    void reject();
+    void setDisplayModes(const std::vector<ViewProvider*>&);
+    void setMaterial(const std::vector<ViewProvider*>&);
+    void fillupMaterials();
+    void setShapeColor(const std::vector<ViewProvider*>&);
+    void setPointSize(const std::vector<ViewProvider*>&);
+    void setLineWidth(const std::vector<ViewProvider*>&);
+    void setTransparency(const std::vector<ViewProvider*>&);
+    std::vector<ViewProvider*> getSelection() const;
+    QMap<QString, App::Material::MaterialType> Materials;
 };
 
 } // namespace Dialog
