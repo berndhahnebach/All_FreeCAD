93f7b3a31b0befc3221a7d3cd34ca924b3628959
wmayer <wmayer@users.sourceforge.net>
2008-04-26 11:55:22 +0000

    + new version number

--- src/Build/Version.h
+++ src/Build/Version.h
@@ -4,15 +4,16 @@
 #define FCVersionMinor "7"
 #define FCVersionName  "Bespin"
 
-#define FCRevision      "725"      //Highest committed revision number
-#define FCRevisionDate  "2007/08/25 13:29:37"     //Date of highest committed revision
-#define FCRevisionRange "724:725"    //Update revision range
-#define FCRepositoryURL "https://free-cad.svn.sourceforge.net/svnroot/free-cad/trunk/src"      //Repository URL of the working copy
-#define FCCurrentDateT  "2007/08/25 14:03:25"      //Current system date & time
+#define FCRevision      "1315"      //Highest committed revision number
+#define FCRevisionDate  "2008/04/26 13:49:44"     //Date of highest committed revision
+#define FCRevisionRange "1315"    //Update revision range
+#define FCRepositoryURL "https://free-cad.svn.sourceforge.net/svnroot/free-cad/trunk"      //Repository URL of the working copy
+#define FCCurrentDateT  "2008/04/26 13:53:47"      //Current system date & time
 
 //Placeholders of the form "$WCxxx?TrueText:FalseText$" are replaced with
 //TrueText if the tested condition is true, and FalseText if false.
 
 #define FCScrClean      "Src modified"     //True if local modifications found
-#define FCScrMixed      "Src mixed"          //True if mixed update revisions found
+#define FCScrMixed      "Src not mixed"          //True if mixed update revisions found
+
 
