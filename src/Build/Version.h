0b33f3feb333ba39274d1e9d3fa2feccb5221994
wmayer <wmayer>
2008-05-19 23:20:35 +0000

    + embed OCC libs into debian package

--- src/Build/Version.h
+++ src/Build/Version.h
@@ -4,11 +4,11 @@
 #define FCVersionMinor "7"
 #define FCVersionName  "Bespin"
 
-#define FCRevision      "1315"      //Highest committed revision number
-#define FCRevisionDate  "2008/04/26 13:49:44"     //Date of highest committed revision
-#define FCRevisionRange "1315"    //Update revision range
+#define FCRevision      "1342"      //Highest committed revision number
+#define FCRevisionDate  "2008/05/19 11:25:16"     //Date of highest committed revision
+#define FCRevisionRange "1342"    //Update revision range
 #define FCRepositoryURL "https://free-cad.svn.sourceforge.net/svnroot/free-cad/trunk"      //Repository URL of the working copy
-#define FCCurrentDateT  "2008/04/26 13:53:47"      //Current system date & time
+#define FCCurrentDateT  "2008/05/19 23:39:47"      //Current system date & time
 
 //Placeholders of the form "$WCxxx?TrueText:FalseText$" are replaced with
 //TrueText if the tested condition is true, and FalseText if false.
