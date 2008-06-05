fa6b3c6473affe6203a711cfd6199bf3eab7d1c2
wmayer <wmayer@users.sourceforge.net>
2008-06-05 19:31:44 +0000

    + update Makefile

---
+++ src/Mod/Part/App/TopoShapeWirePyImp.cpp
@@ -0,0 +1,35 @@
+
+#include "PreCompiled.h"
+
+#include "Mod/Part/App/TopoShape.h"
+
+// inclusion of the generated files (generated out of TopoShapeWirePy.xml)
+#include "TopoShapeWirePy.h"
+#include "TopoShapeWirePy.cpp"
+
+using namespace Part;
+
+// returns a string which represents the object e.g. when printed in python
+const char *TopoShapeWirePy::representation(void) const
+{
+    return "<TopoShape object>";
+}
+
+
+
+Py::Object TopoShapeWirePy::getWire(void) const
+{
+    return Py::Object();
+}
+
+PyObject *TopoShapeWirePy::getCustomAttributes(const char* /*attr*/) const
+{
+    return 0;
+}
+
+int TopoShapeWirePy::setCustomAttributes(const char* /*attr*/, PyObject* /*obj*/)
+{
+    return 0; 
+}
+
+
