dc6282897a8340669721e24d4dc247895ec84e48
wmayer <wmayer@users.sourceforge.net>
2008-07-07 16:26:46 +0000

    + move functions from Part python module to appropriate python classes

--- src/Mod/Part/App/TopoShapeEdgePyImp.cpp
+++ src/Mod/Part/App/TopoShapeEdgePyImp.cpp
@@ -1,11 +1,17 @@
 
 #include "PreCompiled.h"
+#ifndef _PreComp_
+# include <BRepBuilderAPI_MakeEdge.hxx>
+#endif
 
 #include "Mod/Part/App/TopoShape.h"
 
 // inclusion of the generated files (generated out of TopoShapeEdgePy.xml)
 #include "TopoShapeEdgePy.h"
 #include "TopoShapeEdgePy.cpp"
+
+#include "Geometry.h"
+#include "GeometryPy.h"
 
 using namespace Part;
 
@@ -24,6 +30,46 @@ const char *TopoShapeEdgePy::representation(void) const
     static std::string buf;
     buf = str.str();
     return buf.c_str();
+}
+
+PyObject *TopoShapeEdgePy::PyMake(struct _typeobject *, PyObject *, PyObject *)  // Python wrapper
+{
+    // create a new instance of TopoShapeEdgePy and the Twin object 
+    return new TopoShapeEdgePy(new TopoShape);
+}
+
+// constructor method
+int TopoShapeEdgePy::PyInit(PyObject* args, PyObject* /*kwd*/)
+{
+    PyObject *pcObj;
+    double first=DBL_MAX, last=DBL_MAX;
+    if (PyArg_ParseTuple(args, "O!|dd", &(Part::GeometryPy::Type), &pcObj, &first, &last)) {
+        Geometry* geom = static_cast<GeometryPy*>(pcObj)->getGeometryPtr();
+        Handle_Geom_Curve curve = Handle_Geom_Curve::DownCast(geom->handle());
+        if (curve.IsNull()) {
+            PyErr_SetString(PyExc_Exception, "geometry is not a curve type");
+            return -1;
+        }
+
+        if (first==DBL_MAX)
+            first = curve->FirstParameter();
+        if (last==DBL_MAX)
+            last = curve->LastParameter();
+
+        try {
+            BRepBuilderAPI_MakeEdge mkEdge(curve, first, last);
+            getTopoShapePtr()->_Shape = mkEdge.Edge();
+            return 0;
+        }
+        catch (Standard_Failure) {
+            Handle_Standard_Failure e = Standard_Failure::Caught();
+            PyErr_SetString(PyExc_Exception, e->GetMessageString());
+            return -1;
+        }
+    }
+
+    PyErr_SetString(PyExc_Exception, "a curve expected");
+    return -1;
 }
 
 PyObject *TopoShapeEdgePy::getCustomAttributes(const char* /*attr*/) const
