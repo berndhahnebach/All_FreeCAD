b58a112f6c855308828a4c2408f43b422a687a66
wmayer <wmayer>
2008-07-11 10:26:20 +0000

    + new example added

--- src/Mod/Part/MakeBottle.py
+++ src/Mod/Part/MakeBottle.py
@@ -1,7 +1,10 @@
 import Part, math
+import FreeCAD, FreeCADGui
+App=FreeCAD
+Gui=FreeCADGui
 from FreeCAD import Base
 
-def makeBottle(myWidth, myHeight, myThickness):
+def makeBottle(myWidth=50, myHeight=70, myThickness=30):
 	aPnt1=Base.Vector(-myWidth/2.,0,0)
 	aPnt2=Base.Vector(-myWidth/2.,-myThickness/4.,0)
 	aPnt3=Base.Vector(0,-myThickness/2.,0)
@@ -39,3 +42,71 @@ def makeBottle(myWidth, myHeight, myThickness):
 	
 	return myBody
 
+def makeBoreHole():
+	# create a document if needed
+	if App.ActiveDocument == None:
+		App.newDocument("Solid")
+
+	Group = App.ActiveDocument.addObject("App::DocumentObjectGroup","Group")
+	Group.Label="Bore hole"
+
+	V1 = Base.Vector(0,10,0)
+	V2 = Base.Vector(30,10,0)
+	V3 = Base.Vector(30,-10,0)
+	V4 = Base.Vector(0,-10,0)
+	VC1 = Base.Vector(-10,0,0)
+	C1 = Part.Arc(V1,VC1,V4)
+	# and the second one
+	VC2 = Base.Vector(40,0,0)
+	C2 = Part.Arc(V2,VC2,V3)
+	L1 = Part.Line(V1,V2)
+	# and the second one
+	L2 = Part.Line(V4,V3)
+	S1 = Part.Shape([C1,C2,L1,L2])
+
+	W=Part.Wire(S1.Edges)
+	F=Part.Face(W)
+	P=F.makePrism(Base.Vector(0,0,5))
+
+	# add objects with the shape
+	Wire=Group.newObject("Part::Feature","Wire")
+	Wire.Shape=W
+	Face=Group.newObject("Part::Feature","Face")
+	Face.Shape=F
+	Prism=Group.newObject("Part::Feature","Extrude")
+	Prism.Shape=P
+
+	c=Part.Circle(Base.Vector(0,0,-1),Base.Vector(0,0,1),2.0)
+	w=Part.Wire(c.toShape())
+	f=Part.Face(w)
+	p=f.makePrism(Base.Vector(0,0,7))
+	P=P.cut(p)
+
+	# add first borer
+	Bore1=Group.newObject("Part::Feature","Borer_1")
+	Bore1.Shape=p
+	Hole1=Group.newObject("Part::Feature","Borer_Hole1")
+	Hole1.Shape=P
+
+	c=Part.Circle(Base.Vector(0,-11,2.5),Base.Vector(0,1,0),1.0)
+	w=Part.Wire(c.toShape())
+	f=Part.Face(w)
+	p=f.makePrism(Base.Vector(0,22,0))
+	P=P.cut(p)
+
+	# add second borer
+	Bore2=Group.newObject("Part::Feature","Borer_2")
+	Bore2.Shape=p
+	Hole2=Group.newObject("Part::Feature","Borer_Hole2")
+	Hole2.Shape=P
+
+	App.ActiveDocument.recompute()
+
+	# hide all objets except of the final one
+	Gui.ActiveDocument.getObject(Wire.Name).hide()
+	Gui.ActiveDocument.getObject(Face.Name).hide()
+	Gui.ActiveDocument.getObject(Prism.Name).hide()
+	Gui.ActiveDocument.getObject(Bore1.Name).hide()
+	Gui.ActiveDocument.getObject(Hole1.Name).hide()
+	Gui.ActiveDocument.getObject(Bore2.Name).hide()
+	Gui.ActiveDocument.ActiveView.fitAll()
