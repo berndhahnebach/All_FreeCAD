dabeb182cbc7a765b80828b95dcc69f35aee6bb5
wmayer <wmayer@users.sourceforge.net>
2008-08-03 20:16:55 +0000

    + make new release

--- src/Build/Version.h
+++ src/Build/Version.h
@@ -4,16 +4,16 @@
 #define FCVersionMinor "7"
 #define FCVersionName  "Bespin"
 
-#define FCRevision      "1342"      //Highest committed revision number
-#define FCRevisionDate  "2008/05/19 11:25:16"     //Date of highest committed revision
-#define FCRevisionRange "1342"    //Update revision range
+#define FCRevision      "1516"      //Highest committed revision number
+#define FCRevisionDate  "2008/08/03 21:19:00"     //Date of highest committed revision
+#define FCRevisionRange "1516"    //Update revision range
 #define FCRepositoryURL "https://free-cad.svn.sourceforge.net/svnroot/free-cad/trunk"      //Repository URL of the working copy
-#define FCCurrentDateT  "2008/05/19 23:39:47"      //Current system date & time
+#define FCCurrentDateT  "2008/08/03 21:19:09"      //Current system date & time
 
 //Placeholders of the form "$WCxxx?TrueText:FalseText$" are replaced with
 //TrueText if the tested condition is true, and FalseText if false.
 
-#define FCScrClean      "Src modified"     //True if local modifications found
+#define FCScrClean      "Src not modified"     //True if local modifications found
 #define FCScrMixed      "Src not mixed"          //True if mixed update revisions found
 
 
