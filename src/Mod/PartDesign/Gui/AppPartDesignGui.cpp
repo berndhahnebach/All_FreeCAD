2745ff3656662b5ed62ab4bb6eec779b36bcd3f6
jriegel <FreeCAD@juergen-riegel.net>
2008-08-22 14:29:44 +0000

    + add PartDesign module

---
+++ src/Mod/PartDesign/Gui/AppPartDesignGui.cpp
@@ -0,0 +1,69 @@
+/***************************************************************************
+ *   Copyright (c) Jürgen Riegel          (juergen.riegel@web.de) 2007     *
+ *                                                                         *
+ *   This file is part of the FreeCAD CAx development system.              *
+ *                                                                         *
+ *   This library is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU Library General Public           *
+ *   License as published by the Free Software Foundation; either          *
+ *   version 2 of the License, or (at your option) any later version.      *
+ *                                                                         *
+ *   This library  is distributed in the hope that it will be useful,      *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU Library General Public License for more details.                  *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this library; see the file COPYING.LIB. If not,    *
+ *   write to the Free Software Foundation, Inc., 59 Temple Place,         *
+ *   Suite 330, Boston, MA  02111-1307, USA                                *
+ *                                                                         *
+ ***************************************************************************/
+
+
+#include "PreCompiled.h"
+#ifndef _PreComp_
+# include <Python.h>
+#endif
+
+#include <Base/Console.h>
+#include <Gui/Application.h>
+#include <Gui/Language/Translator.h>
+#include "Workbench.h"
+//#include "resources/qrc_PartDesign.cpp"
+
+// use a different name to CreateCommand()
+void CreatePartDesignCommands(void);
+
+void loadPartDesignResource()
+{
+    // add resources and reloads the translators
+    Q_INIT_RESOURCE(PartDesign);
+    Gui::Translator::instance()->refresh();
+}
+
+/* registration table  */
+extern struct PyMethodDef PartDesignGui_Import_methods[];
+
+
+/* Python entry */
+extern "C" {
+void PartDesignGuiExport initPartDesignGui()
+{
+    if (!Gui::Application::Instance) {
+        PyErr_SetString(PyExc_ImportError, "Cannot load Gui module in console application.");
+        return;
+    }
+
+    (void) Py_InitModule("PartDesignGui", PartDesignGui_Import_methods);   /* mod name, table ptr */
+    Base::Console().Log("Loading GUI of PartDesign module... done\n");
+
+    // instanciating the commands
+    CreatePartDesignCommands();
+    PartDesignGui::Workbench::init();
+
+     // add resources and reloads the translators
+    loadPartDesignResource();
+}
+
+} // extern "C" {
