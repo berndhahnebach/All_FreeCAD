de9113d436601e52e54ba28e9546116e66b7ef9f
jriegel <FreeCAD@juergen-riegel.net>
2008-08-22 14:32:49 +0000

    + add Complete module

---
+++ src/Mod/Complete/App/AppComplete.cpp
@@ -0,0 +1,51 @@
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *   for detail see the LICENCE text file.                                 *
+ *   Jürgen Riegel 2007                                                    *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "PreCompiled.h"
+#ifndef _PreComp_
+# include <Python.h>
+#endif
+
+#include <Base/Console.h>
+#include <Base/Interpreter.h>
+ 
+
+extern struct PyMethodDef Complete_methods[];
+
+PyDoc_STRVAR(module_Complete_doc,
+"This module is the Complete module.");
+
+
+/* Python entry */
+extern "C" {
+void AppCompleteExport initComplete()
+{
+    // load dependend module
+    try {
+        Base::Interpreter().loadModule("Part");
+        //Base::Interpreter().loadModule("Mesh");
+    }
+    catch(const Base::Exception& e) {
+        PyErr_SetString(PyExc_ImportError, e.what());
+        return;
+    }
+    Py_InitModule3("Complete", Complete_methods, module_Complete_doc);   /* mod name, table ptr */
+    Base::Console().Log("Loading Complete module... done\n");
+
+
+    // NOTE: To finish the initialization of our own type objects we must
+    // call PyType_Ready, otherwise we run into a segmentation fault, later on.
+    // This function is responsible for adding inherited slots from a type's base class.
+ 
+    //Complete::FeatureViewPart        ::init();
+}
+
+} // extern "C"
---
+++ src/Mod/Complete/App/AppCompletePy.cpp
@@ -0,0 +1,108 @@
+/***************************************************************************
+ *   Copyright (c) Jürgen Riegel          (juergen.riegel@web.de) 2002     *
+ *                                                                         *
+ *   This file is part of the FreeCAD CAx development system.              *
+ *                                                                         *
+ *   This library is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU Library General Public           *
+ *   License as published by the Free Software Foundation; either          *
+ *   version 2 of the License, or (at your option) any later version.      *
+ *                                                                         *
+ *   This library  is distributed in the hope that it will be useful,      *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU Library General Public License for more details.                  *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this library; see the file COPYING.LIB. If not,    *
+ *   write to the Free Software Foundation, Inc., 59 Temple Place,         *
+ *   Suite 330, Boston, MA  02111-1307, USA                                *
+ *                                                                         *
+ ***************************************************************************/
+ 
+#include "PreCompiled.h"
+#ifndef _PreComp_
+# include <BRepPrimAPI_MakeBox.hxx>
+# include <TopoDS_Face.hxx>
+# include <Geom_Plane.hxx>
+# include <Handle_Geom_Plane.hxx>
+#endif
+
+#include <stdio.h>
+
+# if defined (_POSIX_C_SOURCE)
+#   undef  _POSIX_C_SOURCE
+# endif // (re-)defined in pyconfig.h
+#include <Python.h>
+
+#include <Base/Console.h>
+#include <Base/PyObjectBase.h>
+#include <Base/Exception.h>
+#include <Base/FileInfo.h>
+#include <App/Application.h>
+#include <App/Document.h>
+
+// Things from the part module
+#include <Mod/Part/App/TopoShape.h>
+#include <Mod/Part/App/TopoShapePy.h>
+
+#include <Geom_BSplineSurface.hxx>
+#include <Geom_OffsetSurface.hxx>
+#include <GeomAPI_PointsToBSplineSurface.hxx>
+#include <BRepBuilderAPI_MakeFace.hxx>
+#include <TColgp_HArray2OfPnt.hxx>
+#include <TColStd_Array1OfReal.hxx>
+#include <TColStd_Array1OfInteger.hxx>
+#include <BRepBuilderAPI_MakeFace.hxx>
+
+using Base::Console;
+using namespace Part;
+using namespace std;
+
+
+/* module functions */
+static PyObject * open(PyObject *self, PyObject *args)
+{
+    const char* Name;
+    if (!PyArg_ParseTuple(args, "s",&Name))
+        return NULL;
+
+    PY_TRY {
+    } PY_CATCH;
+
+    Py_Return;
+}
+
+/* module functions */
+static PyObject * insert(PyObject *self, PyObject *args)
+{
+    const char* Name;
+    const char* DocName;
+    if (!PyArg_ParseTuple(args, "ss",&Name,&DocName))
+        return NULL;
+
+    PY_TRY {
+    } PY_CATCH;
+
+    Py_Return;
+}
+
+/* module functions */
+static PyObject * read(PyObject *self, PyObject *args)
+{
+    const char* Name;
+    if (!PyArg_ParseTuple(args, "s",&Name))
+        return NULL;
+    PY_TRY {
+    } PY_CATCH;
+  
+    Py_Return;
+}
+
+/* registration table  */
+struct PyMethodDef Complete_methods[] = {
+    {"open"   , open,    1},
+    {"insert" , insert,  1},
+    {"read"   , read,  1},
+    {NULL, NULL}        /* end of table marker */
+};
