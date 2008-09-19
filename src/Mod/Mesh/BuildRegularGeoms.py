68ce40248004e8360b70f95e5f9d403c249c982d
wmayer <wmayer>
2008-09-19 13:17:21 +0000

    + bug fixed: use GIL locker when accessing python functions

--- src/Mod/Mesh/BuildRegularGeoms.py
+++ src/Mod/Mesh/BuildRegularGeoms.py
@@ -3,7 +3,7 @@
 Return value are list of vectors, 3 vectors define a facet.
 
 Copyright (c) Berthold Grupp          2005
-License: LGPL                                                                  *
+License: LGPL
 
 Sample code for creating a mesh:
 	facets = Cube(3.0, 4.0, 5.0)
