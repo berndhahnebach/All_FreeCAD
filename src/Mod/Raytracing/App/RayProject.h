474753188c30ed8670d51fe7196d0d148c0fd3e9
jriegel <FreeCAD@juergen-riegel.net>
2008-12-01 18:27:12 +0000

    + add missing files

---
+++ src/Mod/Raytracing/App/RayProject.h
@@ -0,0 +1,74 @@
+/***************************************************************************
+ *   Copyright (c) Jürgen Riegel          (juergen.riegel@web.de) 2008     *
+ *                                                                         *
+ *   This file is part of the FreeCAD CAx development system.              *
+ *                                                                         *
+ *   This library is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU Library General Public           *
+ *   License as published by the Free Software Foundation; either          *
+ *   version 2 of the License, or (at your option) any later version.      *
+ *                                                                         *
+ *   This library  is distributed in the hope that it will be useful,      *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU Library General Public License for more details.                  *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this library; see the file COPYING.LIB. If not,    *
+ *   write to the Free Software Foundation, Inc., 59 Temple Place,         *
+ *   Suite 330, Boston, MA  02111-1307, USA                                *
+ *                                                                         *
+ ***************************************************************************/
+
+ 
+
+
+#ifndef _RayFeature_h_
+#define _RayFeature_h_
+
+//#include <Base/Factory.h>
+//#include <Base/PyExport.h>
+
+#include <App/DocumentObjectGroup.h>
+
+class PyObjectBase;
+class FeaturePy;
+
+namespace Raytracing
+{
+
+class Property;
+
+/** Base class of all Feature classes in FreeCAD
+ */
+//class RayFeature: public Part::PartFeature
+class RayProject: public App::DocumentObjectGroup
+{
+    PROPERTY_HEADER(Raytracing::RayProject);
+
+public:
+	/// Constructor
+	RayProject(void);
+
+
+
+   /** @name methods overide Feature */
+    //@{
+    /// recalculate the Feature
+    App::DocumentObjectExecReturn *execute(void);
+    short mustExecute() const;
+    /// returns the type name of the ViewProvider
+    const char* getViewProviderName(void) const { 
+        return "RaytracingGui::ViewProviderRayFeature"; 
+    }
+  //@}
+
+
+};
+
+
+} //namespace Raytracing
+
+
+
+#endif //_RayFeature_h_
