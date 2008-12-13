89e8b2a02536a1e53ef2970b27453e44e6903941
wmayer <wmayer>
2008-12-13 15:24:59 +0000

    + fix bug #2412579 GUI doesn't load modules correctly

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 # We almost need to specify the switches for the OpenCascade library to make
 # configure running successfully. 
-CASROOT=${CASROOT:-/opt/OpenCASCADE6.2.0/ros}
-./configure CXXFLAGS="-fno-strict-aliasing" LDFLAGS="-Wl,-z,defs" --with-occ-include=$CASROOT/inc --with-occ-lib=$CASROOT/Linux/lib
+# CASROOT=${CASROOT:-/opt/OpenCASCADE6.2.0/ros}
+#./configure CXXFLAGS="-fno-strict-aliasing" LDFLAGS="-Wl,-z,defs" --with-occ-include=$CASROOT/inc --with-occ-lib=$CASROOT/Linux/lib
+./configure CXXFLAGS="-fno-strict-aliasing" LDFLAGS="-Wl,-z,defs"
 
