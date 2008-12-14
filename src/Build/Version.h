f6a7fdbdd758e624b66063748852518aa1580cab
wmayer <wmayer>
2008-12-14 14:24:39 +0000

    + prepare for new release

--- src/Build/Version.h
+++ src/Build/Version.h
@@ -4,11 +4,11 @@
 #define FCVersionMinor "7"
 #define FCVersionName  "Bespin"
 
-#define FCRevision      "1651"      //Highest committed revision number
-#define FCRevisionDate  "2008/10/08 13:35:10"     //Date of highest committed revision
-#define FCRevisionRange "1651"    //Update revision range
+#define FCRevision      "1786"      //Highest committed revision number
+#define FCRevisionDate  "2008/12/14 15:04:04"     //Date of highest committed revision
+#define FCRevisionRange "1786"    //Update revision range
 #define FCRepositoryURL "https://free-cad.svn.sourceforge.net/svnroot/free-cad/trunk"      //Repository URL of the working copy
-#define FCCurrentDateT  "2008/10/08 22:02:12"      //Current system date & time
+#define FCCurrentDateT  "2008/12/14 15:20:12"      //Current system date & time
 
 //Placeholders of the form "$WCxxx?TrueText:FalseText$" are replaced with
 //TrueText if the tested condition is true, and FalseText if false.
