201e8a982839c746edc72b07d0971fcd5fe13c68
wmayer <wmayer>
2009-01-24 10:16:46 +0000

    + apply patch #2523938

--- src/3rdParty/OCCAdaptMesh/src/TriangleAdapt/triangleAdapt.c
+++ src/3rdParty/OCCAdaptMesh/src/TriangleAdapt/triangleAdapt.c
@@ -1347,7 +1347,7 @@ int triunsuitable(vertex triorg, vertex tridest, vertex triapex, REAL area);
 #endif
 
 /* Open CASCADE Support : Use Open CASCADE exceptions */
-#if (defined(_MSC_VER) && (_MSC_VER < 1500))
+#if (defined(_MSC_VER) && (_MSC_VER < 1600))
 extern "C" void occinternalerrorAdapt ();
 #else
 extern void occinternalerrorAdapt ();
