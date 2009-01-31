56261ce185657e6f04b30b42749fb6d816171ff4
wmayer <wmayer@users.sourceforge.net>
2009-01-31 18:30:16 +0000

    + experiment with threads

--- src/Mod/Sandbox/App/DocumentProtector.cpp
+++ src/Mod/Sandbox/App/DocumentProtector.cpp
@@ -24,6 +24,7 @@
 #include "PreCompiled.h"
 #ifndef _PreComp_
 # include <QCoreApplication>
+# include <QObject>
 # include <QEvent>
 # include <QMutex>
 # include <QMutexLocker>
@@ -33,6 +34,7 @@
 #include "DocumentProtector.h"
 
 #include <App/Document.h>
+#include <Base/Exception.h>
 
 using namespace Sandbox;
 
@@ -91,6 +93,28 @@ public:
     DocumentProtector* dp;
 };
 
+class DocumentReceiver : public QObject
+{
+
+public:
+    DocumentReceiver(QObject *parent = 0) : QObject(parent)
+    {
+    }
+    ~DocumentReceiver()
+    {
+    }
+
+    static DocumentReceiver *globalInstance();
+
+protected:
+    void customEvent(QEvent*);
+    void wakeupThread();
+    void postEventAndWait(QEvent*);
+
+    // friends
+    friend class DocumentProtector;
+};
+
 Q_GLOBAL_STATIC(DocumentReceiver, theInstance)
 
 DocumentReceiver *DocumentReceiver::globalInstance()
@@ -98,14 +122,22 @@ DocumentReceiver *DocumentReceiver::globalInstance()
     return theInstance();
 }
 
-DocumentReceiver* dp = DocumentReceiver::globalInstance();
-
 // Posts an event and waits for it to finish processing
 void DocumentReceiver::postEventAndWait(QEvent* e)
 {
-    QMutexLocker lock(&gProtectorWaitMutex);
-    QCoreApplication::postEvent(this, e);
-    gProtectorEventProcessed.wait(&gProtectorWaitMutex); // waits for wait to finish
+    QThread *currentThread = QThread::currentThread();
+    QThread *thr = this->thread(); // this is the main thread
+
+    if (currentThread == thr) {
+        // we're in the main thread
+        QCoreApplication::sendEvent(this, e);
+        delete e;
+    }
+    else {
+        QMutexLocker lock(&gProtectorWaitMutex);
+        QCoreApplication::postEvent(this, e);
+        gProtectorEventProcessed.wait(&gProtectorWaitMutex); // waits for wait to finish
+    }
 }
 
 // Wake up the waiting automation thread
@@ -120,18 +152,18 @@ void DocumentReceiver::customEvent(QEvent* e)
     if ((int)e->type() == QT_CUSTOM_EVENT_ADD_OBJECT) {
         CustomAddObjectEvent *ev = static_cast<CustomAddObjectEvent*>(e);
         DocumentProtector* dp = ev->dp;
-        dp->obj = dp->doc->addObject(ev->type.c_str(), ev->name.c_str());
+        dp->obj = dp->getDocument()->addObject(ev->type.c_str(), ev->name.c_str());
         wakeupThread();
     }
     else if ((int)e->type() == QT_CUSTOM_EVENT_REM_OBJECT) {
         CustomRemoveObjectEvent *ev = static_cast<CustomRemoveObjectEvent*>(e);
         DocumentProtector* dp = ev->dp;
-        dp->doc->remObject(ev->name.c_str());
+        dp->getDocument()->remObject(ev->name.c_str());
         wakeupThread();
     }
     else if ((int)e->type() == QT_CUSTOM_EVENT_RECOMPUTE) {
         CustomRecomputeEvent *ev = static_cast<CustomRecomputeEvent*>(e);
-        ev->dp->doc->recompute();
+        ev->dp->getDocument()->recompute();
         wakeupThread();
     }
 }
@@ -139,7 +171,7 @@ void DocumentReceiver::customEvent(QEvent* e)
 }
 
 DocumentProtector::DocumentProtector(App::Document* doc)
-  : doc(doc), obj(0)
+  : App::DocumentObserver(doc), obj(0)
 {
 }
 
@@ -147,17 +179,55 @@ DocumentProtector::~DocumentProtector()
 {
 }
 
+void DocumentProtector::init()
+{
+    // this method must be called somewhere from the main thread
+    (void)DocumentReceiver::globalInstance();
+}
+
+void DocumentProtector::slotCreatedDocument(App::Document& Doc)
+{
+}
+
+void DocumentProtector::slotDeletedDocument(App::Document& Doc)
+{
+    if (&Doc == getDocument()) {
+        this->detachDocument();
+    }
+}
+
+void DocumentProtector::slotCreatedObject(App::DocumentObject& Obj)
+{
+}
+
+void DocumentProtector::slotDeletedObject(App::DocumentObject& Obj)
+{
+}
+
+void DocumentProtector::slotChangedObject(App::DocumentObject& Obj, App::Property& Prop)
+{
+}
+
+void DocumentProtector::validate()
+{
+    if (!this->getDocument())
+        throw Base::Exception("Handled document is null");
+}
+
 App::DocumentObject *DocumentProtector::addObject(const std::string& type, const std::string& name)
 {
+    validate();
     DocumentReceiver::globalInstance()->postEventAndWait(new CustomAddObjectEvent(this, type, name));
     return this->obj;
 }
 
 void DocumentProtector::removeObject(const std::string& name)
 {
+    validate();
 }
 
 void DocumentProtector::recompute()
 {
+    validate();
     DocumentReceiver::globalInstance()->postEventAndWait(new CustomRecomputeEvent(this));
 }
