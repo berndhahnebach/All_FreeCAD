d7349acd358950f6d8572daa1512143b12f25fdb
wmayer <wmayer>
2009-04-29 09:39:56 +0000

    + move revolve() to TopoShapePy + add cast_to_shape method to Part module

--- src/Mod/Part/App/TopoShapeWirePyImp.cpp
+++ src/Mod/Part/App/TopoShapeWirePyImp.cpp
@@ -130,28 +130,6 @@ int TopoShapeWirePy::PyInit(PyObject* args, PyObject* /*kwd*/)
     return -1;
 }
 
-PyObject* TopoShapeWirePy::revolve(PyObject *args)
-{
-    PyObject *pPos,*pDir;
-    double d=2.0*Standard_PI;
-    if (PyArg_ParseTuple(args, "O!O!|d", &(Base::VectorPy::Type), &pPos, &(Base::VectorPy::Type), &pDir,&d)) {
-        try {
-            Base::Vector3d pos = static_cast<Base::VectorPy*>(pPos)->value();
-            Base::Vector3d dir = static_cast<Base::VectorPy*>(pDir)->value();
-            TopoDS_Shape shape = this->getTopoShapePtr()->revolve(
-                gp_Ax1(gp_Pnt(pos.x,pos.y,pos.z), gp_Dir(dir.x,dir.y,dir.z)),d);
-            return new TopoShapeShellPy(new TopoShape(shape));
-        }
-        catch (Standard_Failure) {
-            Handle_Standard_Failure e = Standard_Failure::Caught();
-            PyErr_SetString(PyExc_Exception, e->GetMessageString());
-            return 0;
-        }
-    }
-
-    return 0;
-}
-
 Py::Object TopoShapeWirePy::getCenterOfMass(void) const
 {
     GProp_GProps props;
