ddcbf494ad5315319a413e3c4dea895580b28a25
jriegel <FreeCAD@juergen-riegel.net>
2009-05-01 10:03:08 +0000

    + add some modules for VRML and the new combi tree view

---
+++ src/Gui/ViewProviderVRMLObject .cpp
@@ -0,0 +1,113 @@
+/***************************************************************************
+ *   Copyright (c) 2007 Werner Mayer <werner.wm.mayer@gmx.de>              *
+ *                                                                         *
+ *   This file is part of the FreeCAD CAx development system.              *
+ *                                                                         *
+ *   This library is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU Library General Public           *
+ *   License as published by the Free Software Foundation; either          *
+ *   version 2 of the License, or (at your option) any later version.      *
+ *                                                                         *
+ *   This library  is distributed in the hope that it will be useful,      *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU Library General Public License for more details.                  *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this library; see the file COPYING.LIB. If not,    *
+ *   write to the Free Software Foundation, Inc., 59 Temple Place,         *
+ *   Suite 330, Boston, MA  02111-1307, USA                                *
+ *                                                                         *
+ ***************************************************************************/
+
+
+#include "PreCompiled.h"
+
+#ifndef _PreComp_
+# include <Inventor/SoDB.h>
+# include <Inventor/SoInput.h>
+# include <Inventor/nodes/SoSeparator.h>
+#endif
+
+#include "ViewProviderVRMLObject.h"
+#include <App/InventorObject.h>
+#include <Base/FileInfo.h>
+#include <Base/Stream.h>
+#include <sstream>
+
+using namespace Gui;
+
+PROPERTY_SOURCE(Gui::ViewProviderVRMLObject, Gui::ViewProviderDocumentObject)
+
+ViewProviderVRMLObject::ViewProviderVRMLObject()
+{
+    pcBuffer = new SoSeparator();
+    pcBuffer->ref();
+    pcFile = new SoSeparator();
+    pcFile->ref();
+}
+
+ViewProviderVRMLObject::~ViewProviderVRMLObject()
+{
+    pcBuffer->unref();
+    pcFile->unref();
+}
+
+void ViewProviderVRMLObject::attach(App::DocumentObject *pcObj)
+{
+    ViewProviderDocumentObject::attach(pcObj);
+    SoGroup* pcFileBuf = new SoGroup();
+    pcFileBuf->addChild(pcBuffer);
+    pcFileBuf->addChild(pcFile);
+    addDisplayMaskMode(pcFileBuf, "FileBuffer");
+    addDisplayMaskMode(pcBuffer, "Buffer");
+    addDisplayMaskMode(pcFile, "File");
+}
+
+void ViewProviderVRMLObject::setDisplayMode(const char* ModeName)
+{
+  if ( strcmp("File+Buffer",ModeName)==0 )
+    setDisplayMaskMode("FileBuffer");
+  else if ( strcmp("Buffer",ModeName)==0 )
+    setDisplayMaskMode("Buffer");
+  else if ( strcmp("File",ModeName)==0 )
+    setDisplayMaskMode("File");
+  ViewProviderDocumentObject::setDisplayMode( ModeName );
+}
+
+std::vector<std::string> ViewProviderVRMLObject::getDisplayModes(void) const
+{
+  std::vector<std::string> StrList;
+  StrList.push_back("File+Buffer");
+  StrList.push_back("Buffer");
+  StrList.push_back("File");
+  return StrList;
+}
+
+void ViewProviderVRMLObject::updateData(const App::Property* prop)
+{
+    App::InventorObject* ivObj = static_cast<App::InventorObject*>(pcObject);
+    if (prop == &ivObj->Buffer) {
+        // read from buffer
+        SoInput in;
+        std::string buffer = ivObj->Buffer.getValue();
+        pcBuffer->removeAllChildren();
+        if (buffer.empty()) return;
+        in.setBuffer((void *)buffer.c_str(), buffer.size());
+        SoSeparator * node = SoDB::readAll(&in);
+        if (node) pcBuffer->addChild(node);
+    }
+    if (prop == &ivObj->FileName) {
+        // read also from file
+        const char* filename = ivObj->FileName.getValue();
+        QFile file(QString::fromUtf8(filename));
+        SoInput in;
+        pcFile->removeAllChildren();
+        if (file.open(QFile::ReadOnly)) {
+            QByteArray buffer = file.readAll();
+            in.setBuffer((void *)buffer.constData(), buffer.length());
+            SoSeparator * node = SoDB::readAll(&in);
+            if (node) pcFile->addChild(node);
+        }
+    }
+}
