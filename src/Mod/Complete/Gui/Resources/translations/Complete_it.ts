d962ce679163f3dfb4f174df50ba21585bafb20e
wmayer <wmayer>
2009-05-15 16:45:17 +0000

    + support of Italian (Gui, Part, Mesh, Complete)

---
+++ src/Mod/Complete/Gui/Resources/translations/Complete_it.ts
@@ -0,0 +1,92 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>CmdCompleteConstraintAxle</name>
+    <message>
+        <source>Complete</source>
+        <translation>Completo</translation>
+    </message>
+    <message>
+        <source>Constraint Axle...</source>
+        <translation>Asse di vincolo</translation>
+    </message>
+    <message>
+        <source>set a axle constraint between two objects</source>
+        <translation>assegna una asse di vincolo tra due oggetti</translation>
+    </message>
+</context>
+<context>
+    <name>Workbench</name>
+    <message>
+        <source>&amp;File</source>
+        <translation>&amp;File</translation>
+    </message>
+    <message>
+        <source>&amp;Edit</source>
+        <translation>&amp;Modifica</translation>
+    </message>
+    <message>
+        <source>Standard views</source>
+        <translation>Viste standard</translation>
+    </message>
+    <message>
+        <source>&amp;3D View</source>
+        <translation>&amp;Vista 3D</translation>
+    </message>
+    <message>
+        <source>&amp;Zoom</source>
+        <translation>&amp;Zoom</translation>
+    </message>
+    <message>
+        <source>&amp;View</source>
+        <translation>&amp;Visualizza</translation>
+    </message>
+    <message>
+        <source>&amp;Tools</source>
+        <translation>&amp;Strumenti</translation>
+    </message>
+    <message>
+        <source>Analyze</source>
+        <translation>Analizza</translation>
+    </message>
+    <message>
+        <source>Boolean</source>
+        <translation>Booleano</translation>
+    </message>
+    <message>
+        <source>&amp;Meshes</source>
+        <translation>&amp;Griglie</translation>
+    </message>
+    <message>
+        <source>&amp;Part</source>
+        <translation>&amp;Parte</translation>
+    </message>
+    <message>
+        <source>&amp;Drawing</source>
+        <translation>&amp;Disegna</translation>
+    </message>
+    <message>
+        <source>&amp;Windows</source>
+        <translation>&amp;Finestre</translation>
+    </message>
+    <message>
+        <source>&amp;Online-help</source>
+        <translation>&amp;Aiuto in linea</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation>&amp;Aiuto</translation>
+    </message>
+    <message>
+        <source>File</source>
+        <translation>File</translation>
+    </message>
+    <message>
+        <source>Macro</source>
+        <translation>Macro</translation>
+    </message>
+    <message>
+        <source>View</source>
+        <translation>Visualizza</translation>
+    </message>
+</context>
+</TS>
