443d5b53dda9215c35f1a554e04ef33c978fdeda
wmayer <wmayer>
2009-05-23 10:02:56 +0000

    + implement TaskAppearance

--- src/Gui/TaskView/TaskAppearance.h
+++ src/Gui/TaskView/TaskAppearance.h
@@ -31,6 +31,7 @@
 #endif
 
 #include "TaskView.h"
+#include <Gui/Selection.h>
 
 
 class Ui_TaskAppearance;
@@ -40,23 +41,41 @@ class Property;
 }
 
 namespace Gui {
+class ViewProvider;
 namespace TaskView {
 
 
 
-class TaskAppearance : public TaskBox
+class TaskAppearance : public TaskBox, public Gui::SelectionSingleton::ObserverType
 {
     Q_OBJECT
 
 public:
     TaskAppearance(QWidget *parent = 0);
     ~TaskAppearance();
+    /// Observer message from the Selection
+    void OnChange(Gui::SelectionSingleton::SubjectType &rCaller,
+                  Gui::SelectionSingleton::MessageType Reason);
+
+private Q_SLOTS:
+    void on_changeMode_activated(const QString&);
+    void on_changePlot_activated(const QString&);
+    void on_spinTransparency_valueChanged(int);
+    void on_spinPointSize_valueChanged(int);
+    void on_spinLineWidth_valueChanged(int);
 
 protected:
     void changeEvent(QEvent *e);
 
 private:
-    QWidget* widget;
+    void setDisplayModes(const std::vector<Gui::ViewProvider*>&);
+    void setPointSize(const std::vector<Gui::ViewProvider*>&);
+    void setLineWidth(const std::vector<Gui::ViewProvider*>&);
+    void setTransparency(const std::vector<Gui::ViewProvider*>&);
+    std::vector<Gui::ViewProvider*> getSelection() const;
+
+private:
+    QWidget* proxy;
     Ui_TaskAppearance* ui;
 };
 
