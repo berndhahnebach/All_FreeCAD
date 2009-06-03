c471f1b4b7d535c252739478674ef7946aff4437
wmayer <wmayer>
2009-06-03 14:24:25 +0000

    + load/save data of PropertyPythonObject directly in XML file

--- src/App/PropertyPythonObject.cpp
+++ src/App/PropertyPythonObject.cpp
@@ -31,6 +31,7 @@
 #include <Base/Reader.h>
 #include <Base/Console.h>
 #include <Base/Interpreter.h>
+#include <iostream>
 
 using namespace App;
 
@@ -71,80 +72,145 @@ void PropertyPythonObject::setPyObject(PyObject * obj)
     this->object = obj;
 }
 
-void PropertyPythonObject::Save (Base::Writer &writer) const
+std::string PropertyPythonObject::toString() const
 {
-    if (writer.isForceXML()) {
-    }
-    else {
-        writer.Stream() << writer.ind() << "<Python file=\"" << 
-        writer.addFile("Callable.pyc", this) << "\"/>" << std::endl;
-    }
-}
-
-void PropertyPythonObject::Restore(Base::XMLReader &reader)
-{
-    reader.readElement("Python");
-    std::string file(reader.getAttribute("file"));
-    if (!file.empty()) {
-        reader.addFile(file.c_str(),this);
-    }
-}
-
-void PropertyPythonObject::SaveDocFile (Base::Writer &writer) const
-{
+    std::string repr;
     Base::PyGILStateLocker lock;
     try {
-        Py::Module pickle(PyImport_ImportModule("pickle"),true);
+        Py::Module pickle(PyImport_ImportModule("cPickle"),true);
         Py::Callable method(pickle.getAttr(std::string("dumps")));
         Py::Tuple args(1);
         args.setItem(0, this->object);
         Py::Object res = method.apply(args);
         Py::String str(res);
-        std::string buffer = str.as_std_string();
-        for (std::string::iterator it = buffer.begin(); it != buffer.end(); ++it)
-            writer.Stream().put(*it);
+        repr = str.as_std_string();
     }
     catch (Py::Exception&) {
         Base::PyException e; // extract the Python error text
-        Base::Console().Warning("PropertyPythonObject::SaveDocFile: %s\n", e.what());
+        Base::Console().Warning("PropertyPythonObject::toString: %s\n", e.what());
     }
+
+    return repr;
 }
 
-void PropertyPythonObject::RestoreDocFile(Base::Reader &reader)
+void PropertyPythonObject::fromString(const std::string& repr)
 {
-    aboutToSetValue();
     Base::PyGILStateLocker lock;
     try {
-        std::string buffer;
-        char c;
-        while (reader.get(c)) {
-            buffer.push_back(c);
-        }
-
-        Py::Module pickle(PyImport_ImportModule("pickle"),true);
+        Py::Module pickle(PyImport_ImportModule("cPickle"),true);
         Py::Callable method(pickle.getAttr(std::string("loads")));
         Py::Tuple args(1);
-        args.setItem(0, Py::String(buffer));
+        args.setItem(0, Py::String(repr));
         Py::Object res = method.apply(args);
         this->object = res;
     }
     catch (Py::Exception&) {
         Base::PyException e; // extract the Python error text
-        Base::Console().Warning("PropertyPythonObject::RestoreDocFile: %s\n", e.what());
+        Base::Console().Warning("PropertyPythonObject::fromString: %s\n", e.what());
     }
+}
+
+std::string PropertyPythonObject::encodeValue(const std::string& str) const
+{
+    std::string tmp;
+    for (std::string::const_iterator it = str.begin(); it != str.end(); ++it) {
+        if (*it == '<')
+            tmp += "&lt;";
+        else if (*it == '"')
+            tmp += "&quot;";
+        else if (*it == '&')
+            tmp += "&amp;";
+        else if (*it == '>')
+            tmp += "&gt";
+        else if (*it == '\n')
+            tmp += "\\n";
+        else
+            tmp += *it;
+    }
+
+    return tmp;
+}
+
+std::string PropertyPythonObject::decodeValue(const std::string& str) const
+{
+    std::string tmp;
+    for (std::string::const_iterator it = str.begin(); it != str.end(); ++it) {
+        if (*it == '\\') {
+            ++it;
+            if (it != str.end() && *it == 'n') {
+                tmp += '\n';
+            }
+        }
+        else
+            tmp += *it;
+    }
+
+    return tmp;
+}
+
+void PropertyPythonObject::Save (Base::Writer &writer) const
+{
+    //if (writer.isForceXML()) {
+        std::string val = encodeValue(this->toString());
+        writer.Stream() << writer.ind() << "<Python value=\"" <<  val <<"\"/>" << std::endl;
+    //}
+    //else {
+    //    writer.Stream() << writer.ind() << "<Python file=\"" << 
+    //    writer.addFile("pickle", this) << "\"/>" << std::endl;
+    //}
+}
+
+void PropertyPythonObject::Restore(Base::XMLReader &reader)
+{
+    reader.readElement("Python");
+    if (reader.hasAttribute("file")) {
+        std::string file(reader.getAttribute("file"));
+        reader.addFile(file.c_str(),this);
+    }
+    else {
+        std::string buffer = decodeValue(reader.getAttribute("value"));
+        aboutToSetValue();
+        this->fromString(buffer);
+        hasSetValue();
+    }
+}
+
+void PropertyPythonObject::SaveDocFile (Base::Writer &writer) const
+{
+    std::string buffer = this->toString();
+    for (std::string::iterator it = buffer.begin(); it != buffer.end(); ++it)
+        writer.Stream().put(*it);
+}
+
+void PropertyPythonObject::RestoreDocFile(Base::Reader &reader)
+{
+    aboutToSetValue();
+    std::string buffer;
+    char c;
+    while (reader.get(c)) {
+        buffer.push_back(c);
+    }
+    this->fromString(buffer);
     hasSetValue();
 }
 
 unsigned int PropertyPythonObject::getMemSize (void) const
 {
-    return 0;
+    return sizeof(Py::Object);
 }
 
 Property *PropertyPythonObject::Copy(void) const
 {
-    return 0;
+    PropertyPythonObject *p = new PropertyPythonObject();
+    p->object = this->object;
+    return p;
 }
 
 void PropertyPythonObject::Paste(const Property &from)
 {
+    if (from.getTypeId() == PropertyPythonObject::getClassTypeId()) {
+        aboutToSetValue();
+        this->object = static_cast<const PropertyPythonObject&>(from).object;
+        hasSetValue();
+    }
 }
