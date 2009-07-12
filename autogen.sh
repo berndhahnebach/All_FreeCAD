bfade172d18932f8d897c8a3721c6afc398ab15e
wmayer <wmayer@users.sourceforge.net>
2009-07-12 20:24:08 +0000

    + shell script for building debian packages

--- autogen.sh
+++ autogen.sh
@@ -2,9 +2,27 @@
 # autogen.sh
 # Run this script to generate all initial makefiles.
 
-# Create Version.h with Python script
-#echo "creating Version.h"
-#python src/Tools/SubWCRev.py
+# Get version and revision number
+MAJ=0
+MIN=9
+REV=0
+
+if svn --xml info >/dev/null 2>&1; then
+	REV=`svn --xml info | tr -d '\r\n' | sed -e 's/.*<commit.*revision="\([0-9]*\)".*<\/commit>.*/\1/'`
+elif svn --version --quiet >/dev/null 2>&1; then
+	REV=`svn info | grep "^Revision:" | cut -d" " -f2`
+fi
+
+# if revision.m4 does not exist, create it
+REV_FILE=revision.m4
+if [ -f $REV_FILE ]; then
+	echo "$REV_FILE found"
+else
+	echo "m4_define([FREECAD_MAJOR], $MAJ)" > $REV_FILE
+	echo "m4_define([FREECAD_MINOR], $MIN)" >> $REV_FILE
+	echo "m4_define([FREECAD_MICRO], $REV)" >> $REV_FILE
+	echo "$REV_FILE created"
+fi
 
 echo "calling libtoolize"
 libtoolize --force --copy
