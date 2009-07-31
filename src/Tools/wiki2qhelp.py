b701d4f5be2e17f10f012b8d95e54e1560ee106b
yorikvanhavre <yorikvanhavre>
2009-07-31 22:38:56 +0000

    + wiki2qhelp now correctly downloads and cleans the whole wiki

--- src/Tools/wiki2qhelp.py
+++ src/Tools/wiki2qhelp.py
@@ -30,23 +30,28 @@ __url__ = "http://yorik.orgfree.com"
 Caution: work in progress!
 
 This script retrieves the contents of a wiki site and saves it locally,
-To be 
+then calls qt help compiler to produce a qhelp-assistant help file.
+The script can be called without arguments, it will then use the default
+url below, or by passing it an url and optionally a TOC name.
 """
 
 #    CONFIGURATION       #################################################
 
-WEBROOT = "http://sourceforge.net/apps/mediawiki/free-cad/index.php?title="
-INDEX = "Online_Help_Toc"
-TMPFOLDER = "."
-NORETRIEVE = ['Manual']
+DEFAULTURL = "http://sourceforge.net/apps/mediawiki/free-cad" #default URL if no URL is passed
+INDEX = "Online_Help_Toc" # the start page from where to crawl the wiki
+TMPFOLDER = "./tmp" # where the temporary stuff will be downloaded
+NORETRIEVE = ['Manual','Developer_hub','Power_users_hub','Users_hub','Source_documentation', 'User_hub','Main_Page','About_this_site'] # pages that won't be fetched (kept online)
 
 #    END CONFIGURATION      ##############################################
 
 
-import os, re
+import sys, os, re
 from urllib2 import urlopen, HTTPError
 
-css = """/* Basic CSS for offline rendering */
+URL = DEFAULTURL
+wikiindex = "/index.php?title="
+processed = []
+css = """/* Basic CSS for offline wiki rendering */
 
 body {
   font-family: Arial,Helvetica,sans-serif;
@@ -64,6 +69,13 @@ h1 {
   -webkit-border-radius: 5px;
   }
 
+pre {
+  border: 1px dashed #333333;
+  text-align: left;
+  background: #EEEEEE;
+  padding: 5px;
+  }
+
 a:link, a:visited {
   font-weight: bold;
   text-decoration: none;
@@ -79,12 +91,59 @@ a:hover {
   color: #333333;
   border-top: 1px solid #333333;
   }
+
+.wikitable #toc {
+  font-size: 0.8em;
+  }
+
+.external {
+  color: black !important;
+  font-weight: normal !important;
+  }
+
+#toc {
+  display: none;
+  }
+
 """
 
-def clean(html):
+def crawl(site=DEFAULTURL):
+    "downloads an entire wiki site"
+    URL = site
+    print "crawling ", URL, ", saving in ", TMPFOLDER
+    if not os.path.isdir(TMPFOLDER): os.mkdir(TMPFOLDER)
+    file = open(TMPFOLDER + os.sep + "wiki.css",'wb')
+    file.write(css)
+    file.close()
+    todolist = []
+    count = 1
+    indexpages = get(INDEX)
+    todolist.extend(indexpages)
+    while todolist:
+        targetpage = todolist.pop()
+        if not targetpage in NORETRIEVE:
+            print count, ": Fetching ", targetpage
+            pages = get(targetpage)
+            count += 1
+            processed.append(targetpage)
+            for p in pages:
+                if (not (p in todolist)) and (not (p in processed)):
+                    todolist.append(p)
+    print "Done. Fetched ", count, " pages"
+
+def get(page):
+    "downloads a single page, returns the other pages it links to"
+    html = fetchpage(page)
+    html = cleanhtml(html)
+    pages = getlinks(html)
+    html = cleanlinks(html,pages)
+    html = cleanimagelinks(html)
+    output(html,page)
+    return pages
+
+def cleanhtml(html):
     "cleans given html code from dirty script stuff"
-    html = html.replace('\n',' ')
-    html = html.replace('\t','')
+    html = html.replace('\n','Wlinebreak') # removing linebreaks
     html = re.compile('(.*)<div[^>]+column-content+[^>]+>').sub('',html) # stripping before content
     html = re.compile('<div[^>]+column-one+[^>]+>.*').sub('',html) # stripping after content
     html = re.compile('<!--[^>]+-->').sub('',html) # removing comment tags
@@ -92,15 +151,105 @@ def clean(html):
     html = re.compile('<!--\[if[^>]*>.*?endif\]-->').sub('',html) # removing IE tags
     html = re.compile('<div id="jump-to-nav"[^>]*>.*?</div>').sub('',html) # removing nav div
     html = re.compile('<h3 id="siteSub"[^>]*>.*?</h3>').sub('',html) # removing print subtitle
+    html = re.compile('Retrieved from').sub('Online version:',html) # changing online title
+    html = re.compile('<div id="mw-normal-catlinks[^>]>.*?</div>').sub('',html) # removing catlinks
+    html = re.compile('<div class="NavHead.*?</div>').sub('',html) # removing nav stuff
+    html = re.compile('<div class="NavContent.*?</div>').sub('',html) # removing nav stuff
+    html = re.compile('<div class="NavEnd.*?</div>').sub('',html) # removing nav stuff
+    html = re.compile('Wlinebreak').sub('\n',html) # restoring original linebreaks
     return html
     
+def getlinks(html):
+    "returns a list of wikipage links in html file"
+    links = re.findall('<a[^>]*>.*?</a>',html)
+    pages = []
+    for l in links:
+        # rg = re.findall('php\?title=(.*)\" title',l)
+        rg = re.findall('href=.*?php\?title=(.*?)"',l)
+        if rg:
+            rg = rg[0]
+            if "#" in rg:
+                rg = rg.split('#')[0]
+            if ":" in rg:
+                NORETRIEVE.append(rg)
+            if ";" in rg:
+                NORETRIEVE.append(rg)
+            if "&" in rg:
+                NORETRIEVE.append(rg)
+            pages.append(rg)
+    return pages
+
+def getimagelinks(html):
+    "returns a list of image links found in an html file"
+    return re.findall('<img.*?src="(.*?)"',html)
+
+def cleanlinks(html, pages=None):
+    "cleans page links found in html"
+    if not pages: pages = getlinks(html)
+    for page in pages:
+        if  page in NORETRIEVE:
+            output = 'href="' + URL + wikiindex + page + '"'
+        else:
+            output = 'href="' + page + '.html"'
+        html = re.compile('href="[^"]+' + page + '"').sub(output,html)
+    return html
+
+def cleanimagelinks(html,links=None):
+    "cleans image links in given html"
+    if not links: links = getimagelinks(html)
+    if links:
+        for l in links:
+            nl = re.findall('.*/(.*)',l)
+            if nl: html = html.replace(l,nl[0])
+            fetchimage(l)
+    return html
+
+def fetchpage(page):
+    "retrieves given page from the wiki"
+    try:
+        html = (urlopen(URL + wikiindex + page).read())
+        return html
+    except HTTPError:
+        print 'Error: unable to fetch page ' + page
+
+def fetchimage(imagelink):
+    "retrieves given image from the wiki and saves it"
+    filename = re.findall('.*/(.*)',imagelink)[0]
+    if not (filename in processed):
+        try:
+            print "Fetching " + filename
+            data = (urlopen(webroot(URL) + imagelink).read())
+            path = local(filename,image=True)
+            file = open(path,'wb')
+            file.write(data)
+            file.close()
+            processed.append(filename)
+        except HTTPError:
+            print 'Error: unable to fetch file ' + filename
+
+def local(page,image=False):
+    "returns a local path for a given page/image"
+    if image:
+        return TMPFOLDER + os.sep + page
+    else:
+        return TMPFOLDER + os.sep + page + '.html'
+
+def exists(page,image=False):
+    "checks if given page/image already exists"
+    path = local(page,image)
+    if os.path.exists(path): return True
+    return False
+
+def webroot(url):
+    return re.findall('(http://.*?)/',url)[0]
+
 def output(html,page):
     "encapsulates raw html code into nice html body"
     header = "<html><head>"
     header += "<title>"
     header += page
     header += "</title>"
-    header += "<link type='text/css' href='freecad.css' rel='stylesheet'>"
+    header += "<link type='text/css' href='wiki.css' rel='stylesheet'>"
     header += "</head><body>"
     footer = "</body></html>"
     html = header+html+footer
@@ -109,42 +258,11 @@ def output(html,page):
     file.write(html)
     file.close()
 
-def local(page):
-    return TMPFOLDER + '/' + page + '.html'
-
-def getlinks(html):
-    "returns a list of wikipage links in html file"
-    links = re.findall('<a[^>]*>.*?</a>',html)
-    pages = []
-    for l in links:
-        rg = re.findall('php\?title=(.*)\" title',l)
-        if rg: pages.append(rg[0])
-    return pages
-
-def cleanlinks(html, pages=None):
-    "cleans page links found in html"
-    if not pages: pages = getlinks(html)
-    for page in pages:
-        if  page in NORETRIEVE:
-            output = 'href="' + WEBROOT + page + '"'
-        else:
-            output = 'href="' + page + '.html"'
-        html = re.compile('href="[^"]+' + page + '"').sub(output,html)
-    return html
-
-def retrieve(page):
-    "retrieves given page from the wiki"
-    try:
-        print "retrieving " + page
-        html = (urlopen(WEBROOT + page).read())
-        return html
-    except HTTPError:
-        print 'Error getting page source'
-
-def test(page=None):
-    if not page: page = 'Draft_Line'
-    html = retrieve(page)
-    html = clean(html)
-    html = cleanlinks(html)
-    output(html,page)
-        
+def main(arg):	
+    if arg: URL = arg[0]
+    if len(arg) > 1: INDEX = arg[1]
+    crawl()
+    
+if __name__ == "__main__":
+	main(sys.argv[1:])
+      
