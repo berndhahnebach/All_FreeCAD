968388581ef8d141571de364ebebbb4a80878839
jriegel <jriegel>
2009-07-31 14:07:11 +0000

    + implementing Drawing

--- src/Mod/Drawing/App/AppDrawingPy.cpp
+++ src/Mod/Drawing/App/AppDrawingPy.cpp
@@ -24,10 +24,75 @@
 #include "PreCompiled.h"
 #ifndef _PreComp_
 # include <Python.h>
+# include <HLRBRep_HLRToShape.hxx>
+# include <gp_Ax2.hxx>
+# include <gp_Pnt.hxx>
+# include <gp_Dir.hxx>
+# include <HLRBRep_Algo.hxx>
+
+
 #endif
+
+#include <Mod/Part/App/TopoShapePy.h>
+#include <Base/Console.h>
+
+
+using namespace Part;
+
+static PyObject * 
+project(PyObject *self, PyObject *args)
+{
+    PyObject *pcObj;
+    if (!PyArg_ParseTuple(args, "O!", &(TopoShapePy::Type), &pcObj))     // convert args: Python->C
+        return NULL;                             // NULL triggers exception
+
+    PY_TRY {
+        TopoShapePy* pShape = static_cast<TopoShapePy*>(pcObj);
+        //TopoShape* shape = new MeshObject(*pShape->getTopoShapeObjectPtr());
+
+        Handle( HLRBRep_Algo ) brep_hlr = new HLRBRep_Algo;
+        brep_hlr->Add( pShape->getTopoShapePtr()->_Shape );
+
+        gp_Ax2 transform(gp_Pnt(0,0,0),gp_Dir(0,0,1));
+        HLRAlgo_Projector projector( transform );
+        brep_hlr->Projector( projector );
+        brep_hlr->Update();
+        brep_hlr->Hide();
+
+        // extracting the result sets:
+        HLRBRep_HLRToShape shapes( brep_hlr );
+
+        TopoDS_Shape OuterEdges;
+        OuterEdges = shapes.OutLineVCompound();
+
+        //TopoDS_Shape InnerEdges;
+        //InnerEdges = shapes.VCompound();
+
+        //TopoDS_Shape HiddenOuterEdges;
+        //HiddenOuterEdges = shapes.OutLineHCompound();
+
+        //TopoDS_Shape HiddenInnerEdges;
+        //HiddenInnerEdges = shapes.OutLineHCompound();
+
+        Py::List list;
+        list.append(Py::Object(new TopoShapePy(new TopoShape(OuterEdges))));
+ /*       list.append(Py::Object(new TopoShapePy(new TopoShape(InnerEdges))));
+        list.append(Py::Object(new TopoShapePy(new TopoShape(HiddenOuterEdges))));
+        list.append(Py::Object(new TopoShapePy(new TopoShape(HiddenInnerEdges))));*/
+
+        return Py::new_reference_to(list);
+
+    } PY_CATCH;
+
+}
+
+
+
 
 
 /* registration table  */
 struct PyMethodDef Drawing_methods[] = {
+   {"project"       ,project      ,METH_VARARGS,
+     "(Outer,Inner,HOuter,HInner) = project(TopoShape) -- Project a shape and return the visible/invisible parts of it."},
     {NULL, NULL}        /* end of table marker */
 };
