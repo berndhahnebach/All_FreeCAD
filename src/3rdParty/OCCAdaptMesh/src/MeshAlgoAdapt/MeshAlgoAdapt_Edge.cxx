ee2ad2cf33004eefd6aa5c53d25a7c9d73174d86
dvdjimmy <dvdjimmy>
2009-08-10 16:52:08 +0000

    -removed OCCAdaptMesh due to incompatibility with OCC6.3 +changed corresponding cmake files to take care of OCCAdaptMesh removal

--- src/3rdParty/OCCAdaptMesh/src/MeshAlgoAdapt/MeshAlgoAdapt_Edge.cxx
+++
@@ -1,6 +0,0 @@
-// File:	MeshAlgoAdapt_Edge.cxx
-// Created:	Fri Jul  2 11:33:41 1993
-// Author:	Didier PIFFAULT
-//		<dpf@zerox>
-
-#include <MeshAlgoAdapt_Edge.ixx>
--- src/3rdParty/OCCAdaptMesh/src/MeshAlgoAdapt/MeshAlgoAdapt_Triangle.cxx
+++
@@ -1,6 +0,0 @@
-// File:	MeshAlgoAdapt_Triangle.cxx
-// Created:	Fri Jul  2 11:33:22 1993
-// Author:	Didier PIFFAULT
-//		<dpf@zerox>
-
-#include <MeshAlgoAdapt_Triangle.ixx>
