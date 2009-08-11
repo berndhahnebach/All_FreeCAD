58987650525b81bdcf3f0db11b2fb62d1aac92b4
jriegel <FreeCAD@juergen-riegel.net>
2009-08-11 09:59:08 +0000

    + fixes from Joachim + using boost Version numbers to distinguish LibPack Versions + Still using Xercesc 2.8 + changes to reflect LibPack_7.0_x86_VC9.zip

--- cMake/UseLibPack7x.cmake
+++ cMake/UseLibPack7x.cmake
@@ -19,39 +19,39 @@ set(PYTHONLIBS_FOUND TRUE)
 	
 # XercesC
 set(XERCESC_INCLUDE_DIR ${FREECAD_LIBPACK_DIR}/include/xercesc)
-set(XERCESC_LIBRARIES       xerces-c_3.lib)
-set(XERCESC_DEBUG_LIBRARIES xerces-c_3D.lib)
+set(XERCESC_LIBRARIES       xerces-c_2.lib)
+set(XERCESC_DEBUG_LIBRARIES xerces-c_2D.lib)
 set(XERCESC_FOUND TRUE) 
 	
 # Boost
 set(Boost_INCLUDE_DIR ${FREECAD_LIBPACK_DIR}/include/boost)
 set(Boost_LIBRARIES 
-	optimized boost_filesystem-vc90-mt.lib
-    optimized boost_system-vc90-mt.lib 
-	#optimized boost_graph-vc90-mt.lib 
-	optimized boost_program_options-vc90-mt.lib
-	optimized boost_regex-vc90-mt.lib
-	optimized boost_signals-vc90-mt.lib
+	optimized boost_filesystem-vc90-mt-1_39.lib
+    optimized boost_system-vc90-mt-1_39.lib 
+	optimized boost_graph-vc90-mt-1_39.lib 
+	optimized boost_program_options-vc90-mt-1_39.lib
+	optimized boost_regex-vc90-mt-1_39.lib
+	optimized boost_signals-vc90-mt-1_39.lib
 )
 set(Boost_DEBUG_LIBRARIES 
-	debug boost_filesystem-vc90-mt-gd.lib 
-	debug boost_date_time-vc90-mt-gd.lib
-	debug boost_filesystem-vc90-mt-gd.lib
-	debug boost_iostreams-vc90-mt-gd.lib
-	debug boost_math_c99f-vc90-mt-gd.lib
-	debug boost_math_tr1f-vc90-mt-gd.lib
-	debug boost_thread-vc90-mt-gd.lib
-	debug boost_system-vc90-mt-gd.lib
-	debug boost_graph-vc90-mt-gd.lib 
-	debug boost_program_options-vc90-mt-gd.lib
-	debug boost_regex-vc90-mt-gd.lib
-	debug boost_signals-vc90-mt-gd.lib
+	debug boost_filesystem-vc90-mt-gd-1_39.lib 
+	debug boost_date_time-vc90-mt-gd-1_39.lib
+	debug boost_filesystem-vc90-mt-gd-1_39.lib
+	debug boost_iostreams-vc90-mt-gd-1_39.lib
+	debug boost_math_c99f-vc90-mt-gd-1_39.lib
+	debug boost_math_tr1f-vc90-mt-gd-1_39.lib
+	debug boost_thread-vc90-mt-gd-1_39.lib
+	debug boost_system-vc90-mt-gd-1_39.lib
+	debug boost_graph-vc90-mt-gd-1_39.lib 
+	debug boost_program_options-vc90-mt-gd-1_39.lib
+	debug boost_regex-vc90-mt-gd-1_39.lib
+	debug boost_signals-vc90-mt-gd-1_39.lib
 )
 set(Boost_FOUND TRUE) 
 
 # Zlib
 set(ZLIB_INCLUDE_DIR ${FREECAD_LIBPACK_DIR}/include/zlib)
-set(ZLIB_LIBRARIES  zlib1.lib)
+set(ZLIB_LIBRARIES  zdll.lib)
 set(ZLIB_FOUND TRUE) 
 	
 # Coin3D
