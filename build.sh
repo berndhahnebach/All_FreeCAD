1cf80038017c87aa535ccd8dd0ccd9b38f674d60
wmayer <wmayer@users.sourceforge.net>
2009-08-16 15:00:49 +0000

    + small fix

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@
 # configure running successfully. 
 # CASROOT=${CASROOT:-/opt/OpenCASCADE6.2.0/ros}
 #./configure CXXFLAGS="-fno-strict-aliasing" LDFLAGS="-Wl,-z,defs" --with-occ-include=$CASROOT/inc --with-occ-lib=$CASROOT/Linux/lib
-./configure CXXFLAGS="-fno-strict-aliasing" LDFLAGS="-Wl,-z,defs"
+./configure CXXFLAGS="-fno-strict-aliasing -Wno-write-strings" LDFLAGS="-Wl,-z,defs"
 
