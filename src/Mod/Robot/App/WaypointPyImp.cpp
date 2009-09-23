d866cde8094787b98414d2fd9c230fb2c66cf473
jriegel <FreeCAD@juergen-riegel.net>
2009-09-23 21:56:25 +0000

    + implement WaypointPy

--- src/Mod/Robot/App/WaypointPyImp.cpp
+++ src/Mod/Robot/App/WaypointPyImp.cpp
@@ -1,18 +1,36 @@
 
 #include "PreCompiled.h"
 
+
+#include <Base/PlacementPy.h>
+#include <Base/Placement.h>
+#include <Base/Exception.h>
+
 #include "Mod/Robot/App/Waypoint.h"
+
 
 // inclusion of the generated files (generated out of WaypointPy.xml)
 #include "WaypointPy.h"
 #include "WaypointPy.cpp"
 
 using namespace Robot;
+using namespace Base;
 
 // returns a string which represents the object e.g. when printed in python
 const char *WaypointPy::representation(void) const
 {
-    return "<Waypoint object>";
+   double A,B,C;
+    PlacementPy::PointerType ptr = reinterpret_cast<PlacementPy::PointerType>(_pcTwinPointer);
+    std::stringstream str;
+    ptr->getRotation().getEuler(A,B,C);
+
+    str << "Waypoint [Pos=(";
+    str << ptr->getPosition().x << ","<< ptr->getPosition().y << "," << ptr->getPosition().z;
+    str << "), Euler=(" << A << "," << B << "," << C << ")]";
+
+    static std::string buf;
+    buf = str.str();
+    return buf.c_str();
 }
 
 PyObject *WaypointPy::PyMake(struct _typeobject *, PyObject *, PyObject *)  // Python wrapper
@@ -22,8 +40,41 @@ PyObject *WaypointPy::PyMake(struct _typeobject *, PyObject *, PyObject *)  // P
 }
 
 // constructor method
-int WaypointPy::PyInit(PyObject* /*args*/, PyObject* /*kwd*/)
+int WaypointPy::PyInit(PyObject* args, PyObject* kwd)
 {
+    PyObject *pos;
+    char *name="P";
+    char *type = "PTP";
+    float vel = 100;
+    int cont = 0;
+    int tool=0;
+    int base=0;
+
+    static char *kwlist[] = {"type","name", "vel", "cont", "tool", "base",  NULL};
+
+    if (!PyArg_ParseTupleAndKeywords(args, kwd, "O!|ssfiii", kwlist,
+                                      &(Base::PlacementPy::Type), &pos, // the placement object
+                                      &type, &name, &vel, &cont, &tool, &base ))
+        return -1;
+
+    getWaypointPtr()->EndPos = *static_cast<Base::PlacementPy*>(pos)->getPlacementPtr();
+    getWaypointPtr()->Name   = name;
+    std::string typeStr(type);
+    if(typeStr=="PTP")
+        getWaypointPtr()->Type = Waypoint::PTP;
+    else if(typeStr=="LIN")
+        getWaypointPtr()->Type = Waypoint::LIN;
+    else if(typeStr=="CIRC")
+        getWaypointPtr()->Type = Waypoint::CIRC;
+    else if(typeStr=="WAIT")
+        getWaypointPtr()->Type = Waypoint::WAIT;
+    else 
+        getWaypointPtr()->Type = Waypoint::UNDEF;
+    getWaypointPtr()->Velocity = vel;
+    getWaypointPtr()->Cont = cont?true:false;
+    getWaypointPtr()->Tool = tool;
+    getWaypointPtr()->Base = base;
+
     return 0;
 }
 
@@ -32,32 +83,54 @@ int WaypointPy::PyInit(PyObject* /*args*/, PyObject* /*kwd*/)
 
 Py::String WaypointPy::getName(void) const
 {
-    return Py::String();
+    return Py::String(getWaypointPtr()->Name.c_str());
 }
 
-void WaypointPy::setName(Py::String /*arg*/)
+void WaypointPy::setName(Py::String arg)
 {
-
+    getWaypointPtr()->Name = arg.as_std_string();
 }
 
 Py::String WaypointPy::getType(void) const
 {
-    return Py::String();
+    if(getWaypointPtr()->Type == Waypoint::PTP)
+        return Py::String("PTP");
+    else if(getWaypointPtr()->Type == Waypoint::LIN)
+        return Py::String("LIN");
+    else if(getWaypointPtr()->Type == Waypoint::CIRC)
+        return Py::String("CIRC");
+    else if(getWaypointPtr()->Type == Waypoint::WAIT)
+        return Py::String("WAIT");
+    else if(getWaypointPtr()->Type == Waypoint::UNDEF)
+        return Py::String("UNDEF");
+
 }
 
-void WaypointPy::setType(Py::String /*arg*/)
+void WaypointPy::setType(Py::String arg)
 {
+    std::string typeStr(arg.as_std_string());
+    if(typeStr=="PTP")
+        getWaypointPtr()->Type = Waypoint::PTP;
+    else if(typeStr=="LIN")
+        getWaypointPtr()->Type = Waypoint::LIN;
+    else if(typeStr=="CIRC")
+        getWaypointPtr()->Type = Waypoint::CIRC;
+    else if(typeStr=="WAIT")
+        getWaypointPtr()->Type = Waypoint::WAIT;
+    else
+        throw Base::Exception("Unknown waypoint type! Only: PTP,LIN,CIRC,WAIT are allowed.");
 
 }
 
 
 Py::Object WaypointPy::getPos(void) const
 {
-    return Py::Object();
+    return Py::Object(new PlacementPy(new Placement(getWaypointPtr()->EndPos)));
 }
 
-void WaypointPy::setPos(Py::Object /*arg*/)
+void WaypointPy::setPos(Py::Object arg)
 {
+
 
 }
 
