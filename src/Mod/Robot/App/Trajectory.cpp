6adaed94d839f05123788af9b991c1b637991144
jriegel <jriegel>
2009-09-23 14:04:32 +0000

    + Trajectory classes added + Task panel dialogs for controlling the robot ant trajectory added

---
+++ src/Mod/Robot/App/Trajectory.cpp
@@ -0,0 +1,83 @@
+/***************************************************************************
+ *   Copyright (c) Jürgen Riegel          (juergen.riegel@web.de) 2002     *
+ *                                                                         *
+ *   This file is part of the FreeCAD CAx development system.              *
+ *                                                                         *
+ *   This library is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU Library General Public           *
+ *   License as published by the Free Software Foundation; either          *
+ *   version 2 of the License, or (at your option) any later version.      *
+ *                                                                         *
+ *   This library  is distributed in the hope that it will be useful,      *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU Library General Public License for more details.                  *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this library; see the file COPYING.LIB. If not,    *
+ *   write to the Free Software Foundation, Inc., 59 Temple Place,         *
+ *   Suite 330, Boston, MA  02111-1307, USA                                *
+ *                                                                         *
+ ***************************************************************************/
+
+
+#include "PreCompiled.h"
+
+#ifndef _PreComp_
+#endif
+
+#include <Base/Writer.h>
+
+#include "kdl/chain.hpp"
+#include "kdl/chainfksolver.hpp"
+#include "kdl/chainfksolverpos_recursive.hpp"
+#include "kdl/frames_io.hpp"
+#include "kdl/chainiksolver.hpp"
+#include "kdl/chainiksolvervel_pinv.hpp"
+#include "kdl/chainjnttojacsolver.hpp"
+#include "kdl/ChainIkSolverPos_NR.hpp"
+
+#include "Trajectory.h"
+
+#ifndef M_PI
+    #define M_PI 3.14159265358979323846
+    #define M_PI    3.14159265358979323846 /* pi */
+#endif
+
+#ifndef M_PI_2
+    #define M_PI_2  1.57079632679489661923 /* pi/2 */
+#endif
+
+using namespace Robot;
+using namespace Base;
+using namespace KDL;
+
+
+TYPESYSTEM_SOURCE(Robot::Trajectory , Base::Persistence);
+
+Trajectory::Trajectory()
+{
+
+}
+
+Trajectory::~Trajectory()
+{
+}
+
+unsigned int Trajectory::getMemSize (void) const
+{
+	return 0;
+}
+
+void Trajectory::Save (Writer &/*writer*/) const
+{
+}
+
+void Trajectory::Restore(XMLReader &/*reader*/)
+{
+}
+
+
+
+
+ 
---
+++ src/Mod/Robot/Gui/TaskRobot6Axis.cpp
@@ -0,0 +1,84 @@
+/***************************************************************************
+ *   Copyright (c) 2009 Jürgen Riegel <juergen.riegel@web.de>              *
+ *                                                                         *
+ *   This file is part of the FreeCAD CAx development system.              *
+ *                                                                         *
+ *   This library is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU Library General Public           *
+ *   License as published by the Free Software Foundation; either          *
+ *   version 2 of the License, or (at your option) any later version.      *
+ *                                                                         *
+ *   This library  is distributed in the hope that it will be useful,      *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU Library General Public License for more details.                  *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this library; see the file COPYING.LIB. If not,    *
+ *   write to the Free Software Foundation, Inc., 59 Temple Place,         *
+ *   Suite 330, Boston, MA  02111-1307, USA                                *
+ *                                                                         *
+ ***************************************************************************/
+
+
+#include "PreCompiled.h"
+
+#ifndef _PreComp_
+#endif
+
+#include "ui_TaskRobot6Axis.h"
+#include "TaskRobot6Axis.h"
+#include <Gui/Application.h>
+#include <Gui/Document.h>
+#include <Gui/BitmapFactory.h>
+#include <Gui/ViewProvider.h>
+#include <Gui/WaitCursor.h>
+#include <Base/Console.h>
+#include <Gui/Selection.h>
+
+
+using namespace RobotGui;
+using namespace Gui;
+
+TaskRobot6Axis::TaskRobot6Axis(QWidget *parent)
+    : TaskBox(Gui::BitmapFactory().pixmap("document-new"),tr("TaskRobot6Axis"),true, parent)
+{
+    // we need a separate container widget to add all controls to
+    proxy = new QWidget(this);
+    ui = new Ui_TaskRobot6Axis();
+    ui->setupUi(proxy);
+    QMetaObject::connectSlotsByName(this);
+
+    this->groupLayout()->addWidget(proxy);
+
+    Gui::Selection().Attach(this);
+}
+
+TaskRobot6Axis::~TaskRobot6Axis()
+{
+    delete ui;
+    Gui::Selection().Detach(this);
+}
+
+void TaskRobot6Axis::changeEvent(QEvent *e)
+{
+    TaskBox::changeEvent(e);
+    if (e->type() == QEvent::LanguageChange) {
+        ui->retranslateUi(proxy);
+    }
+}
+
+void TaskRobot6Axis::OnChange(Gui::SelectionSingleton::SubjectType &rCaller,
+                              Gui::SelectionSingleton::MessageType Reason)
+{
+    if (Reason.Type == SelectionChanges::AddSelection ||
+        Reason.Type == SelectionChanges::RmvSelection ||
+        Reason.Type == SelectionChanges::SetSelection ||
+        Reason.Type == SelectionChanges::ClrSelection) {
+    }
+}
+
+
+
+
+#include "moc_TaskRobot6Axis.cpp"
