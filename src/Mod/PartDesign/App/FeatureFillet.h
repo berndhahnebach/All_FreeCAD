cac1e7c44a77c5a1811818fb3268e9e4b4f73592
jriegel <FreeCAD@juergen-riegel.net>
2009-10-14 16:06:23 +0000

    + implementing Selection use case for SubElement

--- src/Mod/PartDesign/App/FeatureFillet.h
+++ src/Mod/PartDesign/App/FeatureFillet.h
@@ -37,7 +37,7 @@ class Fillet : public Part::Feature
 public:
     Fillet();
 
-    App::PropertyLink Base;
+    App::PropertyLinkSub Base;
     App::PropertyFloatConstraint Radius;
 
     /** @name methods override feature */
