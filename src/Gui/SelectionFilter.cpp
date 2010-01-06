f50085606e990ec4c709467f3ce3e6c6ea7cb6aa
wmayer <wmayer@users.sourceforge.net>
2010-01-06 12:01:48 +0000

    recreate lex-SelectionFilter.c to fix bad format

--- src/Gui/SelectionFilter.cpp
+++ src/Gui/SelectionFilter.cpp
@@ -141,11 +141,7 @@ int fileno(FILE *stream) {return _fileno(stream);}
 #include "SelectionFilter.tab.c"
 
 // Scanner, defined in SelectionFilter.l
-#ifdef __GNUC__
-#include "lex.SelectionFilter_gcc.c"
-#else
 #include "lex.SelectionFilter.c"
-#endif
 
 bool SelectionFilter::parse(void)
 {
