3199581c60ba2dbf9afb08b37bede2d42386055d
jriegel <jriegel>
2010-01-16 15:35:51 +0000

    + fix the installer

--- src/Build/Version.h
+++ src/Build/Version.h
@@ -4,11 +4,11 @@
 #define FCVersionMinor "10"
 #define FCVersionName  "Dagoba"
 
-#define FCRevision      "2687"      //Highest committed revision number
-#define FCRevisionDate  "2009/11/30 22:03:25"     //Date of highest committed revision
-#define FCRevisionRange "2687"    //Update revision range
-#define FCRepositoryURL "https://free-cad.svn.sourceforge.net/svnroot/free-cad/trunk/src"      //Repository URL of the working copy
-#define FCCurrentDateT  "2009/12/01 10:44:15"      //Current system date & time
+#define FCRevision      "2860"      //Highest committed revision number
+#define FCRevisionDate  "2010/01/16 11:39:40"     //Date of highest committed revision
+#define FCRevisionRange "2860"    //Update revision range
+#define FCRepositoryURL "https://free-cad.svn.sourceforge.net/svnroot/free-cad/trunk"      //Repository URL of the working copy
+#define FCCurrentDateT  "2010/01/16 16:20:33"      //Current system date & time
 
 //Placeholders of the form "$WCxxx?TrueText:FalseText$" are replaced with
 //TrueText if the tested condition is true, and FalseText if false.
