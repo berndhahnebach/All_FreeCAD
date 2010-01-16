83aa39faeb07913b7c64e381ab35fb26a83bc6e5
jriegel <FreeCAD@juergen-riegel.net>
2010-01-16 15:35:51 +0000

    + fix the installer

--- src/WindowsInstaller/BuildInstaller.bat
+++ src/WindowsInstaller/BuildInstaller.bat
@@ -1,7 +1,11 @@
 
-if NOT DEFINED WIXDIR set WIXDIR=C:\Program Files\Windows Installer XML v3
+if NOT DEFINED WIXDIR set WIXDIR=C:\Program Files (x86)\Windows Installer XML v3
 
-"C:\Program Files\TortoiseSVN\bin\SubWCRev.exe" .. Version.wxi.in Version.wxi
+"C:\Program Files\TortoiseSVN\bin\SubWCRev.exe" ..\.. Version.wxi.in Version.wxi
+"C:\Program Files\TortoiseSVN\bin\SubWCRev.exe" ..\.. CopyRelease.bat.in CopyRelease.bat
+"C:\Program Files\TortoiseSVN\bin\SubWCRev.exe" ..\.. ..\Build\Version.h.in ..\Build\Version.h
+
+SET /P M=Reebuild and press enter 
 
 "%WIXDIR%\bin\candle.exe" -out FreeCADBase.wxobj    FreeCADBase.wxs
 "%WIXDIR%\bin\candle.exe" -out LibPack.wxobj        LibPack.wxs
@@ -11,3 +15,4 @@ if NOT DEFINED WIXDIR set WIXDIR=C:\Program Files\Windows Installer XML v3
 
 "%WIXDIR%\bin\light.exe"  -ext WixUIExtension -cultures:en-us -out FreeCAD.msi *.wxobj
 
+call CopyRelease.bat
