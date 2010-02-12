44cb6c83edd2a1c7824cf4ed6ed11001e55748f0
jriegel <jriegel>
2010-02-12 10:33:53 +0000

    + updating batch build script

--- BuildAll.bat
+++ BuildAll.bat
@@ -2,19 +2,13 @@ rem @echo off
 rem   Build script, uses vcbuild to completetly build FreeCAD
 
 rem  set the aprobiated Variables here or outside in the system
-if NOT DEFINED VCDIR set VCDIR="C:\Program Files\Microsoft Visual Studio 9.0\"
+if NOT DEFINED VCDIR set VCDIR=C:\Program Files\Microsoft Visual Studio 9.0
 
 rem Register VS Build programms
-call %VCDIR%\VC\vcvarsall.bat"
+call "%VCDIR%\VC\vcvarsall.bat"
 
-rem Set Standard include paths
-rem set INCLUDE=%INCLUDE%;%FrameworkSDKDir%\include
-rem set INCLUDE=%INCLUDE%;C:\Program Files\Microsoft SDKs\Windows\v6.0A\Include
-
-rem Set lib Pathes
-rem set LIB=%LIB%;C:\Program Files\Microsoft SDKs\Windows\v6.0A\Lib
-rem set LIB=%LIB%;%PROGRAMFILES%\Microsoft Visual Studio\VC98\Lib
 
 rem Start the Visuall Studio build process
-"%VCDIR%\VC\vcpackages\vcbuild.exe" FreeCAD_trunk.sln /useenv 
-"%VCDIR%\VC\vcpackages\vcbuild.exe" FreeCAD_trunk.sln /useenv 
+"%VCDIR%\VC\vcpackages\vcbuild.exe" FreeCAD_trunk.sln "Debug|Win32" 
+"%VCDIR%\VC\vcpackages\vcbuild.exe" FreeCAD_trunk.sln "Debug|Win32" 
+"%VCDIR%\VC\vcpackages\vcbuild.exe" FreeCAD_trunk.sln "Release|Win32" 
