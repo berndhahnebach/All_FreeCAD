602c9e6085085de7bafd7c19d567f3d7925cbc68
wmayer <wmayer@users.sourceforge.net>
2010-02-18 09:31:43 +0000

    + support of Swedish

---
+++ src/Mod/Image/Gui/Image_se.ts
@@ -0,0 +1,122 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="sv_SE">
+<context>
+    <name>CmdImageCapturerTest</name>
+    <message>
+        <source>Image</source>
+        <translation>Bild</translation>
+    </message>
+    <message>
+        <source>CapturerTest</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>test camara capturing</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CmdImageOpen</name>
+    <message>
+        <source>Image</source>
+        <translation>Bild</translation>
+    </message>
+    <message>
+        <source>Open</source>
+        <translation>Öppna</translation>
+    </message>
+    <message>
+        <source>Image open image view function</source>
+        <translation type="unfinished">Funktion zum Öffnen einer Bilddatei</translation>
+    </message>
+</context>
+<context>
+    <name>ImageGui::GLImageBox</name>
+    <message>
+        <source>Undefined type of colour space for image viewing</source>
+        <translation>Odefinierad färgrymd för bildvisning</translation>
+    </message>
+    <message>
+        <source>GLImageBox::getPixFormat</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ImageGui::ImageView</name>
+    <message>
+        <source>&amp;Fit image</source>
+        <translation>An&amp;passa bild</translation>
+    </message>
+    <message>
+        <source>Stretch the image to fit the view</source>
+        <translation>Skala bilden till vyn</translation>
+    </message>
+    <message>
+        <source>&amp;1:1 scale</source>
+        <translation>&amp;1:1 Skala</translation>
+    </message>
+    <message>
+        <source>Display the image at a 1:1 scale</source>
+        <translation>Visa bilden i 1:1 skala</translation>
+    </message>
+    <message>
+        <source>&amp;Original color</source>
+        <translation>&amp;Originalfärg</translation>
+    </message>
+    <message>
+        <source>Display the image with its original color(s)</source>
+        <translation>Visa bilden med dess originalfärger</translation>
+    </message>
+    <message>
+        <source>&amp;Brightened color</source>
+        <translation type="unfinished">Farben auf&amp;hellen</translation>
+    </message>
+    <message>
+        <source>Display the image with brightened color(s)</source>
+        <translation type="unfinished">Bild mit aufgeheltten Farben anzeigen</translation>
+    </message>
+    <message>
+        <source>Standard</source>
+        <translation>Standard</translation>
+    </message>
+    <message>
+        <source>Ready...</source>
+        <translation>Klar...</translation>
+    </message>
+    <message>
+        <source>grey</source>
+        <translation>Grå</translation>
+    </message>
+    <message>
+        <source>zoom</source>
+        <translation>Zoom</translation>
+    </message>
+    <message>
+        <source>outside image</source>
+        <translation>Utanför bild</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Image viewer</source>
+        <translation>Bildvisare</translation>
+    </message>
+    <message>
+        <source>Images (*.png *.xpm *.jpg *.bmp)</source>
+        <translation>Bildformat (*.png *.xpm *.jpg *.bmp)</translation>
+    </message>
+    <message>
+        <source>Choose an image file to open</source>
+        <translation>Välj en bildfil att öppna</translation>
+    </message>
+</context>
+<context>
+    <name>Workbench</name>
+    <message>
+        <source>Image</source>
+        <translation>Bild</translation>
+    </message>
+</context>
+</TS>
---
+++ src/Mod/ReverseEngineering/Gui/Resources/translations/ReverseEngineering_se.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="sv_SE">
+<context>
+    <name>CmdApproxSurface</name>
+    <message>
+        <source>Reverse Engineering</source>
+        <translatorcomment>The term &quot;Reverse engineering&quot; is also used in Sweden</translatorcomment>
+        <translation>Reverse Engineering</translation>
+    </message>
+    <message>
+        <source>Approximate surface...</source>
+        <translation>Approximera yta...</translation>
+    </message>
+    <message>
+        <source>Approximate a B-Spline surface</source>
+        <translation>Approximiera en B-Spline-Yta</translation>
+    </message>
+</context>
+<context>
+    <name>Workbench</name>
+    <message>
+        <source>Reverse Engineering</source>
+        <translatorcomment>The term &quot;Reverse engineering&quot; is also used in Sweden</translatorcomment>
+        <translation>Reverse Engineering</translation>
+    </message>
+</context>
+</TS>
