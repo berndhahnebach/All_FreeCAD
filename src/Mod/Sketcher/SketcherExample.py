ded394a4890c4b7ef8aa8efd08c5b5a482270b6a
jriegel <jriegel>
2010-03-21 13:36:40 +0000

    + additional constraints

--- src/Mod/Sketcher/SketcherExample.py
+++ src/Mod/Sketcher/SketcherExample.py
@@ -3,22 +3,31 @@ from Sketcher import *
 from Part import *
 from FreeCAD import *
 
-# === low level constraint solving ===
+StartPoint = 0
+EndPoint = 1
+MiddlePoint = 2
+
+# === low level sketch building & constraint solving ===
 # create a constraint sketch
 sketch = Sketch()
 
 # create some lines
 l1 = Line(Vector(0,0,0),Vector(10,2,0))
+l2 = Line(Vector(0,0,0),Vector(2,10,0))
 
 # add the geometry to the sketch
-sketch.addGeometry(l1)
+Line1 = sketch.addGeometry(l1)
+Line2 = sketch.addGeometry(l2)
 
 # add constraints
-sketch.addHorizontalConstraint(0,'h')
+sketch.addHorizontalConstraint(Line1,'h')
+sketch.addVerticalConstraint(Line2,'v')
+# set a coincident constraint between the start points of both lines
+sketch.addPointOnPointConstraint(Line1,StartPoint,Line2,StartPoint,'p')
 
 # solve the sketch
 sketch.solve()
 
 # get the result
-sketch.Geometries[0].StartPoint
-sketch.Geometries[0].EndPoint
+sketch.Geometries[0]
+sketch.Geometries[1]
