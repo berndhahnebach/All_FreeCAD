4e7ae6a2177c544df54ed41d7739827455e25b12
jriegel <jriegel>
2010-04-30 16:45:06 +0000

    + new unstable release

--- src/Build/Version.h
+++ src/Build/Version.h
@@ -4,11 +4,11 @@
 #define FCVersionMinor "10"
 #define FCVersionName  "Dagoba"
 
-#define FCRevision      "3052"      //Highest committed revision number
-#define FCRevisionDate  "2010/03/21 19:39:56"     //Date of highest committed revision
-#define FCRevisionRange "3052"    //Update revision range
+#define FCRevision      "3142"      //Highest committed revision number
+#define FCRevisionDate  "2010/04/30 12:42:23"     //Date of highest committed revision
+#define FCRevisionRange "3142"    //Update revision range
 #define FCRepositoryURL "https://free-cad.svn.sourceforge.net/svnroot/free-cad/trunk"      //Repository URL of the working copy
-#define FCCurrentDateT  "2010/03/21 19:59:27"      //Current system date & time
+#define FCCurrentDateT  "2010/04/30 18:38:50"      //Current system date & time
 
 //Placeholders of the form "$WCxxx?TrueText:FalseText$" are replaced with
 //TrueText if the tested condition is true, and FalseText if false.
