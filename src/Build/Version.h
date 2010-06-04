cb5ce2a52b5e26b5f026159c4190f1af42a42323
wmayer <wmayer>
2010-06-04 21:47:23 +0000

    + prepare for new release 0.10

--- src/Build/Version.h
+++ src/Build/Version.h
@@ -4,15 +4,15 @@
 #define FCVersionMinor "10"
 #define FCVersionName  "Dagoba"
 
-#define FCRevision      "3142"      //Highest committed revision number
-#define FCRevisionDate  "2010/04/30 12:42:23"     //Date of highest committed revision
-#define FCRevisionRange "3142"    //Update revision range
+#define FCRevision      "3204"      //Highest committed revision number
+#define FCRevisionDate  "2010/06/04 23:45:29"     //Date of highest committed revision
+#define FCRevisionRange "3204"    //Update revision range
 #define FCRepositoryURL "https://free-cad.svn.sourceforge.net/svnroot/free-cad/trunk"      //Repository URL of the working copy
-#define FCCurrentDateT  "2010/04/30 18:38:50"      //Current system date & time
+#define FCCurrentDateT  "2010/06/04 23:45:29"      //Current system date & time
 
 //Placeholders of the form "$WCxxx?TrueText:FalseText$" are replaced with
 //TrueText if the tested condition is true, and FalseText if false.
 
-#define FCScrClean      "Src modified"     //True if local modifications found
+#define FCScrClean      "Src not modified"     //True if local modifications found
 #define FCScrMixed      "Src not mixed"          //True if mixed update revisions found
 
