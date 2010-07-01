60b132f8e11d0bf0e1530e26fa600dd083371372
jriegel <jriegel>
2010-07-01 22:03:31 +0000

    + prepare for new 0.11 unstable releases

--- src/Build/Version.h
+++ src/Build/Version.h
@@ -4,15 +4,15 @@
 #define FCVersionMinor "11"
 #define FCVersionName  "Dagoba"
 
-#define FCRevision      "3225"      //Highest committed revision number
-#define FCRevisionDate  "2010/06/13 20:36:40"     //Date of highest committed revision
-#define FCRevisionRange "3225"    //Update revision range
+#define FCRevision      "3273"      //Highest committed revision number
+#define FCRevisionDate  "2010/06/30 16:35:49"     //Date of highest committed revision
+#define FCRevisionRange "3273"    //Update revision range
 #define FCRepositoryURL "https://free-cad.svn.sourceforge.net/svnroot/free-cad/trunk"      //Repository URL of the working copy
-#define FCCurrentDateT  "2010/06/13 23:10:10"      //Current system date & time
+#define FCCurrentDateT  "2010/06/30 16:49:16"      //Current system date & time
 
 //Placeholders of the form "$WCxxx?TrueText:FalseText$" are replaced with
 //TrueText if the tested condition is true, and FalseText if false.
 
-#define FCScrClean      "Src not modified"     //True if local modifications found
+#define FCScrClean      "Src modified"     //True if local modifications found
 #define FCScrMixed      "Src not mixed"          //True if mixed update revisions found
 
