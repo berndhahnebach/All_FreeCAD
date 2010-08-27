a88ab5ab02d17ac898d6394eb338ccaf8c3b4f0e
wmayer <wmayer@users.sourceforge.net>
2010-08-27 11:58:28 +0000

    + script to update ts files

--- src/Tools/updatets.py
+++ src/Tools/updatets.py
@@ -22,6 +22,24 @@ DirFilter = ["^Attic$",
              "^\\.svn$",
              "^\\.deps$",
              "^\\.libs$"]
+             
+QMAKE = ""
+LUPDATE = ""
+
+def find_tools():
+    global QMAKE, LUPDATE
+    if (os.system("qmake-qt4 -v") == 0):
+        QMAKE = "qmake-qt4"
+    elif (os.system("qmake -v") == 0):
+        QMAKE = "qmake"
+    else
+        raise Exception("Cannot find qmake")
+    if (os.system("lupdate-qt4 -v") == 0):
+        QMAKE = "lupdate-qt4"
+    elif (os.system("lupdate -v") == 0):
+        QMAKE = "lupdate"
+    else
+        raise Exception("Cannot find lupdate")
 
 def filter_dirs(item):
     global DirFilter
@@ -34,15 +52,17 @@ def filter_dirs(item):
     return True
 
 def update_translation(path):
+    global QMAKE, LUPDATE
     cur = os.getcwd()
     os.chdir(path)
     filename = os.path.basename(path) + ".pro"
-    os.system("qmake-qt4 -project")
-    os.system("lupdate-qt4 " + filename)
+    os.system(QMAKE + " -project")
+    os.system(LUPDATE + " " + filename)
     #os.remove(filename)
     os.chdir(cur)
 
 def main():
+    find_tools()
     dirs=os.listdir("src/Mod")
     for i in range(len(dirs)):
         dirs[i] = "src/Mod/" + dirs[i]
@@ -50,11 +70,12 @@ def main():
     dirs.append("src/App")
     dirs.append("src/Gui")
     dirs = filter(filter_dirs, dirs)
-    print dirs
     for i in dirs:
-        print i
         update_translation(i)
 
 if __name__ == "__main__":
+    os.chdir(os.path.dirname(__file__))
+    os.path.chdir("..")
+    os.path.chdir("..")
     main()
 
