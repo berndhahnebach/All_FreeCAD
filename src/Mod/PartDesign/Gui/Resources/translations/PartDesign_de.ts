8b9cf0e93c76ba2e9590c183e288f3902947fa7a
wmayer <wmayer@users.sourceforge.net>
2010-09-03 12:47:28 +0000

    +update .ts files in PartDesign module

--- src/Mod/PartDesign/Gui/Resources/translations/PartDesign_de.ts
+++ src/Mod/PartDesign/Gui/Resources/translations/PartDesign_de.ts
@@ -1,3 +1,126 @@
-﻿<!DOCTYPE TS><TS>
-
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>CmdPartDesignFillet</name>
+    <message>
+        <source>PartDesign</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Fillet</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Make a fillet on an edge, face or body</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CmdPartDesignNewSketch</name>
+    <message>
+        <source>PartDesign</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Create sketch</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Create a new sketch</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CmdPartDesignPad</name>
+    <message>
+        <source>PartDesign</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Pad</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Pad a selected sketch</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PartDesignGui::TaskPadParameters</name>
+    <message>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Display mode:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Plot mode:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Point size:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Line width:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Transparency:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>TaskPadParameters</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PartDesignGui::Workbench</name>
+    <message>
+        <source>PartDesign</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Sketcher geoms</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Sketcher constrains</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Wrong selection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Select a sketch or 2D object.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The shape of the selected object is empty.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The shape of the selected object is not a wire.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Select an edge, face or body. Only one body is allowed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Wrong object type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Fillet works only on parts</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
 </TS>
