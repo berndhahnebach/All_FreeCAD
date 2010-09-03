09e51f5121b08adff19a879acdcff5917f906af2
wmayer <wmayer>
2010-09-03 11:59:55 +0000

    +update .ts files in Complete module

--- src/Mod/Complete/Gui/Resources/translations/Complete_de.ts
+++ src/Mod/Complete/Gui/Resources/translations/Complete_de.ts
@@ -17,7 +17,7 @@
     </message>
 </context>
 <context>
-    <name>Workbench</name>
+    <name>CompleteGui::Workbench</name>
     <message>
         <source>&amp;File</source>
         <translation>&amp;Datei</translation>
@@ -31,12 +31,16 @@
         <translation>Standard Ansicht</translation>
     </message>
     <message>
-        <source>&amp;3D View</source>
-        <translation>&amp;3D Ansicht</translation>
+        <source>&amp;Stereo</source>
+        <translation>&amp;Stereo</translation>
     </message>
     <message>
         <source>&amp;Zoom</source>
         <translation>&amp;Zoom</translation>
+    </message>
+    <message>
+        <source>Visibility</source>
+        <translation>Sichtbarkeit</translation>
     </message>
     <message>
         <source>&amp;View</source>
@@ -63,8 +67,24 @@
         <translation>&amp;Bauteil</translation>
     </message>
     <message>
+        <source>Parametric</source>
+        <translation>Parametrisch</translation>
+    </message>
+    <message>
+        <source>Ske&amp;tch</source>
+        <translation>Ski&amp;zze</translation>
+    </message>
+    <message>
         <source>&amp;Drawing</source>
         <translation>Zei&amp;chnung</translation>
+    </message>
+    <message>
+        <source>&amp;Raytracing</source>
+        <translation>&amp;Raytracing</translation>
+    </message>
+    <message>
+        <source>&amp;Drafting</source>
+        <translation>Zei&amp;chnungserstellung</translation>
     </message>
     <message>
         <source>&amp;Windows</source>
@@ -89,6 +109,102 @@
     <message>
         <source>View</source>
         <translation>Ansicht</translation>
+    </message>
+    <message>
+        <source>Part design</source>
+        <translation>Bauteil-Entwurf</translation>
+    </message>
+    <message>
+        <source>Sketch based</source>
+        <translation>Skizzenbasiert</translation>
+    </message>
+    <message>
+        <source>Drawings</source>
+        <translation>Zeichnungen</translation>
+    </message>
+    <message>
+        <source>Raytracing</source>
+        <translatorcomment>Zeichnungserstellung</translatorcomment>
+        <translation>Raytracing</translation>
+    </message>
+    <message>
+        <source>Drafting</source>
+        <translation>Zeichnungserstellung</translation>
+    </message>
+</context>
+<context>
+    <name>Workbench</name>
+    <message>
+        <source>&amp;File</source>
+        <translation type="obsolete">&amp;Datei</translation>
+    </message>
+    <message>
+        <source>&amp;Edit</source>
+        <translation type="obsolete">B&amp;earbeiten</translation>
+    </message>
+    <message>
+        <source>Standard views</source>
+        <translation type="obsolete">Standard Ansicht</translation>
+    </message>
+    <message>
+        <source>&amp;3D View</source>
+        <translation type="obsolete">&amp;3D Ansicht</translation>
+    </message>
+    <message>
+        <source>&amp;Zoom</source>
+        <translation type="obsolete">&amp;Zoom</translation>
+    </message>
+    <message>
+        <source>&amp;View</source>
+        <translation type="obsolete">A&amp;nsicht</translation>
+    </message>
+    <message>
+        <source>&amp;Tools</source>
+        <translation type="obsolete">Werk&amp;zeuge</translation>
+    </message>
+    <message>
+        <source>Analyze</source>
+        <translation type="obsolete">Analyse</translation>
+    </message>
+    <message>
+        <source>Boolean</source>
+        <translation type="obsolete">Boolsche Operation</translation>
+    </message>
+    <message>
+        <source>&amp;Meshes</source>
+        <translation type="obsolete">&amp;Netze</translation>
+    </message>
+    <message>
+        <source>&amp;Part</source>
+        <translation type="obsolete">&amp;Bauteil</translation>
+    </message>
+    <message>
+        <source>&amp;Drawing</source>
+        <translation type="obsolete">Zei&amp;chnung</translation>
+    </message>
+    <message>
+        <source>&amp;Windows</source>
+        <translation type="obsolete">&amp;Fenster</translation>
+    </message>
+    <message>
+        <source>&amp;Online-help</source>
+        <translation type="obsolete">&amp;Online-Hilfe</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation type="obsolete">&amp;Hilfe</translation>
+    </message>
+    <message>
+        <source>File</source>
+        <translation type="obsolete">Datei</translation>
+    </message>
+    <message>
+        <source>Macro</source>
+        <translation type="obsolete">Makro</translation>
+    </message>
+    <message>
+        <source>View</source>
+        <translation type="obsolete">Ansicht</translation>
     </message>
 </context>
 </TS>
--- src/Mod/Complete/Gui/Resources/translations/Complete_es.ts
+++ src/Mod/Complete/Gui/Resources/translations/Complete_es.ts
@@ -17,7 +17,7 @@
     </message>
 </context>
 <context>
-    <name>Workbench</name>
+    <name>CompleteGui::Workbench</name>
     <message>
         <source>&amp;File</source>
         <translation>&amp;Archivo</translation>
@@ -31,12 +31,16 @@
         <translation>Vistas estándar</translation>
     </message>
     <message>
-        <source>&amp;3D View</source>
-        <translation>&amp;Vista 3D</translation>
+        <source>&amp;Stereo</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <source>&amp;Zoom</source>
         <translation>&amp;Zoom</translation>
+    </message>
+    <message>
+        <source>Visibility</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <source>&amp;View</source>
@@ -63,8 +67,24 @@
         <translation>&amp;Pieza</translation>
     </message>
     <message>
+        <source>Parametric</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Ske&amp;tch</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <source>&amp;Drawing</source>
         <translation>&amp;Dibujo</translation>
+    </message>
+    <message>
+        <source>&amp;Raytracing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Drafting</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <source>&amp;Windows</source>
@@ -89,6 +109,101 @@
     <message>
         <source>View</source>
         <translation>Vista</translation>
+    </message>
+    <message>
+        <source>Part design</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Sketch based</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Drawings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Raytracing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Drafting</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Workbench</name>
+    <message>
+        <source>&amp;File</source>
+        <translation type="obsolete">&amp;Archivo</translation>
+    </message>
+    <message>
+        <source>&amp;Edit</source>
+        <translation type="obsolete">&amp;Editar</translation>
+    </message>
+    <message>
+        <source>Standard views</source>
+        <translation type="obsolete">Vistas estándar</translation>
+    </message>
+    <message>
+        <source>&amp;3D View</source>
+        <translation type="obsolete">&amp;Vista 3D</translation>
+    </message>
+    <message>
+        <source>&amp;Zoom</source>
+        <translation type="obsolete">&amp;Zoom</translation>
+    </message>
+    <message>
+        <source>&amp;View</source>
+        <translation type="obsolete">&amp;Vista</translation>
+    </message>
+    <message>
+        <source>&amp;Tools</source>
+        <translation type="obsolete">&amp;Herramientas</translation>
+    </message>
+    <message>
+        <source>Analyze</source>
+        <translation type="obsolete">Analizar</translation>
+    </message>
+    <message>
+        <source>Boolean</source>
+        <translation type="obsolete">Booleano</translation>
+    </message>
+    <message>
+        <source>&amp;Meshes</source>
+        <translation type="obsolete">&amp;Mallas</translation>
+    </message>
+    <message>
+        <source>&amp;Part</source>
+        <translation type="obsolete">&amp;Pieza</translation>
+    </message>
+    <message>
+        <source>&amp;Drawing</source>
+        <translation type="obsolete">&amp;Dibujo</translation>
+    </message>
+    <message>
+        <source>&amp;Windows</source>
+        <translation type="obsolete">&amp;Ventanas</translation>
+    </message>
+    <message>
+        <source>&amp;Online-help</source>
+        <translation type="obsolete">&amp;Ayuda online</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation type="obsolete">&amp;Ayuda</translation>
+    </message>
+    <message>
+        <source>File</source>
+        <translation type="obsolete">Archivo</translation>
+    </message>
+    <message>
+        <source>Macro</source>
+        <translation type="obsolete">Macro</translation>
+    </message>
+    <message>
+        <source>View</source>
+        <translation type="obsolete">Vista</translation>
     </message>
 </context>
 </TS>
