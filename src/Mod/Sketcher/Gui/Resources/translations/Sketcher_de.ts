fa7adb3d798707dd08f0c5f1916bc956462aa24f
wmayer <wmayer@users.sourceforge.net>
2010-09-03 11:50:59 +0000

    +update .ts files in Sketcher module

--- src/Mod/Sketcher/Gui/Resources/translations/Sketcher_de.ts
+++ src/Mod/Sketcher/Gui/Resources/translations/Sketcher_de.ts
@@ -2,6 +2,36 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="de_DE">
 <context>
+    <name>CmdSketcherConstrainCoincident</name>
+    <message>
+        <source>Sketcher</source>
+        <translation>Skizze</translation>
+    </message>
+    <message>
+        <source>Constrain lock</source>
+        <translation>Bedingung gesperrt</translation>
+    </message>
+    <message>
+        <source>Create a lock constrain on the selected item</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CmdSketcherConstrainDistance</name>
+    <message>
+        <source>Sketcher</source>
+        <translation>Skizze</translation>
+    </message>
+    <message>
+        <source>Constrain Distance</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>fix a length of a line or the distance between a line and a vertex</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>CmdSketcherConstrainHorizontal</name>
     <message>
         <source>Sketcher</source>
@@ -9,11 +39,19 @@
     </message>
     <message>
         <source>Constrain orizontal</source>
-        <translation>Bedingung horizontal</translation>
+        <translation type="obsolete">Bedingung horizontal</translation>
     </message>
     <message>
         <source>Create a horizontal constrain on the selcted item</source>
-        <translation>Erzeugt eine horizontale Bedingung am selektierten Element</translation>
+        <translation type="obsolete">Erzeugt eine horizontale Bedingung am selektierten Element</translation>
+    </message>
+    <message>
+        <source>Constrain horizontally</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Create a horizontal constraint on the selected item</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -28,7 +66,26 @@
     </message>
     <message>
         <source>Create a Lock constrain on the selcted item</source>
-        <translation>Erzeugt eine gesperrte Bedingung am selektierten Element</translation>
+        <translation type="obsolete">Erzeugt eine gesperrte Bedingung am selektierten Element</translation>
+    </message>
+    <message>
+        <source>Create a lock constrain on the selected item</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CmdSketcherConstrainParallel</name>
+    <message>
+        <source>Sketcher</source>
+        <translation>Skizze</translation>
+    </message>
+    <message>
+        <source>Constrain Parallel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>create a parallel constraint between two lines</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -39,11 +96,19 @@
     </message>
     <message>
         <source>Constrain vertical</source>
-        <translation>Bedingung vertikal</translation>
+        <translation type="obsolete">Bedingung vertikal</translation>
     </message>
     <message>
         <source>Create a vertical constrain on the selcted item</source>
-        <translation>Erzeugt eine vertikale Bedingung am selektierten Element</translation>
+        <translation type="obsolete">Erzeugt eine vertikale Bedingung am selektierten Element</translation>
+    </message>
+    <message>
+        <source>Constrain vertically</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Create a vertical constraint on the selected item</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -59,6 +124,21 @@
     <message>
         <source>Create an arc in the sketch</source>
         <translation>Erzeugt einen Kreisbogen in der Skizze</translation>
+    </message>
+</context>
+<context>
+    <name>CmdSketcherCreateBox</name>
+    <message>
+        <source>Sketcher</source>
+        <translation>Skizze</translation>
+    </message>
+    <message>
+        <source>Create line</source>
+        <translation>Linie erzeugen</translation>
+    </message>
+    <message>
+        <source>Create a line in the sketch</source>
+        <translation>Erzeugt eine Linie in der Skizze</translation>
     </message>
 </context>
 <context>
@@ -140,15 +220,15 @@
     <name>CmdSketcherCreateRectangle</name>
     <message>
         <source>Sketcher</source>
-        <translation>Skizze</translation>
+        <translation type="obsolete">Skizze</translation>
     </message>
     <message>
         <source>Create rectangle</source>
-        <translation>Erzeugt ein Rechteck</translation>
+        <translation type="obsolete">Erzeugt ein Rechteck</translation>
     </message>
     <message>
         <source>Create a rectangle in the sketch</source>
-        <translation>Erzeugt ein Rechteck in der Skizze</translation>
+        <translation type="obsolete">Erzeugt ein Rechteck in der Skizze</translation>
     </message>
 </context>
 <context>
@@ -163,7 +243,11 @@
     </message>
     <message>
         <source>Create text in the scetch</source>
-        <translation>Erzeugt einen Text in der Skizze</translation>
+        <translation type="obsolete">Erzeugt einen Text in der Skizze</translation>
+    </message>
+    <message>
+        <source>Create text in the sketch</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -197,18 +281,247 @@
     </message>
 </context>
 <context>
-    <name>Workbench</name>
+    <name>CmdSketcherNewSketchSF</name>
     <message>
         <source>Sketcher</source>
-        <translation>Skizzierer</translation>
+        <translation>Skizze</translation>
+    </message>
+    <message>
+        <source>Sketchflat sketch</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Create a new sketchflat sketch by starting externel editor</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Gui::TaskView::TaskSketcherCreateCommands</name>
+    <message>
+        <source>Appearance</source>
+        <translation>Erscheinungsbild</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Wrong selection</source>
+        <translation>Falsche Auswahl</translation>
+    </message>
+    <message>
+        <source>Select an edge from the sketch.</source>
+        <translation>Wähle eine Kante aus der Skizze aus.</translation>
+    </message>
+    <message>
+        <source>Double constraint</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The selected edge has already a horizontal constraint!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Impossible constraint</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The selected edge has already a vertical constraint!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Select vertexes from the sketch.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Select exactly two vertexes from the sketch.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Select exactly one line from the sketch.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Select two lines from the sketch.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Select exactly two lines from the sketch.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Edit sketch</source>
+        <translation>Skizze bearbeiten</translation>
+    </message>
+    <message>
+        <source>A dialog is already open in the task panel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Do you want to close this dialog?</source>
+        <translation>Möchten Sie diesen Dialog schließen?</translation>
+    </message>
+</context>
+<context>
+    <name>SketcherGui::InsertDatum</name>
+    <message>
+        <source>Insert datum</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>datum:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SketcherGui::TaskSketcherConstrains</name>
+    <message>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Filter:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>All</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Normal</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Datums</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Named</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Constraints</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SketcherGui::TaskSketcherGeneral</name>
+    <message>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unsolved</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Grid Snap</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>0.1 mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>0.2 mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>0.5 mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>1 mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>2 mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>5 mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>10 mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>20 mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>50 mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>100 mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>200 mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>500 mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>1 m</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>2 m</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>5 m</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>10 m</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>20 m</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Geometrical constraints</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Edit controls</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SketcherGui::Workbench</name>
+    <message>
+        <source>Sketcher</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Sketcher geoms</source>
-        <translation>Skizzierer geometrie</translation>
+        <translation type="unfinished">Skizzierer geometrie</translation>
     </message>
     <message>
         <source>Sketcher constrains</source>
-        <translation>Skizzierer Bedingungen</translation>
+        <translation type="unfinished">Skizzierer Bedingungen</translation>
+    </message>
+</context>
+<context>
+    <name>Workbench</name>
+    <message>
+        <source>Sketcher</source>
+        <translation type="obsolete">Skizzierer</translation>
+    </message>
+    <message>
+        <source>Sketcher geoms</source>
+        <translation type="obsolete">Skizzierer geometrie</translation>
+    </message>
+    <message>
+        <source>Sketcher constrains</source>
+        <translation type="obsolete">Skizzierer Bedingungen</translation>
     </message>
 </context>
 </TS>
--- src/Mod/Sketcher/Gui/Resources/translations/Sketcher_se.ts
+++ src/Mod/Sketcher/Gui/Resources/translations/Sketcher_se.ts
@@ -2,6 +2,36 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="sv_SE">
 <context>
+    <name>CmdSketcherConstrainCoincident</name>
+    <message>
+        <source>Sketcher</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Constrain lock</source>
+        <translation type="unfinished">Begränsningslås</translation>
+    </message>
+    <message>
+        <source>Create a lock constrain on the selected item</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CmdSketcherConstrainDistance</name>
+    <message>
+        <source>Sketcher</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Constrain Distance</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>fix a length of a line or the distance between a line and a vertex</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>CmdSketcherConstrainHorizontal</name>
     <message>
         <source>Sketcher</source>
@@ -9,11 +39,19 @@
     </message>
     <message>
         <source>Constrain orizontal</source>
-        <translation>Horisontell begränsning</translation>
+        <translation type="obsolete">Horisontell begränsning</translation>
     </message>
     <message>
         <source>Create a horizontal constrain on the selcted item</source>
-        <translation>Skapa en horisontell begränsning på den valda detaljen</translation>
+        <translation type="obsolete">Skapa en horisontell begränsning på den valda detaljen</translation>
+    </message>
+    <message>
+        <source>Constrain horizontally</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Create a horizontal constraint on the selected item</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -28,7 +66,26 @@
     </message>
     <message>
         <source>Create a Lock constrain on the selcted item</source>
-        <translation>Skapa en Låsbegränsning på den valda delen</translation>
+        <translation type="obsolete">Skapa en Låsbegränsning på den valda delen</translation>
+    </message>
+    <message>
+        <source>Create a lock constrain on the selected item</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CmdSketcherConstrainParallel</name>
+    <message>
+        <source>Sketcher</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Constrain Parallel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>create a parallel constraint between two lines</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -39,11 +96,19 @@
     </message>
     <message>
         <source>Constrain vertical</source>
-        <translation>Vertikal begränsning</translation>
+        <translation type="obsolete">Vertikal begränsning</translation>
     </message>
     <message>
         <source>Create a vertical constrain on the selcted item</source>
-        <translation>Skapa en vertikal begränsning på den valda delen</translation>
+        <translation type="obsolete">Skapa en vertikal begränsning på den valda delen</translation>
+    </message>
+    <message>
+        <source>Constrain vertically</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Create a vertical constraint on the selected item</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -59,6 +124,21 @@
     <message>
         <source>Create an arc in the sketch</source>
         <translation>Skapa en cirkelbåge i skissen</translation>
+    </message>
+</context>
+<context>
+    <name>CmdSketcherCreateBox</name>
+    <message>
+        <source>Sketcher</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Create line</source>
+        <translation type="unfinished">Skapa linje</translation>
+    </message>
+    <message>
+        <source>Create a line in the sketch</source>
+        <translation type="unfinished">Skapa en linje i skissen</translation>
     </message>
 </context>
 <context>
@@ -140,15 +220,15 @@
     <name>CmdSketcherCreateRectangle</name>
     <message>
         <source>Sketcher</source>
-        <translation>Skissare</translation>
+        <translation type="obsolete">Skissare</translation>
     </message>
     <message>
         <source>Create rectangle</source>
-        <translation>Skapa rektangel</translation>
+        <translation type="obsolete">Skapa rektangel</translation>
     </message>
     <message>
         <source>Create a rectangle in the sketch</source>
-        <translation>Skapa en rektangel i skissen</translation>
+        <translation type="obsolete">Skapa en rektangel i skissen</translation>
     </message>
 </context>
 <context>
@@ -163,7 +243,11 @@
     </message>
     <message>
         <source>Create text in the scetch</source>
-        <translation>Skapa text i skissen</translation>
+        <translation type="obsolete">Skapa text i skissen</translation>
+    </message>
+    <message>
+        <source>Create text in the sketch</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -197,18 +281,247 @@
     </message>
 </context>
 <context>
-    <name>Workbench</name>
+    <name>CmdSketcherNewSketchSF</name>
     <message>
         <source>Sketcher</source>
-        <translation>Skissare</translation>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Sketchflat sketch</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Create a new sketchflat sketch by starting externel editor</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Gui::TaskView::TaskSketcherCreateCommands</name>
+    <message>
+        <source>Appearance</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Wrong selection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Select an edge from the sketch.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Double constraint</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The selected edge has already a horizontal constraint!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Impossible constraint</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The selected edge has already a vertical constraint!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Select vertexes from the sketch.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Select exactly two vertexes from the sketch.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Select exactly one line from the sketch.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Select two lines from the sketch.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Select exactly two lines from the sketch.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Edit sketch</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>A dialog is already open in the task panel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Do you want to close this dialog?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SketcherGui::InsertDatum</name>
+    <message>
+        <source>Insert datum</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>datum:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SketcherGui::TaskSketcherConstrains</name>
+    <message>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Filter:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>All</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Normal</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Datums</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Named</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Constraints</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SketcherGui::TaskSketcherGeneral</name>
+    <message>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unsolved</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Grid Snap</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>0.1 mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>0.2 mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>0.5 mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>1 mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>2 mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>5 mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>10 mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>20 mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>50 mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>100 mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>200 mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>500 mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>1 m</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>2 m</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>5 m</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>10 m</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>20 m</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Geometrical constraints</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Edit controls</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SketcherGui::Workbench</name>
+    <message>
+        <source>Sketcher</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Sketcher geoms</source>
-        <translation>Skissgeometrier</translation>
+        <translation type="unfinished">Skissgeometrier</translation>
     </message>
     <message>
         <source>Sketcher constrains</source>
-        <translation>Skissbegränsningar</translation>
+        <translation type="unfinished">Skissbegränsningar</translation>
+    </message>
+</context>
+<context>
+    <name>Workbench</name>
+    <message>
+        <source>Sketcher</source>
+        <translation type="obsolete">Skissare</translation>
+    </message>
+    <message>
+        <source>Sketcher geoms</source>
+        <translation type="obsolete">Skissgeometrier</translation>
+    </message>
+    <message>
+        <source>Sketcher constrains</source>
+        <translation type="obsolete">Skissbegränsningar</translation>
     </message>
 </context>
 </TS>
