a163975fdb0ea753e905719a85e3e537943bd246
wmayer <wmayer@users.sourceforge.net>
2010-10-06 15:25:01 +0000

    + new version + fix in German translation + add DLLs for Salome SMESH to WiX scripts

--- src/Build/Version.h
+++ src/Build/Version.h
@@ -4,15 +4,15 @@
 #define FCVersionMinor "11"
 #define FCVersionName  "Dagoba"
 
-#define FCRevision      "3578"      //Highest committed revision number
-#define FCRevisionDate  "2010/09/14 22:54:59"     //Date of highest committed revision
-#define FCRevisionRange "3578"    //Update revision range
-#define FCRepositoryURL "https://free-cad.svn.sourceforge.net/svnroot/free-cad/trunk"      //Repository URL of the working copy
-#define FCCurrentDateT  "2010/09/14 23:13:59"      //Current system date & time
+#define FCRevision      "3607"      //Highest committed revision number
+#define FCRevisionDate  "2010/10/05 18:46:56"     //Date of highest committed revision
+#define FCRevisionRange "3602:3607"    //Update revision range
+#define FCRepositoryURL "https://free-cad.svn.sourceforge.net/svnroot/free-cad/trunk/src"      //Repository URL of the working copy
+#define FCCurrentDateT  "2010/10/06 13:37:37"      //Current system date & time
 
 //Placeholders of the form "$WCxxx?TrueText:FalseText$" are replaced with
 //TrueText if the tested condition is true, and FalseText if false.
 
-#define FCScrClean      "Src not modified"     //True if local modifications found
-#define FCScrMixed      "Src not mixed"          //True if mixed update revisions found
+#define FCScrClean      "Src modified"     //True if local modifications found
+#define FCScrMixed      "Src mixed"          //True if mixed update revisions found
 
