a256f8150c18283ca0fdd661e630309fcfe38a99
wmayer <wmayer@users.sourceforge.net>
2010-11-01 10:32:41 +0000

    + automatically update Version.h when running configure

--- src/Build/Version.h
+++ src/Build/Version.h
@@ -4,11 +4,11 @@
 #define FCVersionMinor "11"
 #define FCVersionName  "Dagoba"
 
-#define FCRevision      "3607"      //Highest committed revision number
-#define FCRevisionDate  "2010/10/05 18:46:56"     //Date of highest committed revision
-#define FCRevisionRange "3602:3607"    //Update revision range
-#define FCRepositoryURL "https://free-cad.svn.sourceforge.net/svnroot/free-cad/trunk/src"      //Repository URL of the working copy
-#define FCCurrentDateT  "2010/10/06 13:37:37"      //Current system date & time
+#define FCRevision      "3681"      //Highest committed revision number
+#define FCRevisionDate  "2010/11/01 09:40:38"     //Date of highest committed revision
+#define FCRevisionRange "3678:3681"    //Update revision range
+#define FCRepositoryURL "https://free-cad.svn.sourceforge.net/svnroot/free-cad/trunk"      //Repository URL of the working copy
+#define FCCurrentDateT  "2010/11/01 11:30:23"      //Current system date & time
 
 //Placeholders of the form "$WCxxx?TrueText:FalseText$" are replaced with
 //TrueText if the tested condition is true, and FalseText if false.
