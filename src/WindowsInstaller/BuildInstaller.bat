642a24664b8fd47c4f2b5048f300b97116125732
jriegel <jriegel>
2010-11-13 15:25:44 +0000

    + adding yoriks bitmaps to installer + add FEM to the installer and as a default build in CMakeLists.txt + new LibPack Version with all SMESH stuff

--- src/WindowsInstaller/BuildInstaller.bat
+++ src/WindowsInstaller/BuildInstaller.bat
@@ -16,6 +16,6 @@ SET /P M=Reebuild and press enter
 "%WIXDIR%\bin\candle.exe" -dProcessorArchitecture=%PLATFORM% -out FreeCADModules.wxobj FreeCADModules.wxs
 "%WIXDIR%\bin\candle.exe" -dProcessorArchitecture=%PLATFORM% -out FreeCAD.wxobj        FreeCAD.wxs
 
-"%WIXDIR%\bin\light.exe"  -ext WixUIExtension -sice:ICE03 -sice:ICE60 -sice:ICE82 -sice:ICE83 -cultures:en-us -out FreeCAD.msi *.wxobj
+"%WIXDIR%\bin\light.exe"  -dWixUIBannerBmp=Bitmaps/BanerBitmap.bmp -dWixUIDialogBmp=Bitmaps/BackgroundBitmap.bmp -ext WixUIExtension -sice:ICE03 -sice:ICE60 -sice:ICE82 -sice:ICE83 -cultures:en-us -out FreeCAD.msi *.wxobj 
 
 call CopyRelease.bat
