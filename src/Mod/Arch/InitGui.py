ac325b28ddf75415ea4dd9f22f560b9f8123233e
Yorik van Havre <yorik@uncreated.net>
2011-01-17 23:47:42 +0000

    + start of the Arch module

---
+++ src/Mod/Arch/InitGui.py
@@ -0,0 +1,43 @@
+class ArchWorkbench(Workbench):
+	"Arch workbench object"
+	Icon = """
+                /* XPM */
+                static char * arch_xpm[] = {
+                "16 16 9 1",
+                " 	c None",
+                ".	c #543016",
+                "+	c #6D2F08",
+                "@	c #954109",
+                "#	c #874C24",
+                "$	c #AE6331",
+                "%	c #C86423",
+                "&	c #FD7C26",
+                "*	c #F5924F",
+                "                ",
+                "                ",
+                "       #        ",
+                "      ***$#     ",
+                "    .*******.   ",
+                "   *##$****#+   ",
+                " #**%&&##$#@@   ",
+                ".$**%&&&&+@@+   ",
+                "@&@#$$%&&@@+..  ",
+                "@&&&%#.#$#+..#$.",
+                " %&&&&+%#.$**$@+",
+                "   @%&+&&&$##@@+",
+                "     @.&&&&&@@@ ",
+                "        @%&&@@  ",
+                "           @+   ",
+                "                "};
+			"""
+	MenuText = "Arch"
+	ToolTip = "Architecture workbench"
+	
+	def Initialize(self):
+		Log ('Loading Arch module... done\n')
+	def Activated(self):
+		Msg("ArchWorkbench::Activated()\n")
+	def Deactivated(self):
+		Msg("ArchWorkbench::Deactivated()\n")
+
+Gui.addWorkbench(ArchWorkbench)
