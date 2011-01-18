fa3dcd4098c1b9dd82df878ee535f87fc40e77ac
wmayer <wmayer@users.sourceforge.net>
2011-01-18 10:38:36 +0000

    0000244: dash fails to execute autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -25,7 +25,7 @@ else
 fi
 
 # create m4 subdirectory which fails for some older versions of autotools
-[[ -d m4 ]] || mkdir m4
+[ -d m4 ] || mkdir m4
 
 if which glibtoolize > /dev/null 2>&1; then
 	echo "calling glibtoolize"
