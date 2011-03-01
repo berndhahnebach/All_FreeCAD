01a83abb11dffa6c38378d19536b0ba06563d9a6
Yorik van Havre <yorik@uncreated.net>
2011-03-01 18:57:10 +0000

    + added simplified chinese (zh - 100%) and croatian (hr - 76%) translations

---
+++ src/Mod/Raytracing/Gui/Resources/translations/Raytracing_hr.ts
@@ -0,0 +1,237 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS>
+  <context>
+    <name>CmdRaytracingExportProject</name>
+    <message>
+      <location/>
+      <source>File</source>
+      <translation>Datoteka</translation>
+    </message>
+    <message>
+      <location/>
+      <source>&amp;Export project...</source>
+      <translation>&amp;Izvezi projekt...</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Export the Povray project file</source>
+      <translation>Export the Povray project file</translation>
+    </message>
+  </context>
+  <context>
+    <name>CmdRaytracingNewPartSegment</name>
+    <message>
+      <location/>
+      <source>Raytracing</source>
+      <translation>Raytracing</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Insert part</source>
+      <translation>Umetni dio</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Insert a new part object into a Povray project</source>
+      <translation>Insert a new part object into a Povray project</translation>
+    </message>
+  </context>
+  <context>
+    <name>CmdRaytracingNewPovrayProject</name>
+    <message>
+      <location/>
+      <source>Raytracing</source>
+      <translation>Raytracing</translation>
+    </message>
+    <message>
+      <location/>
+      <source>New Povray project</source>
+      <translation>New Povray project</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Insert new Povray project into the document</source>
+      <translation>Insert new Povray project into the document</translation>
+    </message>
+  </context>
+  <context>
+    <name>CmdRaytracingNewProject</name>
+    <message>
+      <location/>
+      <source>Raytracing</source>
+      <translation>Raytracing</translation>
+    </message>
+  </context>
+  <context>
+    <name>CmdRaytracingQuickRender</name>
+    <message>
+      <location/>
+      <source>Raytracing</source>
+      <translation>Raytracing</translation>
+    </message>
+  </context>
+  <context>
+    <name>CmdRaytracingWriteCamera</name>
+    <message>
+      <location/>
+      <source>Raytracing</source>
+      <translation>Raytracing</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Export camera to povray</source>
+      <translation>Export camera to povray</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Export the camera positon of the active 3D view in PovRay format to a file</source>
+      <translation>Export the camera positon of the active 3D view in PovRay format to a file</translation>
+    </message>
+  </context>
+  <context>
+    <name>CmdRaytracingWritePart</name>
+    <message>
+      <location/>
+      <source>Raytracing</source>
+      <translation>Raytracing</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Export part to povray...</source>
+      <translation>Izvoz dijela u povray ...</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Write the selected Part (object) as a povray file</source>
+      <translation>Napišite odabrani dio (objekta) kao povray datoteke</translation>
+    </message>
+  </context>
+  <context>
+    <name>CmdRaytracingWriteView</name>
+    <message>
+      <location/>
+      <source>Raytracing</source>
+      <translation>Raytracing</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Export view to povray...</source>
+      <translation>Pogled u povray ...</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Write the active 3D view with camera and all its content to a povray file</source>
+      <translation>Napišite aktivni 3D prikaz s kamerom i sve njezine sadržaje u povray datoteke</translation>
+    </message>
+  </context>
+  <context>
+    <name>QObject</name>
+    <message>
+      <location/>
+      <source>Povray(*.pov)</source>
+      <translation>Povray (*. pov)</translation>
+    </message>
+    <message>
+      <location/>
+      <source>All Files (*.*)</source>
+      <translation>Sve datoteke (*.*)</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Export page</source>
+      <translation>Izvezi stranicu</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Wrong selection</source>
+      <translation>Pogrešan odabir</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Select a Part object.</source>
+      <translation>Odaberite dio objekta.</translation>
+    </message>
+    <message>
+      <location/>
+      <source>No Povray project to insert</source>
+      <translation>Nema Povray projekta za umetanje</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Create a Povray project to insert a view.</source>
+      <translation>Napravite Povray projekt za umetanje pogleda.</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Select one Povray project object.</source>
+      <translation>Odaberite jedan objekt Povray projekta.</translation>
+    </message>
+  </context>
+  <context>
+    <name>RaytracingGui::DlgSettingsRay</name>
+    <message>
+      <location/>
+      <source>Raytracing</source>
+      <translation>Raytracing</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Directories</source>
+      <translation>Mape</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Default Project dir:</source>
+      <translation>Default Project dir:</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Camera file name:</source>
+      <translation>Camera file name:</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Part file name:</source>
+      <translation>Part file name:</translation>
+    </message>
+    <message>
+      <location/>
+      <source>TempCamera.inc</source>
+      <translation>TempCamera.inc</translation>
+    </message>
+    <message>
+      <location/>
+      <source>TempPart.inc</source>
+      <translation>TempPart.inc</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Mesh export settings</source>
+      <translation>Izvoz postavki mreže</translation>
+    </message>
+    <message>
+      <location/>
+      <source>max mesh deviation:</source>
+      <translation>max mesh deviation:</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Do not calculate vertex normals</source>
+      <translation>Do not calculate vertex normals</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Write u,v coordinates</source>
+      <translation>Napišite u,v koordinate</translation>
+    </message>
+  </context>
+  <context>
+    <name>RaytracingGui::Workbench</name>
+    <message>
+      <location/>
+      <source>&amp;Raytracing</source>
+      <translation>Metode trasiranja putem zrake</translation>
+    </message>
+  </context>
+</TS>
---
+++ src/Mod/Raytracing/Gui/Resources/translations/Raytracing_zh.ts
@@ -0,0 +1,237 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS>
+  <context>
+    <name>CmdRaytracingExportProject</name>
+    <message>
+      <location/>
+      <source>File</source>
+      <translation>文件</translation>
+    </message>
+    <message>
+      <location/>
+      <source>&amp;Export project...</source>
+      <translation>&amp;E 导出项目...</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Export the Povray project file</source>
+      <translation>导出 Povray 项目文件</translation>
+    </message>
+  </context>
+  <context>
+    <name>CmdRaytracingNewPartSegment</name>
+    <message>
+      <location/>
+      <source>Raytracing</source>
+      <translation>光线追踪</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Insert part</source>
+      <translation>插入零件</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Insert a new part object into a Povray project</source>
+      <translation>插入一个新的零件对象到PovRay项目</translation>
+    </message>
+  </context>
+  <context>
+    <name>CmdRaytracingNewPovrayProject</name>
+    <message>
+      <location/>
+      <source>Raytracing</source>
+      <translation>光线追踪</translation>
+    </message>
+    <message>
+      <location/>
+      <source>New Povray project</source>
+      <translation>新的 Povray 项目</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Insert new Povray project into the document</source>
+      <translation>将新的PovRay工程插入文档</translation>
+    </message>
+  </context>
+  <context>
+    <name>CmdRaytracingNewProject</name>
+    <message>
+      <location/>
+      <source>Raytracing</source>
+      <translation>光线追踪</translation>
+    </message>
+  </context>
+  <context>
+    <name>CmdRaytracingQuickRender</name>
+    <message>
+      <location/>
+      <source>Raytracing</source>
+      <translation>光线追踪</translation>
+    </message>
+  </context>
+  <context>
+    <name>CmdRaytracingWriteCamera</name>
+    <message>
+      <location/>
+      <source>Raytracing</source>
+      <translation>光线追踪</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Export camera to povray</source>
+      <translation>输出相机为PovRay文件</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Export the camera positon of the active 3D view in PovRay format to a file</source>
+      <translation>根据相机位置将当前3D视图输出为一个PovRay文件</translation>
+    </message>
+  </context>
+  <context>
+    <name>CmdRaytracingWritePart</name>
+    <message>
+      <location/>
+      <source>Raytracing</source>
+      <translation>光线追踪</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Export part to povray...</source>
+      <translation>输出零件为Povray...</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Write the selected Part (object) as a povray file</source>
+      <translation>将已选择零件(对象)写入Povray文件</translation>
+    </message>
+  </context>
+  <context>
+    <name>CmdRaytracingWriteView</name>
+    <message>
+      <location/>
+      <source>Raytracing</source>
+      <translation>光线追踪</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Export view to povray...</source>
+      <translation>视图输出为Povray...</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Write the active 3D view with camera and all its content to a povray file</source>
+      <translation>使用相机将当前3D动态视图以及文本写入到Povray文件</translation>
+    </message>
+  </context>
+  <context>
+    <name>QObject</name>
+    <message>
+      <location/>
+      <source>Povray(*.pov)</source>
+      <translation>Povray(*.pov)</translation>
+    </message>
+    <message>
+      <location/>
+      <source>All Files (*.*)</source>
+      <translation>所有文件(*.*)</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Export page</source>
+      <translation>导出页</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Wrong selection</source>
+      <translation>选择错误</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Select a Part object.</source>
+      <translation>选择零件对象.</translation>
+    </message>
+    <message>
+      <location/>
+      <source>No Povray project to insert</source>
+      <translation>没有Povray项目可插入</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Create a Povray project to insert a view.</source>
+      <translation>创建一个Povray项目以插入视图.</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Select one Povray project object.</source>
+      <translation>选择一个Povray项目.</translation>
+    </message>
+  </context>
+  <context>
+    <name>RaytracingGui::DlgSettingsRay</name>
+    <message>
+      <location/>
+      <source>Raytracing</source>
+      <translation>光线追踪</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Directories</source>
+      <translation>目录</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Default Project dir:</source>
+      <translation>默认的项目路径:</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Camera file name:</source>
+      <translation>相机文件的名称:</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Part file name:</source>
+      <translation>零件的名称:</translation>
+    </message>
+    <message>
+      <location/>
+      <source>TempCamera.inc</source>
+      <translation>TempCamera.inc</translation>
+    </message>
+    <message>
+      <location/>
+      <source>TempPart.inc</source>
+      <translation>TempPart.inc</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Mesh export settings</source>
+      <translation>网格的导出设置</translation>
+    </message>
+    <message>
+      <location/>
+      <source>max mesh deviation:</source>
+      <translation>最大网偏差:</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Do not calculate vertex normals</source>
+      <translation>不计算顶点法线</translation>
+    </message>
+    <message>
+      <location/>
+      <source>Write u,v coordinates</source>
+      <translation>写U，V坐标</translation>
+    </message>
+  </context>
+  <context>
+    <name>RaytracingGui::Workbench</name>
+    <message>
+      <location/>
+      <source>&amp;Raytracing</source>
+      <translation>&amp;R 光线追踪</translation>
+    </message>
+  </context>
+</TS>
