2974feff140be58e739941a33184f830d02da087
logari81 <logari81>
2011-03-31 20:30:34 +0000

    + untabify and coding style mprovements

--- src/Mod/Sketcher/App/sketchsolve_cp/errorfuncs.cpp
+++ src/Mod/Sketcher/App/sketchsolve_cp/errorfuncs.cpp
@@ -84,40 +84,40 @@ double VerticalError(std::vector<double> &parms)
 double PointOnPointError(std::vector<double> &parms)
 {
     //Hopefully avoid this constraint, make coincident points use the same parameters
-	double dx = parms[0] - parms[2];
-	double dy = parms[1] - parms[3];
+    double dx = parms[0] - parms[2];
+    double dy = parms[1] - parms[3];
     return dx*dx + dy*dy;
 }
 
 double P2PDistanceError(std::vector<double> &parms)
 {
-	double dx = parms[0] - parms[2];
-	double dy = parms[1] - parms[3];
-	double d = parms[4];
+    double dx = parms[0] - parms[2];
+    double dy = parms[1] - parms[3];
+    double d = parms[4];
     double err = dx*dx+dy*dy - d * d;
-	return err*err;
+    return err*err;
 }
 
 double P2PDistanceHorzError(std::vector<double> &parms)
 {
-	double dx = parms[0] - parms[2];
-	double d = parms[4];
+    double dx = parms[0] - parms[2];
+    double d = parms[4];
     double err = dx*dx - d * d;
-	return err*err;
+    return err*err;
 }
 
 double P2PDistanceVertError(std::vector<double> &parms)
 {
-	double dy = parms[1] - parms[3];
-	double d = parms[4];
+    double dy = parms[1] - parms[3];
+    double d = parms[4];
     double err = dy*dy - d * d;
-	return err * err;
+    return err * err;
 }
 
 double PointOnLineError(std::vector<double> &parms)
 {
-	double dx = parms[0] - parms[2];
-	double dy = parms[1] - parms[3];
+    double dx = parms[0] - parms[2];
+    double dy = parms[1] - parms[3];
 
     double m=dy/dx; //Slope
     double n=dx/dy; //1/Slope
@@ -138,87 +138,87 @@ double PointOnLineError(std::vector<double> &parms)
 
 double P2LDistanceE(double lx, double ly, double dx, double dy, double px, double py)
 {
-	double t=-(lx*dx-px*dx+ly*dy-py*dy)/(dx*dx+dy*dy);
-	double Xint=lx+dx*t;
-	double Yint=ly+dy*t;
-	double temp= sqrt((px - Xint)*(px - Xint)+(py - Yint)*(py - Yint));
-	return temp;
+    double t=-(lx*dx-px*dx+ly*dy-py*dy)/(dx*dx+dy*dy);
+    double Xint=lx+dx*t;
+    double Yint=ly+dy*t;
+    double temp= sqrt((px - Xint)*(px - Xint)+(py - Yint)*(py - Yint));
+    return temp;
 }
 
 double P2LDistanceError(std::vector<double> &parms)                      
 {
-	double dx = parms[0] - parms[2];
-	double dy = parms[1] - parms[3];
+    double dx = parms[0] - parms[2];
+    double dy = parms[1] - parms[3];
 
-	double radsq = parms[6] * parms[6];
+    double radsq = parms[6] * parms[6];
     double temp= P2LDistanceE(parms[0],parms[1],dx,dy,parms[4],parms[5]) - sqrt(radsq);
     return temp*temp*100;
 }
 
 double EllipseTangentError(std::vector<double> &parms)                      
 {
-	//double ldx = parms[0] - parms[2];
-	//double ldy = parms[1] - parms[3];
+    //double ldx = parms[0] - parms[2];
+    //double ldy = parms[1] - parms[3];
 
-	double ex = parms[4];
-	double ey = parms[5];
-	double majr = parms[6];
-	double minr = parms[7];
-	double rot = parms[8];
+    double ex = parms[4];
+    double ey = parms[5];
+    double majr = parms[6];
+    double minr = parms[7];
+    double rot = parms[8];
 
-	//find the major and minor axis
-	if(minr > majr)
-	{
-		double temp = majr;
-		majr = minr;
-		minr = temp;
-		rot += M_PI/2;
-	}
+    //find the major and minor axis
+    if(minr > majr)
+    {
+        double temp = majr;
+        majr = minr;
+        minr = temp;
+        rot += M_PI/2;
+    }
 
-	double t = majr;
-	majr = minr;
-	minr = t;
+    double t = majr;
+    majr = minr;
+    minr = t;
 
-	//majr*=2;
-	//minr*=2;
+    //majr*=2;
+    //minr*=2;
 
-	rot = -rot;
+    rot = -rot;
 
-	//rotate the line around the ellipse
-	double lx1 = cos(rot)*(parms[0]-ex) - sin(rot)*(parms[1]-ey);
-	double ly1 = sin(rot)*(parms[0]-ex) + cos(rot)*(parms[1]-ey);
-	double lx2 = cos(rot)*(parms[2]-ex) - sin(rot)*(parms[3]-ey);
-	double ly2 = sin(rot)*(parms[2]-ex) + cos(rot)*(parms[3]-ey);
+    //rotate the line around the ellipse
+    double lx1 = cos(rot)*(parms[0]-ex) - sin(rot)*(parms[1]-ey);
+    double ly1 = sin(rot)*(parms[0]-ex) + cos(rot)*(parms[1]-ey);
+    double lx2 = cos(rot)*(parms[2]-ex) - sin(rot)*(parms[3]-ey);
+    double ly2 = sin(rot)*(parms[2]-ex) + cos(rot)*(parms[3]-ey);
 
-	double ldx = lx1 - lx2;
-	double ldy = ly1 - ly2;
+    double ldx = lx1 - lx2;
+    double ldy = ly1 - ly2;
 
-	double g = (2 * ldx * lx1)/majr/majr+(2 * ldy * ly1)/minr / minr;
-	double det = g*g-4 * (ldx*ldx/majr/majr+ldy*ldy/minr/minr) * (lx1*lx1/majr/majr+ly1*ly1/minr/minr-1);
+    double g = (2 * ldx * lx1)/majr/majr+(2 * ldy * ly1)/minr / minr;
+    double det = g*g-4 * (ldx*ldx/majr/majr+ldy*ldy/minr/minr) * (lx1*lx1/majr/majr+ly1*ly1/minr/minr-1);
 
-	return det * det * 10;
+    return det * det * 10;
 
-/*	//calculate the eccentricity
-	double e = sqrt(majr * majr - minr * minr);
+/*  //calculate the eccentricity
+    double e = sqrt(majr * majr - minr * minr);
 
-	//Find the focal points
-	double f1x = ex + cos(rot) * e;
-	double f1y = ey + sin(rot) * e;
-	double f2x = ex - cos(rot) * e;
-	double f2y = ey - sin(rot) * e;
+    //Find the focal points
+    double f1x = ex + cos(rot) * e;
+    double f1y = ey + sin(rot) * e;
+    double f2x = ex - cos(rot) * e;
+    double f2y = ey - sin(rot) * e;
 
-	double d1 = P2LDistanceE(parms[0],parms[1],ldx,ldy,f1x,f1y);
-	double d2 = P2LDistanceE(parms[0],parms[1],ldx,ldy,f2x,f2y);
+    double d1 = P2LDistanceE(parms[0],parms[1],ldx,ldy,f1x,f1y);
+    double d2 = P2LDistanceE(parms[0],parms[1],ldx,ldy,f2x,f2y);
 
-	double err = d1 + d2 - 2 * majr;
-	return err * err * 1000;*/
+    double err = d1 + d2 - 2 * majr;
+    return err * err * 1000;*/
 }
 
 
 double P2LDistanceVertError(std::vector<double> &parms)
 {
-	double dx = parms[0] - parms[2];
-	double dy = parms[1] - parms[3];
+    double dx = parms[0] - parms[2];
+    double dy = parms[1] - parms[3];
 
     double t=(parms[4]- parms[0])/dx;
     double Yint=parms[1]+dy*t;
@@ -228,8 +228,8 @@ double P2LDistanceVertError(std::vector<double> &parms)
 
 double P2LDistanceHorzError(std::vector<double> &parms)
 {
-	double dx = parms[0] - parms[2];
-	double dy = parms[1] - parms[3];
+    double dx = parms[0] - parms[2];
+    double dy = parms[1] - parms[3];
 
     double t=(parms[5]- parms[1])/dy;
     double Xint=parms[0]+dx*t;
@@ -239,21 +239,20 @@ double P2LDistanceHorzError(std::vector<double> &parms)
 
 double LineLengthError(std::vector<double> &parms)
 {
-	double dx = parms[0] - parms[2];
-	double dy = parms[1] - parms[3];
+    double dx = parms[0] - parms[2];
+    double dy = parms[1] - parms[3];
     double temp= sqrt(dx*dx+dy*dy) - parms[4];
-	return temp*temp*100;
+    return temp*temp*100;
 }
-			
 
 double EqualLengthError(std::vector<double> &parms)
 {
-	double dx = parms[0] - parms[2];
-	double dy = parms[1] - parms[3];
-	double dx2 = parms[4] - parms[6];
-	double dy2 = parms[5] - parms[7];
+    double dx = parms[0] - parms[2];
+    double dy = parms[1] - parms[3];
+    double dx2 = parms[4] - parms[6];
+    double dy2 = parms[5] - parms[7];
 
-	double temp = sqrt(dx*dx+dy*dy) -  sqrt(dx2*dx2+dy2*dy2);
+    double temp = sqrt(dx*dx+dy*dy) -  sqrt(dx2*dx2+dy2*dy2);
     return temp*temp;
 }
 
@@ -265,23 +264,23 @@ double EqualScalarError(std::vector<double> &parms)
 
 double PointOnArcAngleError(std::vector<double> &parms)
 {
-	double a1x = cos(parms[5]) * parms[4] + parms[2];
-	double a1y = sin(parms[5]) * parms[4] + parms[3];
-	double dx = parms[0] - a1x;
-	double dy = parms[1] - a1y;
+    double a1x = cos(parms[5]) * parms[4] + parms[2];
+    double a1y = sin(parms[5]) * parms[4] + parms[3];
+    double dx = parms[0] - a1x;
+    double dy = parms[1] - a1y;
     return dx*dx + dy*dy;
 }
 
 double ArcAngleOnArcAngleError(std::vector<double> &parms)
 {
-	double a1x = cos(parms[3]) * parms[2] + parms[0];
-	double a1y = sin(parms[3]) * parms[2] + parms[1];
+    double a1x = cos(parms[3]) * parms[2] + parms[0];
+    double a1y = sin(parms[3]) * parms[2] + parms[1];
 
-	double a2x = cos(parms[7]) * parms[6] + parms[4];
-	double a2y = sin(parms[7]) * parms[6] + parms[5];
+    double a2x = cos(parms[7]) * parms[6] + parms[4];
+    double a2y = sin(parms[7]) * parms[6] + parms[5];
 
-	double dx = a2x - a1x;
-	double dy = a2y - a1y;
+    double dx = a2x - a1x;
+    double dy = a2y - a1y;
     return dx*dx + dy*dy;
 }
 
@@ -295,7 +294,7 @@ double ColinearError(std::vector<double>& parms)
     // Calculate the error between the expected intersection point
     // and the true point of the second lines two end points on the
     // first line
-	double error=0;
+    double error=0;
     if(m<=1 && m>-1)
     {
         //Calculate the expected y point given the x coordinate of the point
@@ -312,15 +311,15 @@ double ColinearError(std::vector<double>& parms)
         Ex=parms[0]+n*(parms[7]-parms[1]);
         error+=(Ex-parms[6])*(Ex-parms[6]);
     }
-	return error;
+    return error;
 }
 
 double LinePerpToAngleError(std::vector<double>& parms)
 {
-	double dx = parms[0] - parms[2];
-	double dy = parms[1] - parms[3];
+    double dx = parms[0] - parms[2];
+    double dy = parms[1] - parms[3];
 
-	double dx2 = sin(parms[4]);
+    double dx2 = sin(parms[4]);
     double dy2 = cos(parms[4]);
 
     double hyp1=sqrt(dx*dx+dy*dy);
--- src/Mod/Sketcher/App/sketchsolve_cp/solve.h
+++ src/Mod/Sketcher/App/sketchsolve_cp/solve.h
@@ -12,7 +12,7 @@
 #include <list>
 #include <vector>
 #ifndef WIN32
-        #define _hypot hypot
+    #define _hypot hypot
 #endif
 
 //This define selects between the old arc define by start point end point and center point
@@ -30,56 +30,56 @@
 
 enum constraintType
 {
-pointOnPoint,
-pointToLine,    
-pointOnLine,     
-horizontal,      
-vertical,        
-internalAngle,   
-radiusValue,     
-tangentToArc,    
-tangentToArcStart,
-tangentToArcEnd,
-tangentToCircle, 
-arcRules,        
-P2PDistance,     
-P2PDistanceVert,  
-P2PDistanceHorz,  
-P2LDistance,      
-P2LDistanceVert,  
-P2LDistanceHorz,  
-lineLength,        
-equalLegnth,       
-arcRadius,         
-equalRadiusArcs,   
-equalRadiusCircles,
-equalRadiusCircArc,
-concentricArcs,   
-concentricCircles,
-concentricCircArc,
-circleRadius,     
-externalAngle,    
-parallel,         
-perpendicular,    
-colinear,         
-pointOnCircle,    
-pointOnArc,       
-pointOnLineMidpoint,
-pointOnArcMidpoint, 
-pointOnCircleQuad,  
-symmetricPoints,    
-symmetricLines,     
-symmetricCircles,   
-symmetricArcs,        
-pointOnArcStart,      
-pointOnArcEnd,        
-arcStartToArcEnd,     
-arcStartToArcStart,   
-arcEndToArcEnd,       
-arcTangentToArc,       
-circleTangentToCircle, 
-circleTangentToArc,
-tangentToEllipse
+    pointOnPoint,
+    pointToLine,
+    pointOnLine,
+    horizontal,
+    vertical,
+    internalAngle,
+    radiusValue,
+    tangentToArc,
+    tangentToArcStart,
+    tangentToArcEnd,
+    tangentToCircle,
+    arcRules,
+    P2PDistance,
+    P2PDistanceVert,
+    P2PDistanceHorz,
+    P2LDistance,
+    P2LDistanceVert,
+    P2LDistanceHorz,
+    lineLength,
+    equalLegnth,
+    arcRadius,
+    equalRadiusArcs,
+    equalRadiusCircles,
+    equalRadiusCircArc,
+    concentricArcs,
+    concentricCircles,
+    concentricCircArc,
+    circleRadius,
+    externalAngle,
+    parallel,
+    perpendicular,
+    colinear,
+    pointOnCircle,
+    pointOnArc,
+    pointOnLineMidpoint,
+    pointOnArcMidpoint,
+    pointOnCircleQuad,
+    symmetricPoints,
+    symmetricLines,
+    symmetricCircles,
+    symmetricArcs,
+    pointOnArcStart,
+    pointOnArcEnd,
+    arcStartToArcEnd,
+    arcStartToArcStart,
+    arcEndToArcEnd,
+    arcTangentToArc,
+    circleTangentToCircle,
+    circleTangentToArc,
+    tangentToEllipse
 };
 
 ///////////////////////////////////////
@@ -100,73 +100,73 @@ tangentToEllipse
 /// Solve exit codes
 ///////////////////////////////////////
 
-#define succsess 0
+#define success 0
 #define noSolution 1
 
 class point
 {
 public:
-        point(){x = 0; y = 0;}
-        double * x;
-        double * y;
+    point(){x = 0; y = 0;}
+    double *x;
+    double *y;
 };
 
 class line
 {
 public:
-        line(){}
-        point p1;
-        point p2;
+    line(){}
+    point p1;
+    point p2;
 };
 
 
 class arc
 {
-	public:
-	arc(){startAngle=0;endAngle=0;rad=0;}
-	double *startAngle;
-	double *endAngle;
-	double *rad;//This is called parameter in the constraint class.
-	point start;
-	point end;
-	point center;
+    public:
+    arc(){startAngle=0;endAngle=0;rad=0;}
+    double *startAngle;
+    double *endAngle;
+    double *rad;//This is called parameter in the constraint class.
+    point start;
+    point end;
+    point center;
 };
 
 class circle
 {
 public:
-        circle(){rad = 0;}
-        point center;
-        double *rad;
+    circle(){rad = 0;}
+    point center;
+    double *rad;
 };
 
 class ellipse
 {
 public:
-        ellipse(){radone = 0;radtwo=0;rot=0;}
-        point center;
-        double *radone;
-		double *radtwo;
-		double *rot;
+    ellipse(){radone = 0;radtwo=0;rot=0;}
+    point center;
+    double *radone;
+    double *radtwo;
+    double *rot;
 };
 
 class constraint
 {
 public:
-        constraint(){parameter = 0;}
-        constraintType type;
-        point point1;
-        point point2;
-        line line1;
-        line line2;
-        line SymLine;
-        circle circle1;
-        circle circle2;
-		ellipse ellipse1;
-		ellipse ellipse2;
-        arc arc1;
-        arc arc2;
-        double *parameter; //radius, length, angle etc...
+    constraint(){parameter = 0;}
+    constraintType type;
+    point point1;
+    point point2;
+    line line1;
+    line line2;
+    line SymLine;
+    circle circle1;
+    circle circle2;
+    ellipse ellipse1;
+    ellipse ellipse2;
+    arc arc1;
+    arc arc2;
+    double *parameter; //radius, length, angle etc...
 };
 
 
@@ -175,150 +175,150 @@ void debugprint(std::string s);
 
 enum varLocation
 {
-   Vector,
-   Static
+    Vector,
+    Static
 };
 
 enum dependencyType
 {
-   line1,
-   line2,
-   point1,
-   point2,
-   parameter,
-   arc1,
-   arc2,
-   circle1,
-   circle2,
-   line1_p1,
-   line1_p2,
-   line2_p1,
-   line2_p2,
-   arc1_center,
-   arc2_center,
-   circle1_center,
-   circle2_center,
-   line1_p1_x,
-   line1_p1_y,
-   line1_p2_x,
-   line1_p2_y,
-   line2_p1_x,
-   line2_p1_y,
-   line2_p2_x,
-   line2_p2_y,
-   arc1_center_x,
-   arc1_center_y,
-   arc2_center_x,
-   arc2_center_y,
-   circle1_center_x,
-   circle1_center_y,
-   circle2_center_x,
-   circle2_center_y,
-   arc1_rad,
-   arc2_rad,
-   circle1_rad,
-   circle2_rad,
-   arc1_startAngle,
-   arc1_endAngle,
-   arc2_startAngle,
-   arc2_endAngle,
-   ellipse1,
-   ellipse2,
-   ellipse1_center,
-   ellipse2_center,
-   ellipse1_center_x,
-   ellipse1_center_y,
-   ellipse2_center_x,
-   ellipse2_center_y,
-   ellipse1_rad1,
-   ellipse2_rad1,
-   ellipse1_rad2,
-   ellipse2_rad2,
-   ellipse1_rot,
-   ellipse2_rot
+    line1,
+    line2,
+    point1,
+    point2,
+    parameter,
+    arc1,
+    arc2,
+    circle1,
+    circle2,
+    line1_p1,
+    line1_p2,
+    line2_p1,
+    line2_p2,
+    arc1_center,
+    arc2_center,
+    circle1_center,
+    circle2_center,
+    line1_p1_x,
+    line1_p1_y,
+    line1_p2_x,
+    line1_p2_y,
+    line2_p1_x,
+    line2_p1_y,
+    line2_p2_x,
+    line2_p2_y,
+    arc1_center_x,
+    arc1_center_y,
+    arc2_center_x,
+    arc2_center_y,
+    circle1_center_x,
+    circle1_center_y,
+    circle2_center_x,
+    circle2_center_y,
+    arc1_rad,
+    arc2_rad,
+    circle1_rad,
+    circle2_rad,
+    arc1_startAngle,
+    arc1_endAngle,
+    arc2_startAngle,
+    arc2_endAngle,
+    ellipse1,
+    ellipse2,
+    ellipse1_center,
+    ellipse2_center,
+    ellipse1_center_x,
+    ellipse1_center_y,
+    ellipse2_center_x,
+    ellipse2_center_y,
+    ellipse1_rad1,
+    ellipse2_rad1,
+    ellipse1_rad2,
+    ellipse2_rad2,
+    ellipse1_rot,
+    ellipse2_rot
 };
 
 class SolveImpl;
 
 class SolveImpl
 {
-	std::vector<double(*)(std::vector<double>&)> errors;
-	std::vector<std::vector<dependencyType> > dependencies;
-	std::set<constraintType> depset;
-	std::vector<std::vector<std::pair<varLocation,void*> > > constraintvars;
-	std::vector<double* > myvec;
-	std::vector<constraintType> constrainttypes;
-	std::map<double*,std::pair<varLocation,void*> > mapparms;
-	std::map<int,std::vector<int> > vecmap;
-	std::set<double*> mapset;
-	std::vector<double> pvec;
-	int next_vector;
+    std::vector<double(*)(std::vector<double>&)> errors;
+    std::vector<std::vector<dependencyType> > dependencies;
+    std::set<constraintType> depset;
+    std::vector<std::vector<std::pair<varLocation,void*> > > constraintvars;
+    std::vector<double*> myvec;
+    std::vector<constraintType> constrainttypes;
+    std::map<double*,std::pair<varLocation,void*> > mapparms;
+    std::map<int,std::vector<int> > vecmap;
+    std::set<double*> mapset;
+    std::vector<double> pvec;
+    int next_vector;
 
-	void LoadDouble(std::vector<std::pair<varLocation,void*> > &mylist, double *d, int c);
-	void LoadPoint(std::vector<std::pair<varLocation,void*> > &mylist, point p, int c);
-	void LoadLine(std::vector<std::pair<varLocation,void*> > &mylist,line l, int c);
-	void LoadArc(std::vector<std::pair<varLocation,void*> > &mylist,arc a, int c);
-	void LoadCircle(std::vector<std::pair<varLocation,void*> > &mylist,circle c, int con);
-	void LoadEllipse(std::vector<std::pair<varLocation,void*> > &mylist,ellipse e, int con);
-	void registerconstraint(constraintType,double(*)(std::vector<double>&));
-	void registerdependency(constraintType,dependencyType);
-	double GetErrorForGrad(int i);
+    void LoadDouble(std::vector<std::pair<varLocation,void*> > &mylist, double *d, int c);
+    void LoadPoint(std::vector<std::pair<varLocation,void*> > &mylist, point p, int c);
+    void LoadLine(std::vector<std::pair<varLocation,void*> > &mylist, line l, int c);
+    void LoadArc(std::vector<std::pair<varLocation,void*> > &mylist, arc a, int c);
+    void LoadCircle(std::vector<std::pair<varLocation,void*> > &mylist, circle c, int con);
+    void LoadEllipse(std::vector<std::pair<varLocation,void*> > &mylist, ellipse e, int con);
+    void registerconstraint(constraintType,double(*)(std::vector<double>&));
+    void registerdependency(constraintType,dependencyType);
+    double GetErrorForGrad(int i);
 
 protected:
-	std::map<double*,int> parms;
+    std::map<double*,int> parms;
 
 public:
-	SolveImpl();
-	~SolveImpl();
+    SolveImpl();
+    ~SolveImpl();
 
-	void Load(constraint* c, int nconstraints, double** p, int nparms);
-	void Load(constraint &c);
-	void Unload();
-	double GetError();
-	double GetError(int i);
+    void Load(constraint *c, int nconstraints, double **p, int nparms);
+    void Load(constraint &c);
+    void Unload();
+    double GetError();
+    double GetError(int i);
 
-	int GetVectorSize() const;
-	double GetInitialValue(int i);
-	double GetGradient(int i, double pert);
-	virtual double GetElement(size_t i) =0; //Pure virtual
-	virtual void SetElement(size_t i, double v) = 0;
-	virtual int solve(double  **x,int xLength, constraint * cons, int consLength, int isFine) = 0;
+    int GetVectorSize() const;
+    double GetInitialValue(int i);
+    double GetGradient(int i, double pert);
+    virtual double GetElement(size_t i) = 0; //Pure virtual
+    virtual void SetElement(size_t i, double v) = 0;
+    virtual int solve(double **x, int xLength, constraint *cons, int consLength, int isFine) = 0;
 };
 
 class Solver: public SolveImpl
 {
-	int xLength;
-	std::vector<double> x;
-	double **xsave;
-	std::vector<double> origSolution;
-	std::vector<double> grad;
-	std::vector<double> s;
-	std::vector<std::vector<double> > N;
-	std::vector<double> xold;
-	std::vector<double> deltaX;
-	std::vector<double> gradnew;
-	std::vector<double> gamma;
-	std::vector<double> gammatDotN;
-	std::vector<std::vector<double> > FirstSecond;
+    int xLength;
+    std::vector<double> x;
+    double **xsave;
+    std::vector<double> origSolution;
+    std::vector<double> grad;
+    std::vector<double> s;
+    std::vector<std::vector<double> > N;
+    std::vector<double> xold;
+    std::vector<double> deltaX;
+    std::vector<double> gradnew;
+    std::vector<double> gamma;
+    std::vector<double> gammatDotN;
+    std::vector<std::vector<double> > FirstSecond;
     std::vector<std::vector<double> > deltaXDotGammatDotN;
     std::vector<std::vector<double> > gammatDotDeltaXt;
     std::vector<std::vector<double> > NDotGammaDotDeltaXt;
 
-	void allocate(int xLength);
-	int solveI(double  **x,int xLength, constraint * cons, int consLength, int isFine);
-	void deallocate();
+    void allocate(int xLength);
+    int solveI(double **x, int xLength, constraint *cons, int consLength, int isFine);
+    void deallocate();
 public:
-	Solver();
-	~Solver();
-	
-	int solve(double  **x,int xLength, constraint * cons, int consLength, int isFine);
-	double GetElement(size_t i){return x[i];}
-	void SetElement(size_t i, double v) { x[i] = v;}
+    Solver();
+    ~Solver();
+
+    int solve(double **x, int xLength, constraint *cons, int consLength, int isFine);
+    double GetElement(size_t i) {return x[i];}
+    void SetElement(size_t i, double v) { x[i] = v;}
 };
 
 //Function Prototypes
-double calc(constraint * cons, int consLength);
-void derivatives(double **x,double *gradF,int xLength, constraint * cons, int consLength);
+double calc(constraint *cons, int consLength);
+void derivatives(double **x, double *gradF, int xLength, constraint *cons, int consLength);
 
 //Error functions
 double HorizontalError(std::vector<double> &parms);
