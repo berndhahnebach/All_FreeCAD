6fccf325f06690314f1aafd1963413b470475cdf
wmayer <wmayer>
2011-08-07 09:01:11 +0000

    + replace __object__ with __vobject__

--- src/Tools/DocumentObject.py
+++ src/Tools/DocumentObject.py
@@ -72,7 +72,7 @@ class DocumentObject(object):
 
 class ViewProvider(object):
     def __init__(self):
-        self.__object__=None
+        self.__vobject__=None
     #def getIcon(self):
     #    return ""
     #def claimChildren(self):
@@ -88,7 +88,7 @@ class ViewProvider(object):
     #def onChanged(self, prop):
     #    return None
     def addDisplayMode(self,node,mode):
-        self.__object__.addDisplayMode(node,mode)
+        self.__vobject__.addDisplayMode(node,mode)
     #def getDefaultDisplayMode(self):
     #    return ""
     #def getDisplayModes(self):
@@ -96,66 +96,66 @@ class ViewProvider(object):
     #def setDisplayMode(self):
     #    return None
     def addProperty(self,type,name='',group='',doc='',attr=0,readonly=False,hidden=False):
-        self.__object__.addProperty(type,name,group,doc,attr,readonly,hidden)
+        self.__vobject__.addProperty(type,name,group,doc,attr,readonly,hidden)
     def update(self):
-        self.__object__.update()
+        self.__vobject__.update()
     def show(self):
-        self.__object__.show()
+        self.__vobject__.show()
     def hide(self):
-        self.__object__.hide()
+        self.__vobject__.hide()
     def isVisible(self):
-        return self.__object__.isVisible()
+        return self.__vobject__.isVisible()
     def toString(self):
-        return self.__object__.toString()
+        return self.__vobject__.toString()
     def startEditing(self,mode=0):
-        return self.__object__.startEditing(mode)
+        return self.__vobject__.startEditing(mode)
     def finishEditing(self):
-        self.__object__.finishEditing()
+        self.__vobject__.finishEditing()
     def isEditing(self):
-        self.__object__.isEditing()
+        self.__vobject__.isEditing()
     def setTransformation(self,trsf):
-        return self.__object__.setTransformation(trsf)
+        return self.__vobject__.setTransformation(trsf)
     def supportedProperties(self):
-        return self.__object__.supportedProperties()
+        return self.__vobject__.supportedProperties()
     def isDerivedFrom(self, obj):
-        return self.__object__.isDerivedFrom(obj)
+        return self.__vobject__.isDerivedFrom(obj)
     def getAllDerivedFrom(self):
-        return self.__object__.getAllDerivedFrom()
+        return self.__vobject__.getAllDerivedFrom()
     def getProperty(self,attr):
-        return self.__object__.getPropertyByName(attr)
+        return self.__vobject__.getPropertyByName(attr)
     def getTypeOfProperty(self,attr):
-        return self.__object__.getTypeOfProperty(attr)
+        return self.__vobject__.getTypeOfProperty(attr)
     def getGroupOfProperty(self,attr):
-        return self.__object__.getGroupOfProperty(attr)
+        return self.__vobject__.getGroupOfProperty(attr)
     def getDocumentationOfProperty(self,attr):
-        return self.__object__.getDocumentationOfProperty(attr)
+        return self.__vobject__.getDocumentationOfProperty(attr)
     @property
     def Annotation(self):
-        return self.__object__.Annotation
+        return self.__vobject__.Annotation
     @property
     def RootNode(self):
-        return self.__object__.RootNode
+        return self.__vobject__.RootNode
     @property
     def DisplayModes(self):
-        return self.__object__.listDisplayModes()
+        return self.__vobject__.listDisplayModes()
     @property
     def PropertiesList(self):
-        return self.__object__.PropertiesList
+        return self.__vobject__.PropertiesList
     @property
     def Type(self):
-        return self.__object__.Type
+        return self.__vobject__.Type
     @property
     def Module(self):
-        return self.__object__.Module
+        return self.__vobject__.Module
     @property
     def Content(self):
-        return self.__object__.Content
+        return self.__vobject__.Content
     @property
     def MemSize(self):
-        return self.__object__.MemSize
+        return self.__vobject__.MemSize
     @property
     def Object(self):
-        return self.__object__.Object
+        return self.__vobject__.Object
 
 def testMethod():
     class MyFeature(DocumentObject):
