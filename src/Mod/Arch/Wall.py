7e4061f86e316b723182b16ece28f2b0f5f4498d
Yorik van Havre <yorik@uncreated.net>
2011-09-23 23:38:45 +0000

    + allow Arch Wall without base shape

--- src/Mod/Arch/Wall.py
+++ src/Mod/Arch/Wall.py
@@ -65,22 +65,24 @@ class CommandWall:
                 makeWall(obj)
             FreeCAD.ActiveDocument.commitTransaction()
         else:
-            line = Draft.makeWire([Vector(0,0,0),Vector(0,1,0)])
-            wall = makeWall(line)
+            wall = makeWall()
        
 class Wall(Component.Component):
     "The Wall object"
     def __init__(self,obj):
         Component.Component.__init__(self,obj)
         obj.addProperty("App::PropertyLength","Width","Base",
-                        "The width of this wall")
+                        "The width of this wall. Not used if this wall is based on a face")
         obj.addProperty("App::PropertyLength","Height","Base",
-                        "The height of this wall. Keep 0 for automatic")
+                        "The height of this wall. Keep 0 for automatic. Not used if this wall is based on a solid")
+        obj.addProperty("App::PropertyLength","Length","Base",
+                        "The length of this wall. Not used if this wall is based on a shape")
         obj.addProperty("App::PropertyEnumeration","Align","Base",
                         "The alignment of this wall on its base object, if applicable")
         obj.Align = ['Left','Right','Center']
         self.Type = "Wall"
         obj.Width = 0.1
+        obj.Length = 1
         obj.Height = 0
         
     def execute(self,obj):
@@ -91,6 +93,31 @@ class Wall(Component.Component):
             self.createGeometry(obj)
 
     def createGeometry(self,obj):
+        
+        def getbase(wire):
+            "returns a full shape from a base wire"
+            dvec = fcgeo.vec(wire.Edges[0]).cross(normal)
+            dvec.normalize()
+            if obj.Align == "Left":
+                dvec = dvec.multiply(obj.Width)
+                w2 = fcgeo.offsetWire(wire,dvec)
+                sh = fcgeo.bind(wire,w2)
+            elif obj.Align == "Right":
+                dvec = dvec.multiply(obj.Width)
+                dvec = fcvec.neg(dvec)
+                w2 = fcgeo.offsetWire(wire,dvec)
+                sh = fcgeo.bind(wire,w2)
+            elif obj.Align == "Center":
+                dvec = dvec.multiply(obj.Width/2)
+                w1 = fcgeo.offsetWire(wire,dvec)
+                dvec = fcvec.neg(dvec)
+                w2 = fcgeo.offsetWire(wire,dvec)
+                sh = fcgeo.bind(w1,w2)
+            if height:
+                norm = Vector(normal).multiply(height)
+                sh = sh.extrude(norm)
+            return sh
+        
         pl = obj.Placement
 
         # getting default values
@@ -120,40 +147,29 @@ class Wall(Component.Component):
                 elif base.Wires:
                     temp = None
                     for wire in obj.Base.Shape.Wires:
-                        dvec = fcgeo.vec(wire.Edges[0]).cross(normal)
-                        dvec.normalize()
-                        if obj.Align == "Left":
-                            dvec = dvec.multiply(obj.Width)
-                            w2 = fcgeo.offsetWire(wire,dvec)
-                            sh = fcgeo.bind(wire,w2)
-                        elif obj.Align == "Right":
-                            dvec = dvec.multiply(obj.Width)
-                            dvec = fcvec.neg(dvec)
-                            w2 = fcgeo.offsetWire(wire,dvec)
-                            sh = fcgeo.bind(wire,w2)
-                        elif obj.Align == "Center":
-                            dvec = dvec.multiply(obj.Width/2)
-                            w1 = fcgeo.offsetWire(wire,dvec)
-                            dvec = fcvec.neg(dvec)
-                            w2 = fcgeo.offsetWire(wire,dvec)
-                            sh = fcgeo.bind(w1,w2)
-                        if height:
-                            norm = Vector(normal).multiply(height)
-                            sh = sh.extrude(norm)
-                            print sh,sh.Edges,dir(sh)
+                        sh = getbase(wire)
                         if temp:
                             temp = temp.oldFuse(sh)
                         else:
                             temp = sh
                     base = temp
-                for app in obj.Additions:
-                    base = base.oldFuse(app.Shape)
-                    app.ViewObject.hide() #to be removed
-                for hole in obj.Subtractions:
-                    base = base.cut(hole.Shape)
-                    hole.ViewObject.hide() # to be removed
-                obj.Shape = base
-                if not fcgeo.isNull(pl): obj.Placement = pl
+        else:
+            if obj.Length == 0:
+                return
+            v1 = Vector(0,0,0)
+            v2 = Vector(obj.Length,0,0)
+            w = Part.Wire(Part.Line(v1,v2).toShape())
+            base = getbase(w)
+            
+        for app in obj.Additions:
+            base = base.oldFuse(app.Shape)
+            app.ViewObject.hide() #to be removed
+        for hole in obj.Subtractions:
+            base = base.cut(hole.Shape)
+            hole.ViewObject.hide() # to be removed
+        obj.Shape = base
+        if not fcgeo.isNull(pl):
+            obj.Placement = pl
 
 class ViewProviderWall(Component.ViewProviderComponent):
     "A View Provider for the Wall object"
