DeepSOIC/FreeCAD-ellipse

PartDesign: accomodating to sketch's multisupport
* FeatureSketchBased getSupportFace check for mapping mode
* Fixes failure to place sketches on DatumPlanes

src/Mod/PartDesign/App/FeatureSketchBased.cpp
@@ -191,7 +191,7 @@ std::vector<TopoDS_Wire> SketchBased::getSketchWires() const {
 // this method, it becomes null!
 const TopoDS_Face SketchBased::getSupportFace() const {
     const Part::Part2DObject* sketch = getVerifiedSketch();
-    if (sketch->Support.getValue()) {
+    if (sketch->MapMode.getValue() == Part::mmFlatFace  &&  sketch->Support.getValue()) {
         const auto &Support = sketch->Support;
         App::DocumentObject* ref = Support.getValue();
 

berndhahnebach/All_FreeCAD

src/Mod/PartDesign/App/FeatureSketchBased.cpp
@@ -191,7 +191,7 @@ std::vector<TopoDS_Wire> SketchBased::getSketchWires() const {
 // this method, it becomes null!
 const TopoDS_Face SketchBased::getSupportFace() const {
     const Part::Part2DObject* sketch = getVerifiedSketch();
-    if (sketch->Support.getValue()) {
+    if (sketch->MapMode.getValue() == Attacher::mmFlatFace  &&  sketch->Support.getValue()) {
         const auto &Support = sketch->Support;
         App::DocumentObject* ref = Support.getValue();
 