FreeCAD/FreeCAD

Fixed bugs in various features that reversed the list of selections

src/Mod/PartDesign/Gui/TaskLinearPatternParameters.cpp
@@ -132,7 +132,7 @@ void TaskLinearPatternParameters::setupUI()
     for (std::vector<App::DocumentObject*>::const_iterator i = originals.begin(); i != originals.end(); i++)
     {
         if ((*i) != NULL)
-            ui->listWidgetFeatures->insertItem(0, QString::fromAscii((*i)->getNameInDocument()));
+            ui->listWidgetFeatures->addItem(QString::fromAscii((*i)->getNameInDocument()));
     }
     // ---------------------
 
@@ -227,7 +227,7 @@ void TaskLinearPatternParameters::onSelectionChanged(const Gui::SelectionChanges
     if (msg.Type == Gui::SelectionChanges::AddSelection) {
         if (originalSelected(msg)) {
             if (selectionMode == addFeature)
-                ui->listWidgetFeatures->insertItem(0, QString::fromAscii(msg.pObjectName));
+                ui->listWidgetFeatures->addItem(QString::fromAscii(msg.pObjectName));
             else
                 removeItemFromListWidget(ui->listWidgetFeatures, msg.pObjectName);
 

src/Mod/PartDesign/Gui/TaskMirroredParameters.cpp
@@ -118,7 +118,7 @@ void TaskMirroredParameters::setupUI()
     for (std::vector<App::DocumentObject*>::const_iterator i = originals.begin(); i != originals.end(); i++)
     {
         if ((*i) != NULL)
-            ui->listWidgetFeatures->insertItem(0, QString::fromAscii((*i)->getNameInDocument()));
+            ui->listWidgetFeatures->addItem(QString::fromAscii((*i)->getNameInDocument()));
     }
     // ---------------------
 
@@ -192,7 +192,7 @@ void TaskMirroredParameters::onSelectionChanged(const Gui::SelectionChanges& msg
 
         if (originalSelected(msg)) {
             if (selectionMode == addFeature)
-                ui->listWidgetFeatures->insertItem(0, QString::fromAscii(msg.pObjectName));
+                ui->listWidgetFeatures->addItem(QString::fromAscii(msg.pObjectName));
             else
                 removeItemFromListWidget(ui->listWidgetFeatures, msg.pObjectName);
             exitSelectionMode();

src/Mod/PartDesign/Gui/TaskMultiTransformParameters.cpp
@@ -142,7 +142,7 @@ TaskMultiTransformParameters::TaskMultiTransformParameters(ViewProviderTransform
     for (std::vector<App::DocumentObject*>::const_iterator i = originals.begin(); i != originals.end(); i++)
     {
         if ((*i) != NULL)
-            ui->listWidgetFeatures->insertItem(0, QString::fromAscii((*i)->getNameInDocument()));
+            ui->listWidgetFeatures->addItem(QString::fromAscii((*i)->getNameInDocument()));
     }
     // ---------------------
 }
@@ -151,7 +151,7 @@ void TaskMultiTransformParameters::onSelectionChanged(const Gui::SelectionChange
 {
     if (originalSelected(msg)) {
         if (selectionMode == addFeature)
-            ui->listWidgetFeatures->insertItem(0, QString::fromAscii(msg.pObjectName));
+            ui->listWidgetFeatures->addItem(QString::fromAscii(msg.pObjectName));
         else
             removeItemFromListWidget(ui->listWidgetFeatures, msg.pObjectName);
         exitSelectionMode();

src/Mod/PartDesign/Gui/TaskPolarPatternParameters.cpp
@@ -131,7 +131,7 @@ void TaskPolarPatternParameters::setupUI()
     for (std::vector<App::DocumentObject*>::const_iterator i = originals.begin(); i != originals.end(); i++)
     {
         if ((*i) != NULL)
-            ui->listWidgetFeatures->insertItem(0, QString::fromAscii((*i)->getNameInDocument()));
+            ui->listWidgetFeatures->addItem(QString::fromAscii((*i)->getNameInDocument()));
     }
     // ---------------------
 
@@ -201,7 +201,7 @@ void TaskPolarPatternParameters::onSelectionChanged(const Gui::SelectionChanges&
 
         if (originalSelected(msg)) {
             if (selectionMode == addFeature)
-                ui->listWidgetFeatures->insertItem(0, QString::fromAscii(msg.pObjectName));
+                ui->listWidgetFeatures->addItem(QString::fromAscii(msg.pObjectName));
             else
                 removeItemFromListWidget(ui->listWidgetFeatures, msg.pObjectName);
             exitSelectionMode();

src/Mod/PartDesign/Gui/TaskScaledParameters.cpp
@@ -113,7 +113,7 @@ void TaskScaledParameters::setupUI()
     for (std::vector<App::DocumentObject*>::const_iterator i = originals.begin(); i != originals.end(); i++)
     {
         if ((*i) != NULL)
-            ui->listWidgetFeatures->insertItem(0, QString::fromAscii((*i)->getNameInDocument()));
+            ui->listWidgetFeatures->addItem(QString::fromAscii((*i)->getNameInDocument()));
     }
     // ---------------------
 
@@ -144,7 +144,7 @@ void TaskScaledParameters::onSelectionChanged(const Gui::SelectionChanges& msg)
 {
     if (originalSelected(msg)) {
         if (selectionMode == addFeature)
-            ui->listWidgetFeatures->insertItem(0, QString::fromAscii(msg.pObjectName));
+            ui->listWidgetFeatures->addItem(QString::fromAscii(msg.pObjectName));
         else
             removeItemFromListWidget(ui->listWidgetFeatures, msg.pObjectName);
         exitSelectionMode();

berndhahnebach/All_FreeCAD

src/Mod/PartDesign/Gui/TaskLinearPatternParameters.cpp
@@ -133,7 +133,7 @@ void TaskLinearPatternParameters::setupUI()
     for (std::vector<App::DocumentObject*>::const_iterator i = originals.begin(); i != originals.end(); ++i)
     {
         if ((*i) != NULL)
-            ui->listWidgetFeatures->insertItem(0, QString::fromLatin1((*i)->getNameInDocument()));
+            ui->listWidgetFeatures->addItem(QString::fromLatin1((*i)->getNameInDocument()));
     }
     // ---------------------
 
@@ -235,7 +235,7 @@ void TaskLinearPatternParameters::onSelectionChanged(const Gui::SelectionChanges
     if (msg.Type == Gui::SelectionChanges::AddSelection) {
         if (originalSelected(msg)) {
             if (selectionMode == addFeature)
-                ui->listWidgetFeatures->insertItem(0, QString::fromLatin1(msg.pObjectName));
+                ui->listWidgetFeatures->addItem(QString::fromLatin1(msg.pObjectName));
             else
                 removeItemFromListWidget(ui->listWidgetFeatures, msg.pObjectName);
 

src/Mod/PartDesign/Gui/TaskMirroredParameters.cpp
@@ -118,7 +118,7 @@ void TaskMirroredParameters::setupUI()
     for (std::vector<App::DocumentObject*>::const_iterator i = originals.begin(); i != originals.end(); ++i)
     {
         if ((*i) != NULL)
-            ui->listWidgetFeatures->insertItem(0, QString::fromLatin1((*i)->getNameInDocument()));
+            ui->listWidgetFeatures->addItem(QString::fromLatin1((*i)->getNameInDocument()));
     }
     // ---------------------
 
@@ -192,7 +192,7 @@ void TaskMirroredParameters::onSelectionChanged(const Gui::SelectionChanges& msg
 
         if (originalSelected(msg)) {
             if (selectionMode == addFeature)
-                ui->listWidgetFeatures->insertItem(0, QString::fromLatin1(msg.pObjectName));
+                ui->listWidgetFeatures->addItem(QString::fromLatin1(msg.pObjectName));
             else
                 removeItemFromListWidget(ui->listWidgetFeatures, msg.pObjectName);
             exitSelectionMode();

src/Mod/PartDesign/Gui/TaskMultiTransformParameters.cpp
@@ -142,7 +142,7 @@ TaskMultiTransformParameters::TaskMultiTransformParameters(ViewProviderTransform
     for (std::vector<App::DocumentObject*>::const_iterator i = originals.begin(); i != originals.end(); i++)
     {
         if ((*i) != NULL)
-            ui->listWidgetFeatures->insertItem(0, QString::fromLatin1((*i)->getNameInDocument()));
+            ui->listWidgetFeatures->addItem(QString::fromLatin1((*i)->getNameInDocument()));
     }
     // ---------------------
 }
@@ -151,7 +151,7 @@ void TaskMultiTransformParameters::onSelectionChanged(const Gui::SelectionChange
 {
     if (originalSelected(msg)) {
         if (selectionMode == addFeature)
-            ui->listWidgetFeatures->insertItem(0, QString::fromLatin1(msg.pObjectName));
+            ui->listWidgetFeatures->addItem(QString::fromLatin1(msg.pObjectName));
         else
             removeItemFromListWidget(ui->listWidgetFeatures, msg.pObjectName);
         exitSelectionMode();

src/Mod/PartDesign/Gui/TaskPolarPatternParameters.cpp
@@ -132,7 +132,7 @@ void TaskPolarPatternParameters::setupUI()
     for (std::vector<App::DocumentObject*>::const_iterator i = originals.begin(); i != originals.end(); ++i)
     {
         if ((*i) != NULL)
-            ui->listWidgetFeatures->insertItem(0, QString::fromLatin1((*i)->getNameInDocument()));
+            ui->listWidgetFeatures->addItem(QString::fromLatin1((*i)->getNameInDocument()));
     }
     // ---------------------
 
@@ -206,7 +206,7 @@ void TaskPolarPatternParameters::onSelectionChanged(const Gui::SelectionChanges&
 
         if (originalSelected(msg)) {
             if (selectionMode == addFeature)
-                ui->listWidgetFeatures->insertItem(0, QString::fromLatin1(msg.pObjectName));
+                ui->listWidgetFeatures->addItem(QString::fromLatin1(msg.pObjectName));
             else
                 removeItemFromListWidget(ui->listWidgetFeatures, msg.pObjectName);
             exitSelectionMode();

src/Mod/PartDesign/Gui/TaskScaledParameters.cpp
@@ -114,7 +114,7 @@ void TaskScaledParameters::setupUI()
     for (std::vector<App::DocumentObject*>::const_iterator i = originals.begin(); i != originals.end(); ++i)
     {
         if ((*i) != NULL)
-            ui->listWidgetFeatures->insertItem(0, QString::fromLatin1((*i)->getNameInDocument()));
+            ui->listWidgetFeatures->addItem(QString::fromLatin1((*i)->getNameInDocument()));
     }
     // ---------------------
 
@@ -149,7 +149,7 @@ void TaskScaledParameters::onSelectionChanged(const Gui::SelectionChanges& msg)
 {
     if (originalSelected(msg)) {
         if (selectionMode == addFeature)
-            ui->listWidgetFeatures->insertItem(0, QString::fromLatin1(msg.pObjectName));
+            ui->listWidgetFeatures->addItem(QString::fromLatin1(msg.pObjectName));
         else
             removeItemFromListWidget(ui->listWidgetFeatures, msg.pObjectName);
         exitSelectionMode();