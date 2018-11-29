FreeCAD/FreeCAD

Drags and drop with groups

src/Gui/ViewProviderGroupExtension.cpp
@@ -111,6 +111,31 @@ void ViewProviderGroupExtension::extensionDropObject(App::DocumentObject* obj) {
     Gui::Command::doCommand(Gui::Command::App, cmd.toUtf8());
 }
 
+void ViewProviderGroupExtension::extensionDropReplaceObject(App::DocumentObject* oldValue, App::DocumentObject* newValue) {
+
+    App::DocumentObject* grp = static_cast<App::DocumentObject*>(getExtendedViewProvider()->getObject());
+    App::Document* doc = grp->getDocument();
+
+    // build Python command for execution
+    QString cmd;
+    cmd = QString::fromLatin1("App.getDocument(\"%1\").getObject(\"%2\").removeObject("
+                        "App.getDocument(\"%1\").getObject(\"%3\"))")
+                        .arg(QString::fromLatin1(doc->getName()))
+                        .arg(QString::fromLatin1(grp->getNameInDocument()))
+                        .arg(QString::fromLatin1(oldValue->getNameInDocument()));
+
+    Gui::Command::doCommand(Gui::Command::App, cmd.toUtf8());
+    cmd = QString::fromLatin1("App.getDocument(\"%1\").getObject(\"%2\").addObject("
+                        "App.getDocument(\"%1\").getObject(\"%3\"))")
+                        .arg(QString::fromLatin1(doc->getName()))
+                        .arg(QString::fromLatin1(grp->getNameInDocument()))
+                        .arg(QString::fromLatin1(newValue->getNameInDocument()));
+
+    Gui::Command::doCommand(Gui::Command::App, cmd.toUtf8());
+}
+    
+
+
 std::vector< App::DocumentObject* > ViewProviderGroupExtension::extensionClaimChildren(void) const {
 
     auto* group = getExtendedViewProvider()->getObject()->getExtensionByType<App::GroupExtension>();    

src/Gui/ViewProviderGroupExtension.h
@@ -46,6 +46,7 @@ public:
     virtual bool extensionCanDropObjects() const override;
     virtual bool extensionCanDropObject(App::DocumentObject*) const override;
     virtual void extensionDropObject(App::DocumentObject*) override;   
+    virtual void extensionDropReplaceObject(App::DocumentObject* oldValue, App::DocumentObject* newValue) override;
  
     virtual void extensionHide(void) override;
     virtual void extensionShow(void) override;

berndhahnebach/All_FreeCAD

src/Gui/ViewProviderGroupExtension.cpp
@@ -110,6 +110,29 @@ void ViewProviderGroupExtension::extensionDropObject(App::DocumentObject* obj) {
     Gui::Command::doCommand(Gui::Command::App, cmd.toUtf8());
 }
 
+void ViewProviderGroupExtension::extensionReplaceObject(App::DocumentObject* oldValue, App::DocumentObject* newValue) {
+
+    App::DocumentObject* grp = static_cast<App::DocumentObject*>(getExtendedViewProvider()->getObject());
+    App::Document* doc = grp->getDocument();
+
+    // build Python command for execution
+    QString cmd;
+    cmd = QString::fromLatin1("App.getDocument(\"%1\").getObject(\"%2\").removeObject("
+                        "App.getDocument(\"%1\").getObject(\"%3\"))")
+                        .arg(QString::fromLatin1(doc->getName()))
+                        .arg(QString::fromLatin1(grp->getNameInDocument()))
+                        .arg(QString::fromLatin1(oldValue->getNameInDocument()));
+
+    Gui::Command::doCommand(Gui::Command::App, cmd.toUtf8());
+    cmd = QString::fromLatin1("App.getDocument(\"%1\").getObject(\"%2\").addObject("
+                        "App.getDocument(\"%1\").getObject(\"%3\"))")
+                        .arg(QString::fromLatin1(doc->getName()))
+                        .arg(QString::fromLatin1(grp->getNameInDocument()))
+                        .arg(QString::fromLatin1(newValue->getNameInDocument()));
+
+    Gui::Command::doCommand(Gui::Command::App, cmd.toUtf8());
+}
+
 std::vector< App::DocumentObject* > ViewProviderGroupExtension::extensionClaimChildren(void) const {
 
     auto* group = getExtendedViewProvider()->getObject()->getExtensionByType<App::GroupExtension>();    

src/Gui/ViewProviderGroupExtension.h
@@ -46,6 +46,7 @@ public:
     virtual bool extensionCanDropObjects() const override;
     virtual bool extensionCanDropObject(App::DocumentObject*) const override;
     virtual void extensionDropObject(App::DocumentObject*) override;   
+    virtual void extensionReplaceObject(App::DocumentObject* oldValue, App::DocumentObject* newValue) override;
  
     virtual void extensionHide(void) override;
     virtual void extensionShow(void) override;